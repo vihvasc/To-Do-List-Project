591de323f6adae1beb439a6c3a77808605fe801f
leandro oliveira <leandrooliveira35@gmail.com>
2021-08-15 14:15:12 -0300

    requisito 5 completo

---
+++ script.js
@@ -0,0 +1,12 @@
+const textoTarefa = document.getElementById('texto-tarefa');
+const criarTarefa = document.getElementById('criar-tarefa');
+const listaTarefas = document.getElementById('lista-tarefas');
+
+criarTarefa.addEventListener('click', adicionarTarefa);
+
+function adicionarTarefa() {
+  const tarefa = document.createElement('li');
+  console.log(textoTarefa.value);
+  tarefa.innerText = textoTarefa.value;
+  listaTarefas.appendChild(tarefa);
+}
