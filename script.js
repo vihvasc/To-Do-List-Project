eae73c167e62f1b62c3f720e6d23f9fda7fb403a
leandro oliveira <leandrooliveira35@gmail.com>
2021-08-15 15:41:31 -0300

    requisito 8 e 9 completos

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const listaTarefas = document.getElementById('lista-tarefas');
 
 criarTarefa.addEventListener('click', taskAdd);
 listaTarefas.addEventListener('click', taskSelected);
+listaTarefas.addEventListener('dblclick', taskCompleted);
 
 function taskAdd() {
   const tarefa = document.createElement('li');
@@ -14,5 +15,19 @@ function taskAdd() {
 }
 
 function taskSelected(event) {
-  event.target.classList.add('selected');
+  const selected = document.querySelector('.selected');
+  if (!selected) {
+    event.target.classList.add('selected');
+  } else {
+    selected.classList.remove('selected');
+    event.target.classList.add('selected');
+  }
+}
+
+function taskCompleted(event) {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
 }
