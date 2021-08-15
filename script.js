02460c19bf593a487cd9dfeb52cb9e77a32183aa
iuryroque <iury.roque@hotmail.com>
2021-08-15 16:39:31 -0300

    Requisito 11 concluido

--- script.js
+++ script.js
@@ -2,14 +2,21 @@ let inputText = document.getElementById("texto-tarefa");
 let ordenadeList = document.getElementById("lista-tarefas");
 const buttonAdd = document.getElementById("criar-tarefa");
 const buttonClearAll = document.getElementById("apaga-tudo");
+const buttonClearCompleted = document.getElementById("remover-finalizados");
 
 buttonAdd.addEventListener("click",addList);
 function addList() {
-  const createLi = document.createElement("li");
-  createLi.innerText = inputText.value
-  createLi.className = "lista"
-  ordenadeList.appendChild(createLi);
-  inputText.value = "";
+
+  if (inputText.value !== "") {
+    const createLi = document.createElement("li");
+    createLi.innerText = inputText.value
+    createLi.className = "lista"
+    ordenadeList.appendChild(createLi);
+//Limpa o campo de texto após adicionar
+    inputText.value = "";
+  } else {
+    let placeholder = inputText.placeholder = "Lista vazia!";
+  }
 }
 
 ordenadeList.addEventListener("click", changeBackgroundColor);
@@ -25,15 +32,22 @@ function changeBackgroundColor(event) {
 
 ordenadeList.addEventListener("dblclick", completedList);
 function completedList(event) {
-  const completed = document.querySelector(".completed");
-  if (!completed) {
-    event.target.classList.add("completed");
-  }else{
+  if (event.target.classList.contains("completed")) {
     event.target.classList.remove("completed");
+  }else{
+    event.target.classList.add("completed");
   }
 }
 
 buttonClearAll.addEventListener("click", clearAll);
 function clearAll() {
   ordenadeList.innerHTML = "";
+}
+
+buttonClearCompleted.addEventListener("click",clearCompleted)
+function clearCompleted() {
+  let completed = document.querySelectorAll(".completed");
+  for (const key of completed) {
+    ordenadeList.removeChild(key)
+  }
 }
