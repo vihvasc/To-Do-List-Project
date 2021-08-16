32a1c2f0e87daa2e01d16671a67d9e0c50e2b911
Gabriel Rodrigues <huijirohankei@gmail.com>
2021-08-16 15:52:33 -0300

    Adicionado funcionalidade total a lista de tarefas

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const addBtn = document.querySelector('#criar-tarefa');
 const addText = document.querySelector('#texto-tarefa');
 const todoList = document.querySelector('#lista-tarefas');
+const delBtn = document.querySelector('#apaga-tudo');
+const endBtn = document.querySelector('#remover-finalizados')
 
+delBtn.addEventListener('click', () => {
+    todoList.innerHTML = '';
+})
+
+endBtn.addEventListener('click', () => {
+    document.querySelectorAll('.completed').forEach((e) => {
+        e.remove()
+    });
+})
 
 addBtn.addEventListener('click', () => {
     if (addText.value !== '') {
@@ -14,12 +25,16 @@ addBtn.addEventListener('click', () => {
         todo.addEventListener('click', (e) => {
             let allTodos = document.querySelectorAll('.todo');
             allTodos.forEach(e => {
-                e.className = 'todo';
+                e.classList.remove('selected');
             });
-            e.target.className = 'todo selected';
+            e.target.classList.add('selected');
 
         })
+
+        todo.addEventListener('dblclick', (e) => {
+            e.target.classList.toggle('completed');
+        })
     } else {
-        alert('Especifique a tarefa')
+        alert('Especifique a tarefa');
     }
 });
