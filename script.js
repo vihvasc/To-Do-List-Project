cac321212390302a0d1e23c35e6c93f4462ff426
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-16 15:02:07 -0300

    Finaliza requisito02 e insere orientaçao document.body dentro da constant body.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const body = document.body;
 // Requisitos Obrigatórios:
 // 01 - Adicione à sua lista o título "Minha Lista de Tarefas" em uma tag
 // *** O que será verificado:
@@ -5,9 +6,9 @@
 
 function adicionaTitulo() {
   const elementoHeader = document.createElement('header');
-  elementoHeader.textContent = 'Minha Lista de Tarefas';
   elementoHeader.id = 'titulo-header';
-  document.body.appendChild(elementoHeader);
+  elementoHeader.textContent = 'Minha Lista de Tarefas';
+  body.appendChild(elementoHeader);
 }
 adicionaTitulo();
 
@@ -16,6 +17,9 @@ adicionaTitulo();
 // ***** Será verificado que existe na sua página um elemento com o id funcionamento com o conteúdo Clique duas vezes em um item para marcá-lo como completo
 
 function adicionaParagrafo() {
-  
+  const elementoP = document.createElement('p');
+  elementoP.id = 'funcionamento';
+  elementoP.textContent = 'Clique duas vezes em um item para marcá-lo como completo';
+  body.appendChild(elementoP);
 }
 adicionaParagrafo();
