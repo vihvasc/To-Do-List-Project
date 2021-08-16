fbb3023e3a98d10cc47b9c37f3d701275efa2a1c
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-08-16 16:06:57 -0300

    Mudando cor de fundo da li para cinza ao ser clicada

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const addButton = document.getElementById('criar-tarefa')
-const inputText = document.getElementById('texto-tarefa')
-
+const list = document.getElementById('lista-tarefas')
 
 function addTask() {
-  const list = document.getElementById('lista-tarefas')
+  const inputText = document.getElementById('texto-tarefa')
 
   let task = document.createElement('li')
   task.innerText = inputText.value
@@ -11,5 +10,10 @@ function addTask() {
 
   inputText.value = ''
 }
-
+function changeLiColor(e) {
+  let element = e.target
+  element.style.backgroundColor = 'rgb(128, 128, 128)'
+  element.classList.add('selected')
+}
 addButton.addEventListener('click', addTask)
+list.addEventListener('click', changeLiColor)
