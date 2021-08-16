622f77d5e43d2c6ecd70c95694349d17468b6875
Rafael Toledo <rafael.toledo@engenharia.ufjf.br>
2021-08-16 16:06:04 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
   let inputTarefa = document.getElementById("texto-tarefa");
   let criarTarefa = document.getElementById("criar-tarefa");
   let listaTarefa = document.getElementById("lista-tarefas");
+  let body = document.querySelector('body');
 
   criarTarefa.addEventListener("click", adicionaTarefa);
 
@@ -11,3 +12,25 @@
       listaTarefa.appendChild(novaTarefa);
       inputTarefa.value = "";
   }
+
+  function addSelected(element){
+      let tarefas = Array.from(listaTarefa.children);
+      for (let index = 0; index < tarefas.length; index += 1){
+          let tarefaAtual = tarefas[index];
+          if(tarefaAtual.classList.contains("selected")){
+              tarefaAtual.classList.remove("selected");
+          }
+      }
+      element.classList.add("selected");
+  }
+
+  function checkItem(originEvent) {
+    const element = originEvent.target;
+    if (element.parentNode.id === 'lista-tarefas') {
+      addSelected(element);
+    }
+  }
+
+  body.addEventListener('click', checkItem);
+
+  
