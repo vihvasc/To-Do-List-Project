9581754f5fd21f89a762f19ccb2a247483c2a3a4
Amanda Fernandes <aferanda@gmail.com>
2021-08-16 18:33:02 -0300

    Adiciona requisitos 7 e 8

--- script.js
+++ script.js
@@ -3,23 +3,21 @@ const buttonAdd = document.getElementById('criar-tarefa');
 const listTasks = document.getElementById('lista-tarefas');
 const arrayList = document.getElementsByTagName('li');
 
+// Requisito 7 e 8
+function changeBgColor(event) {
+  for (let index = 0; index < arrayList.length; index += 1) {
+    arrayList[index].style.backgroundColor = '';
+  }
+  const eventClicked = event.target;
+  eventClicked.style.backgroundColor = 'rgb(128, 128, 128)';
+}
 // Requisitos 5 e 6
 function addTasks() {
   const li = document.createElement('li');
   li.innerText = input.value;
+  li.addEventListener('click', changeBgColor);
   listTasks.appendChild(li);
   input.value = '';
-  changeBgColor();
 }
 
 buttonAdd.addEventListener('click', addTasks);
-
-// Requisito 7
-function changeBgColor() {
-  for (let index = 0; index < arrayList.length; index += 1) {
-    arrayList[index].addEventListener('click', () => {
-      arrayList[index].className = 'clicked';
-      arrayList[index].style.backgroundColor = 'rgb(128, 128, 128)';
-    });
-  }
-}
