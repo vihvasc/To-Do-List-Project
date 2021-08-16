7b3be857dadc86bbdde26f1447a9f28bcd87421c
Arthur <viegas.arthur2013@hotmail.com>
2021-08-16 19:06:26 -0300

    requisito 8: seleciona apenas uma task

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let ordenedList = document.querySelector('#lista-tarefas');
-let taskItens = document.querySelectorAll('.task-item');
+let taskItens = document.getElementsByClassName('task-item');
 let taskInput = document.querySelector('#texto-tarefa');
 let addButton = document.querySelector('#criar-tarefa');
 
@@ -8,21 +8,31 @@ let addButton = document.querySelector('#criar-tarefa');
 function addList(){
 let task = taskInput.value
 let taskList = document.createElement('li');
-    taskList.innerHTML = task;
-    taskList.style.backgroundColor = '';
-  if (taskInput.value == ''){
-    alert('Por favor adicione uma tarefa primeiro!');
-  }else{
-    ordenedList.appendChild(taskList);
-    taskInput.value = '';
-  }
+  taskList.innerHTML = task;
+  taskList.className = 'task-item'
+  taskList.style.backgroundColor = '';
+ if (taskInput.value == ''){
+  alert('Por favor adicione uma tarefa primeiro!');
+ }else{
+  ordenedList.appendChild(taskList);
+  taskInput.value = '';
+ }
 }
-function taskBackgroundColor(event){
-  if (event.target.style.backgroundColor == ''){
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-  } else if (event.target.style.backgroundColor == 'rgb(128, 128, 128)'){
-    event.target.style.backgroundColor = '';
-  }
+
+function taskHighlight(event){
+      if (event.target.style.backgroundColor == ''){
+        event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+      } 
 }
+function clearHighlight(event){
+  for (let i = 0; i < taskItens.length; i++) {
+    if (taskItens[i].style.backgroundColor === 'rgb(128, 128, 128)'){
+        taskItens[i].style.backgroundColor = '';
+    }
+  }
+  taskHighlight(event)
+}
+
+
 addButton.addEventListener('click', addList)
-ordenedList.addEventListener('click', taskBackgroundColor)
+ordenedList.addEventListener('click', clearHighlight)
