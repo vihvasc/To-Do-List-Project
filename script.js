9149e86dff2203bef4c0103655c79240386043ba
alxedv <alexmaia.etel@gmail.com>
2021-08-16 15:31:43 -0300

    Adiciona o requisito 8

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Faz a função de adicionar o input como item da lista
 let listaTarefa = document.querySelector("#lista-tarefas")
 let btnCriarTarefa = document.getElementById("criar-tarefa")
 btnCriarTarefa.addEventListener("click", adicionaTarefa)
@@ -5,14 +6,24 @@ btnCriarTarefa.addEventListener("click", adicionaTarefa)
 function adicionaTarefa(){
     let criarTarefa = document.getElementById("texto-tarefa").value
     let tarefa = document.createElement("li")
-    tarefa.id = "tarefa"
+    tarefa.className = "tarefa"
     tarefa.innerText = criarTarefa
     listaTarefa.appendChild(tarefa)
     criarTarefa = document.getElementById("texto-tarefa").value = ""
     tarefa.addEventListener("click", recebeClick)
 }
 
-function recebeClick(){
-    let tarefa = document.getElementById("tarefa")
-    tarefa.style.backgroundColor = "rgb(128, 128, 128)"
+//Função para selecionar o item da lista
+function recebeClick(item){
+    let itens = document.getElementsByClassName("tarefa")
+    for(let index = 0; index < itens.length; index += 1){
+        if(itens[index].classList.contains("selected")){
+            itens[index].classList.toggle("selected")
+            itens[index].style.backgroundColor = "white"
+        }
+    }
+    item.target.classList.toggle("selected")
+    if(item.target.classList.contains("selected")){
+        item.target.style.backgroundColor = "rgb(128, 128, 128)"
+    }
 }
