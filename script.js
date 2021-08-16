51a76bcd8804fb22c1ec1317d075c3aed50b0610
Deivid <deividmborges@gmail.com>
2021-08-16 16:48:38 -0300

    adiciona requisito 6 e 7

--- script.js
+++ script.js
@@ -1,19 +1,25 @@
 window.onload = criaListaTarefas;
 
 function criaListaTarefas() {
-  generateClickEvent();
+  geraItemList();
+  selecionaItemList();
 }
 
 let list = document.getElementById('lista-tarefas');
 let inputBox = document.getElementById('texto-tarefa');
 
-function generateClickEvent() {
+function geraItemList() {
   let buttonAdcItemList = document.getElementById('criar-tarefa');
   buttonAdcItemList.addEventListener('click', adicionaItemLista)
 }
 function adicionaItemLista() {
   let itemList = document.createElement('li');
   itemList.innerText = inputBox.value;
+  itemList.className = 'item-list';
   inputBox.value = '';
+  itemList.addEventListener('click', changeBackgroundColor)
   list.appendChild(itemList);
+}
+function changeBackgroundColor(event) {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
