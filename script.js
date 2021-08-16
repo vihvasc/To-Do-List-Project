b4b1cbac3488d1209217908d08cb5430c67c5c52
Iago Mesquita <iagomesquitaalves@gmail.com>
2021-08-16 19:00:48 -0300

    Refatora quesitos 4 e 5, input; ol e button na mesma section

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 
 const section = document.getElementsByTagName('section')[0];
 
-let listaOrdenada = document.createElement('ol');
-listaOrdenada.id = "lista-tarefas";
-section.appendChild(listaOrdenada);
+let input = document.createElement('input');
+input.type = 'text';
+input.id = 'texto-tarefa';
+section.appendChild(input);
 
 let button = document.createElement('button');
 button.id = 'criar-tarefa';
 section.appendChild(button);
+
+let listaOrdenada = document.createElement('ol');
+listaOrdenada.id = "lista-tarefas";
+section.appendChild(listaOrdenada);
+
