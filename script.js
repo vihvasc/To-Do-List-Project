955f50f65f413f94982e18a264691f9db5aefdc1
Rodrigo Lima <rodrigootavio19@gmail.com>
2021-08-16 15:56:19 -0400

    cumpre requisito 9

--- script.js
+++ script.js
@@ -2,21 +2,40 @@ const takeBtnNewTask = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 
-function changeColor(evento) {
-  const backColor = evento.target;
+function removeAllClass(classe) {
   const currentList = document.querySelectorAll('#lista-tarefas li');
 
   for (let i = 0; i < currentList.length; i += 1) {
-    currentList[i].classList.remove('selected');
+    currentList[i].classList.remove(classe);
   }
+}
 
-  backColor.className = 'selected';
+function changeColor(evento) {
+  const backColor = evento.target;
+
+  if (backColor.classList.contains('selected')) {
+    removeAllClass('selected');
+  } else {
+    removeAllClass('selected');
+    backColor.classList.add('selected');
+  }
+}
+
+function decoredText(evento) {
+  const risk = evento.target;
+
+  if (risk.classList.contains('completed')) {
+    risk.classList.remove('completed');
+  } else {
+    risk.classList.add('completed');
+  }
 }
 
 function appendNewTask() {
   const newTask = document.createElement('li');
 
   newTask.addEventListener('click', changeColor);
+  newTask.addEventListener('dblclick', decoredText);
   newTask.innerText = input.value;
   taskList.appendChild(newTask);
 
