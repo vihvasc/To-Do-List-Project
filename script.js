8f8b935017b18f1976caa14ffd7f175aa0fa1341
Rodolpho Coller <rodolpho.coller12@gmail.com>
2021-08-16 16:06:51 -0300

    adiciona requisito 07 e 08

--- script.js
+++ script.js
@@ -4,9 +4,27 @@ button.addEventListener('click', addTask);
 
 function addTask() {
   let ol = document.getElementById('lista-tarefas');
-  let li = document.createElement('li');
   let input = document.getElementById('texto-tarefa');
+  let li = document.createElement('li');
   ol.appendChild(li).innerText = input.value;
   input.value = '';
 }
-let input = document.getElementById('texto-tarefa');
+
+let li = document.getElementsByTagName('li');
+let ul = document.getElementById('lista-tarefas');
+
+ul.addEventListener('click', markTask);
+
+function markTask(event) {
+  const selectClick = event.target;
+  cleanMark()
+  if (selectClick.tagName === 'LI') {
+    event.target.style.backgroundColor = 'rgb(128,128,128)';
+  }
+}
+
+function cleanMark() {
+  for (let i = 0; i < li.length; i += 1) {
+    li[i].style.backgroundColor = 'white';
+  }
+}
