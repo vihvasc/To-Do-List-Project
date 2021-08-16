d8294810624eef6ee78f4c72e2461ab55d143f7a
Antonio Carlos Nunes da Silva Junior <tonhaun@yahoo.com.br>
2021-08-16 15:23:38 -0300

    Cumprido os requisitos 5 e 6

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+function createTask() {
+  const actionButton = document.getElementById('criar-tarefa');
+  const contentTarget = document.getElementById('lista-tarefas');
+  const inputElement = document.getElementById('texto-tarefa');
 
+  actionButton.addEventListener('click', function() {
+    const newTask = document.createElement('li');
+    newTask.innerHTML = inputElement.value;
+    contentTarget.appendChild(newTask);
+    inputElement.value = '';
+  })
+};
+
+createTask();
