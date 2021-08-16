ea3e88c3602695f15195a5fb738b776ef912a673
Denis Jonathan <denisjonathan7@gmail.com>
2021-08-16 17:21:35 -0300

    Requisito 7 concluído

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-const taskInput = document.getElementById('texto-tarefa');
-const listaTarefas = document.getElementById('lista-tarefas');
-const criarTarefa = document.getElementById('criar-tarefa');
+const taskInput = document.getElementById('texto-tarefa'); // Input
+const listaTarefas = document.getElementById('lista-tarefas'); // Lista Ordenada
+const criarTarefa = document.getElementById('criar-tarefa'); // Botão adicionar tarefa
 
 function addNewTask() {
   const li = document.createElement('li');
   li.innerText = taskInput.value;
+  li.className = 'tarefa';
   listaTarefas.appendChild(li);
   taskInput.value = '';
+  addListenerForTasks();
+}
+
+function addListenerForTasks() {
+  const tarefasCriadas = [...document.getElementsByClassName('tarefa')]; // Todas tarefas criadas
+  tarefasCriadas.forEach(tarefa => {
+    tarefa.addEventListener('click', paintGrey);
+  });
+}
+
+function paintGrey(event) {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
 criarTarefa.addEventListener('click', addNewTask);
