e56741c63b7b6f8e7489bed0ab79aef387c6d2fa
Sayro <sayrodamaceno70@gmail.com>
2021-08-16 16:25:49 -0300

    cumpre requisito 7 e 8

--- script.js
+++ script.js
@@ -6,8 +6,28 @@ function addTask() {
   const listItem = document.createElement('li');
   const task = inputText.value;
   listItem.innerText = task;
+  listItem.className = 'item';
   itens.appendChild(listItem);
   inputText.value = '';
 }
 
 buttonAdd.addEventListener('click', addTask);
+
+// altera cor de fundo da lista
+const tasks = document.getElementById('lista-tarefas');
+
+function removeClass() {
+  const classList = document.querySelectorAll('.item');
+  for (let index = 0; index < classList.length; index += 1) {
+    classList[index].classList.remove('selected');
+  }
+}
+
+function changeColor(event) {
+  if (event.target.className === 'item') {
+    removeClass();
+    event.target.classList.add('selected');
+  }
+}
+
+tasks.addEventListener('click', changeColor);
