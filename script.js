5e8df17290434ae1ca3534d3146883983a29cc46
Rafael Colombo <rcolombodev@gmail.com>
2021-08-16 16:54:13 -0300

    adiciona requisito 7

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
-const taskList = document.getElementById('lista-tarefas');
-const input = document.querySelector('#texto-tarefa');
-const addTaskButton = document.querySelector('#criar-tarefa');
+const ordererList = document.getElementById('lista-tarefas');// lista ordenada.
+const input = document.querySelector('#texto-tarefa'); // input
+const addTaskButton = document.querySelector('#criar-tarefa');// botão adicionar tarefa.
 
 function addTask() {
   addTaskButton.addEventListener('click', () => {
     const createList = document.createElement('li');
     createList.innerText = input.value;
-    taskList.appendChild(createList);
+    createList.className = 'task';
+    ordererList.appendChild(createList);
     input.value = '';
+    addGrayBackground();
   });
+}
+
+// function addListenerForTasks() {
+//   const createdTasks = document.getElementsByClassName('task');// todas tarefas criadas.
+//   for (let index = 0; index < createdTasks.length; index += 1) {
+//     createdTasks[index].addEventListener('click', addGrayBackground);
+//   }
+// }
+
+function addGrayBackground() {
+  const createdTasks = document.getElementsByClassName('task');// todas tarefas criadas.
+  for (let index = 0; index < createdTasks.length; index += 1) {
+    createdTasks[index].addEventListener('click', (event) => {
+      const eventColor = event.target;
+      eventColor.style.backgroundColor = 'rgb(128, 128, 128)';
+    });
+  }
 }
 
 function allTask() {
   addTask();
+  addListenerForTasks();
 }
 window.onload = allTask;
