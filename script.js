3ec0ac5ffee691b5b79c56acac8b7f3edd7fc941
henrique <hickm018@gmail.com>
2021-08-16 16:11:51 -0300

    Adiciona requisito 7

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-const button = document.getElementById('criar-tarefa');
-button.addEventListener('click', getInputAddLi);
+requisito5e6();
 
-function getInputAddLi() {
-  const li = document.createElement('li');
-  const paiLi = document.getElementById('lista-tarefas');
-  paiLi.appendChild(li);
-  let input = document.getElementById('texto-tarefa');
-  let input2 = input.value;
-  li.innerText = input2;
-  input.value = '';
+function requisito5e6() {
+  const button = document.getElementById('criar-tarefa');
+  button.addEventListener('click', getInputAddLi);
+
+  function getInputAddLi() {
+    const li = document.createElement('li');
+    const paiLi = document.getElementById('lista-tarefas');
+    paiLi.appendChild(li);
+    li.className = 'colors';
+    let input = document.getElementById('texto-tarefa');
+    let input2 = input.value;
+    li.innerText = input2;
+    input.value = '';
+    requisito7();
+  }
+}
+
+function requisito7() {
+  const listasLi = document.querySelectorAll('.colors');
+  console.log(listasLi);
+  for (let key of listasLi) {
+    key.addEventListener('click', changeColor);
+  }
+
+  function changeColor(event) {
+    console.log('ola');
+    const evento = event.target;
+    evento.style.backgroundColor = 'gray';
+  }
 }
