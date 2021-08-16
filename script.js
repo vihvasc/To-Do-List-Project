c886087577466b8347e44a137a3c16c45c31a367
Matheus <matheusfelippeh@gmail.com>
2021-08-16 15:42:45 -0300

    add requisite 8

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 const buttonAddTask = document.getElementById('criar-tarefa');
 buttonAddTask.addEventListener('click',addTask);
 const taskList = document.getElementById('lista-tarefas');
+const allTasks = document.getElementsByClassName('task');
 
 function addTask(){
     let textInput = document.getElementById('texto-tarefa').value 
     let newTask = document.createElement('li')
     newTask.innerText = textInput
     newTask.addEventListener('click',changeColor)
+    newTask.className = 'task'
     taskList.appendChild(newTask)
     document.getElementById('texto-tarefa').value = ''
 }
 
 function changeColor(event){
+    let index = allTasks.length
+    for(let count = 0;count < index; count += 1){
+        if(window.getComputedStyle(allTasks[count]).backgroundColor === 'rgb(128, 128, 128)'){
+            allTasks[count].style.backgroundColor = 'white'
+        }
+    }
     event.target.style.backgroundColor = 'rgb(128, 128, 128)'
-    console.log('oi')
 }
