aa222c15dbbe8fe7ceb2942a0cfc9b03a8e499e6
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-16 16:02:06 -0300

    Remove DEBUG code do script.js

--- script.js
+++ script.js
@@ -52,8 +52,6 @@ criaLO();
 // ***** No campo de input será digitado o texto de uma tarefa qualquer e, em seguida, clicar-se-á no botão de criar tarefa. Será verificado que, após o clique, o texto digitado aparece na lista e desaparece do input.
 // ***** A adição de elementos na lista será feita algumas vezes, e será checado se todos os itens criados permanecem na lista na medida em que novos são adicionados.
 
-const textoAAdicionar = document.getElementById('texto-tarefa');
-console.log(textoAAdicionar.innerText);
 function criaTarefa() {
   // Adicionar Botão com id='criar-tarefa'
   const botaoCriarTarefa = document.createElement('button');
