27eb76120ddf121c5c0729b51538fe6cf7a0744b
Jesse Benevides <jessebenevides@gmail.com>
2021-08-16 18:31:20 -0300

    Requisto 5 - botão criado

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-// Cria o input para adicionar Tarefas
 createInput();
+createButton('Adicionar', 'criar-tarefa', 'input-button');
 
+// Cria o input para adicionar Tarefas
 function createInput() {
-  let inputContent = document.getElementById('input-container');
+  let inputContainer = document.getElementById('input-container');
   let input = document.createElement('input');
 
   input.id = 'texto-tarefa';
-  inputContent.appendChild(input);
+  inputContainer.appendChild(input);
+}
+
+// Cria botão - parametros: id do botão, texto do botão e id do pai
+function createButton(buttonText, buttonId, parentId) {
+  let buttonParent = document.getElementById(parentId);
+  let button = document.createElement('button');
+
+  button.id = buttonId;
+  button.innerText = buttonText;
+  buttonParent.appendChild(button);
 }
