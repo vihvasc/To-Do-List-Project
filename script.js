e2df222c2860c4955d4f1a452304f3ec35eab413
Wilian Pereira <wilian_pereira92@outlook.com>
2021-08-16 17:13:29 -0300

    Refatorando desafio 1 e adicionado desafio 2: Criando um paragrafo

---
+++ script.js
@@ -0,0 +1,12 @@
+const corpoDoProjeto = document.getElementsByTagName('body');
+const headerTitle = document.createElement('header');
+const title = document.createElement('h1');
+title.innerText = 'Minha Lista de Tarefas';
+const createSection = document.createElement('section');
+const createParagraph = document.createElement('p');
+createParagraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+createParagraph.id = 'funcionamento';
+corpoDoProjeto[0].appendChild(headerTitle);
+headerTitle.appendChild(title);
+corpoDoProjeto[0].appendChild(createSection);
+createSection.appendChild(createParagraph);
