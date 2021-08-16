1c95293a05160e26304dd465a9e8ddc6fd2774ae
Adriana <adrianam754@gmail.com>
2021-08-16 18:23:06 -0300

    Adicionada requisito 8

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const addButton = document.getElementById('criar-tarefa');
-const addInput = document.getElementById('texto-tarefa');
-const listOrd = document.getElementById('lista-tarefas');
+const addButton = document.getElementById('criar-tarefa'); //botao
+const addInput = document.getElementById('texto-tarefa'); // input
+const listOrd = document.getElementById('lista-tarefas');//lista ordenada
+const taskCreate = document.getElementsByTagName('li');//listas das tarefas criadas
 
 function inputTask() {
   addButton.addEventListener('click', function () {
@@ -8,6 +9,24 @@ function inputTask() {
     li.innerText = addInput.value;
     addInput.value = "";
     listOrd.appendChild(li);
+    addEvent();
   });
 }
 inputTask()
+
+// Requisito 7
+
+function addEvent() {
+  for (let index = 0; index < taskCreate.length; index += 1) {
+    taskCreate[index].addEventListener('click', function () {
+      removeSelector()
+      taskCreate[index].style.backgroundColor = ' rgb(128, 128, 128)';
+    });
+  }
+}
+
+function removeSelector() {
+  for (let index = 0; index < taskCreate.length; index += 1) {
+    taskCreate[index].style.backgroundColor = "";
+  }
+}
