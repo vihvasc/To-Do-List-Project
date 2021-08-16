5bf56f7076639b4dce13d28918f1a708618af181
Erik Lopes <erik.lopesdev@outlook.com>
2021-08-16 19:40:29 -0300

    desafio 8

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-function verifyInputValueEmpty() {
-  const getOrdenedList = document.querySelector('#lista-tarefas');
-  const getTask = document.querySelector('#texto-tarefa');
+const getOrdenedList = document.querySelector('#lista-tarefas');
+const getTask = document.querySelector('#texto-tarefa');
+const getCreateTaskButton = document.querySelector('#criar-tarefa');
 
+function verifyInputValueEmpty() {
   if (getTask.value === '') {
     return alert('Insira um texto para a sua tarefa!');
   }
@@ -12,13 +13,39 @@ function verifyInputValueEmpty() {
 }
 
 function createTask() {
-  const getCreateTaskButton = document.querySelector('#criar-tarefa');
+  getCreateTaskButton.addEventListener('click', verifyInputValueEmpty);
+}
 
-  getCreateTaskButton.addEventListener('click', () => {
-    verifyInputValueEmpty();
+function createNotification() {
+  const notificationDiv = document.querySelector('#notification');
+  notificationDiv.className = 'notification';
+  setTimeout(() => {
+    notificationDiv.className = 'display-none';
+  }, 2000);
+}
+
+function verifyAddBackground() {
+  const getClass = 'selected';
+  const getClassSelected = document.getElementsByClassName(getClass);
+  if (getClassSelected.length === 1) {
+    createNotification();
+  } else {
+    event.target.classList.add(getClass);
+  }
+}
+
+function addBackground() {
+  const getClass = 'selected';
+  getOrdenedList.addEventListener('click', (e) => {
+    if (e.target.classList.contains(getClass)) {
+      e.target.classList.remove(getClass);
+    } else {
+      verifyAddBackground();
+    }
   });
 }
 
 window.onload = () => {
   createTask();
+  addBackground();
 };
