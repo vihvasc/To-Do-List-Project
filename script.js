c908f64c93fafe1070e0ac562c639afa804a3493
Jesse Benevides <jessebenevides@gmail.com>
2021-08-16 18:52:44 -0300

    Adiciona requisito 5 e 6

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-createInput();
 createButton('Adicionar', 'criar-tarefa', 'input-button');
+createInput();
+
+const inputButton = document.getElementById('criar-tarefa');
+const input = document.getElementById('texto-tarefa');
+const taskList = document.getElementById('lista-tarefas');
+
+inputButton.addEventListener('click', addNewTask);
 
 // Cria o input para adicionar Tarefas
 function createInput() {
@@ -18,4 +24,17 @@ function createButton(buttonText, buttonId, parentId) {
   button.id = buttonId;
   button.innerText = buttonText;
   buttonParent.appendChild(button);
+}
+
+// Adiciona uma nova tarefa na lista
+function addNewTask() {
+  let newtask = document.createElement('li');
+
+  if (input.value) {
+    newtask.innerText = input.value;
+    taskList.appendChild(newtask);
+    input.value = '';
+  } else {
+    alert('Campo vazio!');
+  }
 }
