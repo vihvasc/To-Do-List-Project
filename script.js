dc09b1c3e85d6ff6154ab3b00ee2936e30b5fd36
Rodolfo <rodolfoserralha@hotmail.com>
2021-08-16 15:33:54 -0300

    adicionadno requisito 05

---
+++ script.js
@@ -0,0 +1,45 @@
+// // Requisito 01 - adicionando tag h1
+
+// const h1 = document.createElement('h1');
+// const body = document.getElementsByTagName('body');
+// h1.innerText = 'Minha Lista de Tarefas';
+// document.body.appendChild(h1);
+
+// // Requisito 02 - adicionando tag p e texto
+
+// const p = document.createElement('p');
+// p.id = 'funcionamento';
+// p.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+// document.body.appendChild(p);
+
+// // Requisito 03 - adicionando input
+
+// const input = document.createElement('input');
+
+
+// function addEventButton() {
+// button.addEventListener('click' function() {
+//     const valueInput = document.querySelector('#texto-tarefa');
+//     const valueInput = inputValue = input.value;
+//     const input = document.querySelector('#texto-tarefa');
+//     document.button.appendChild('input');
+//     input.value = '';
+// })
+// }
+
+// addEventButton();
+
+const valueInput = document.querySelector('#texto-tarefa');
+const olList = document.querySelector('#lista-tarefas');
+const button = document.querySelector('#criar-tarefa');
+
+function addInputValue () {
+  button.addEventListener('click' function() {
+  const li = document.createElement('li');
+  li.innerText = valueInput.value;
+  olList.appendChild(li);
+  valueInput = '';
+  })
+}
+
+addInputValue();
