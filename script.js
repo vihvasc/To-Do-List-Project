fb8679a65778a95ab210c0b15f795b7468c48776
Camilla Del Guerso <camilladelguerso@gmail.com>
2021-08-16 15:34:56 -0300

    Adiciona requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
+let getOl = document.getElementById('lista-tarefas');
+let getInput = document.getElementById('texto-tarefa');
 function addLi() {
-    let getOl = document.getElementById('lista-tarefas');
-    let getInput = document.getElementById('texto-tarefa');
     let setLi = document.createElement('li');
-    let valorInput = getInput.value;
     setLi.className = 'tarefas';
-    setLi.innerText = valorInput;    
-    getOl.appendChild(setLi);    
+    setLi.innerText = getInput.value;
+    getOl.appendChild(setLi);
     getInput.value = '';
 }
-
 function clickButton() {
     let getButton = document.getElementById('criar-tarefa');
-    
     getButton.addEventListener('click', addLi);
 }
 clickButton();
+
+function changeBackgroundColor(e){
+    if(e.target.classList.contains('tarefas')){
+        let elementSelected = document.querySelector('.selected');
+        if (elementSelected) {
+            elementSelected.classList.remove('selected');
+            elementSelected.style.backgroundColor = 'white';
+        }
+        e.target.classList.add('selected');
+        e.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
+}
+getOl.addEventListener('click', changeBackgroundColor);
