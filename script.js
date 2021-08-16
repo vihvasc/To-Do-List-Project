8e6fff0a108a19cbda6dfbe1ee898e0748e0d83b
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-08-16 16:05:33 -0300

    Adiciona requisitos 7 e 10

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
+function mudaCorBG() {
+// Conforme exemplo visto em
+// http://www.w3bai.com/pt/jsref/met_element_addeventlistener.html
+  this.style.backgroundColor = 'rgb(128,128,128)';
+}
+
 function criarTarefa() {
   const tarefaLista = document.createElement('li');
   const textoTarefaInput = document.getElementById('texto-tarefa');
   const listaTarefas = document.getElementById('lista-tarefas');
-  console.log(textoTarefaInput.innerText);
+  tarefaLista.className = 'tarefasAdicionadas';
   tarefaLista.innerText = textoTarefaInput.value;
+  tarefaLista.addEventListener('click', mudaCorBG);
   listaTarefas.appendChild(tarefaLista);
   textoTarefaInput.value = '';
 }
 
+function apagarLista() {
+  const tarefas = document.getElementById('lista-tarefas');
+  tarefas.innerHTML = '';
+}
+
 const cliqueBotaoCriarTarefa = document.getElementById('criar-tarefa');
 cliqueBotaoCriarTarefa.addEventListener('click', criarTarefa);
+
+const cliqueBotaoApagar = document.getElementById('apaga-tudo');
+cliqueBotaoApagar.addEventListener('click', apagarLista);
