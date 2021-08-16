c4fec3a00da65ae1783726ea2fd348e79ea1d797
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-16 15:53:51 -0300

    Finaliza requisito05.

--- script.js
+++ script.js
@@ -46,6 +46,32 @@ function criaLO() {
 }
 criaLO();
 
+// 05 - Adicione um botão com id="criar-tarefa" e, ao clicar nesse botão, um novo item deverá ser criado ao final da lista e o texto do input deve ser limpo
+// *** O que será verificado:
+// ***** Será verificada a existência de um elemento do tipo button com o id criar-tarefa
+// ***** No campo de input será digitado o texto de uma tarefa qualquer e, em seguida, clicar-se-á no botão de criar tarefa. Será verificado que, após o clique, o texto digitado aparece na lista e desaparece do input.
+// ***** A adição de elementos na lista será feita algumas vezes, e será checado se todos os itens criados permanecem na lista na medida em que novos são adicionados.
+
+const textoAAdicionar = document.getElementById('texto-tarefa');
+console.log(textoAAdicionar.innerText);
+function criaTarefa() {
+  // Adicionar Botão com id='criar-tarefa'
+  const botaoCriarTarefa = document.createElement('button');
+  botaoCriarTarefa.id = 'criar-tarefa';
+  botaoCriarTarefa.textContent = 'Adicionar Tarefa';
+  body.appendChild(botaoCriarTarefa);
+  // Ao se clickar no botão, captura-se o texto do input e armazena em uma variável
+  botaoCriarTarefa.addEventListener('click', function() {
+      const textoAAdicionar = document.getElementById('texto-tarefa');
+      const listaDeTarefas = document.getElementById('lista-tarefas');
+      // Ao se clickar no botão, cria-se uma 'const' li com o conteúdo dessa variável.
+      const novoItem = document.createElement('li');
+      novoItem.innerHTML = textoAAdicionar.value;
+      // Faz-se o appendChild desse elemento li dentro da ol
+      listaDeTarefas.appendChild(novoItem);
+  })
+}
+criaTarefa();
 // 0
 // *** 
 // ***** 
