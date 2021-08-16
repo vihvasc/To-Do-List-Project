508de5ddc1ff532aff7c28da556c50742c3bda16
Vitor <vitoremerich.contato@gmail.com>
2021-08-16 15:26:55 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ function addTask() {
   inputText.value = ""
 }
 taskButton.addEventListener("click", addTask);
+
+let itemList = document.getElementsByTagName("li");
+
+function clickItem(event) {
+  let selectedItem = event.target;
+
+  for(i = 0; i < itemList.length; i += 1) {
+      allItens = itemList[i];
+      allItens.classList.remove("selected");
+  }
+  selectedItem.classList.add("selected");
+}
+list.addEventListener("click", clickItem);
