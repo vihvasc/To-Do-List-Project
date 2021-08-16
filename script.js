ac051e08fd69f258071b553230bf6571ae5a3dda
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2021-08-16 19:00:06 -0300

    welckson valentim - Realiza setimo requisito do projeto todo list, apos aprovacao local

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 let button = document.querySelector('#criar-tarefa');
+let listaTarefa = document.querySelector('#lista-tarefas');
 
-button.addEventListener('click', function(){
+button.addEventListener('click', function() {
   let textoTarefa = document.querySelector('#texto-tarefa');
   let listaTarefa = document.querySelector('#lista-tarefas');
   let criaLi = document.createElement('li');
   criaLi.innerHTML = textoTarefa.value;
   listaTarefa.appendChild(criaLi);
   textoTarefa.value = '';
-})
+});
+
+function corTarefa(event) {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+listaTarefa.addEventListener('click', corTarefa);
