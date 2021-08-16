b859e317c9daada6fcb995b3be4c67a33117fe03
Flavio Junior <flaviopds.jr@gmail.com>
2021-08-16 18:04:58 -0300

    adiciona exercicio 7

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 window.onload = function () {
-
-  let button = document.querySelector("#cria-tarefa");
+  let button = document.querySelector("#criar-tarefa");
   
-
   button.addEventListener("click", function() {
     
     let campoDigitado = document.querySelector("#texto-tarefa");
     let value = campoDigitado.value;
     let li = document.createElement("li");
     li.innerHTML = value;
-    document.querySelector("#lista-tarefas").appendChild(li);
-  })
+    document.querySelector("#lista-tarefas").appendChild(li); 
+    li.className = "color"
+    campoDigitado.value = '';
+    
+    function changeBg(){
+      li.style.backgroundColor = 'rgb(' + 128 + ',' + 128 + ',' + 128 + ')'
+    }
+    
+    li.addEventListener("click", changeBg, false)
+})
 
 }
+
+
