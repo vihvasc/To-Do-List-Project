f46687e37ce3f50ce043e745cc993e1b7cc46afd
Nicolas Franzolin <nicolas.franzolin@gmail.com>
2021-08-16 17:19:21 -0300

    Adiciona Requisito 7

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ let inputTask = document.getElementById('texto-tarefa');
 let buttonAdd = document.getElementById('criar-tarefa');
 let taskList = document.getElementById('lista-tarefas');
 
-console.log(taskList);
-
 //Criando uma tarefa nova como item list para ser usado na OL depois
 
 function createNewTask(task) {
   let li = document.createElement('li');
+  li.classList.add('list');
   li.innerText = task;
+  li.addEventListener('click', selectTask); //linha criada após requisito 6
   return li;
 }
 
@@ -21,11 +21,18 @@ function addNewTask() {
 }
 
 function listtennerButtonAdd() {
-    buttonAdd.addEventListener('click', addNewTask);
+  buttonAdd.addEventListener('click', addNewTask);
 }
 
 function callAllFunctions() {
-    listtennerButtonAdd();
+  listtennerButtonAdd();
+}
+
+function selectTask(event) {
+  let selectedTask = event.target;
+  let lastSelectedTask = document.querySelector('.selected');
+  event.target.classList.add('selected');
+  console.log('cheguei na function selecttask');
 }
 
 window.onload = callAllFunctions;
