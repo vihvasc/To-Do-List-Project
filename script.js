bb87e039002a2f5844d6fea471a99f1e24f41f0e
Valdênio <valdeniomartinho@gmail.com>
2021-08-16 18:50:25 -0300

    requisito 8

--- script.js
+++ script.js
@@ -2,15 +2,24 @@ function criarTarefa(){
     let input = document.getElementById('texto-tarefa');
     let texto = input.value;
     let itemLista = document.getElementById('lista-tarefas');
-    let novoItem = document.createElement('li');    
+    let novoItem = document.createElement('li');     
+    
     novoItem.innerHTML = texto;
     itemLista.appendChild(novoItem);
     input.value = "";
 
     novoItem.addEventListener('click', alteraCor);
-    function alteraCor(evento) {
-        evento.target.style.backgroundColor = 'rgb(128,128,128)'
-    }
-        
+    
     
 }
+function alteraCor(evento) {
+    let listaTotal = document.getElementsByTagName('li');
+    
+    for (let i of listaTotal){
+             
+        i.style.backgroundColor = 'white';     
+   
+    }
+    evento.target.style.backgroundColor = 'rgb(128,128,128)';
+}
+    
