81160b60be53dd2de34b8c02d6a966939977a526
Gabriel Rodrigues <huijirohankei@gmail.com>
2021-08-16 16:57:12 -0300

    Feito exercícios bonus

--- script.js
+++ script.js
@@ -1,10 +1,58 @@
-const addBtn = document.querySelector('#criar-tarefa');
-const addText = document.querySelector('#texto-tarefa');
-const todoList = document.querySelector('#lista-tarefas');
-const delBtn = document.querySelector('#apaga-tudo');
-const endBtn = document.querySelector('#remover-finalizados')
+let addBtn = document.querySelector('#criar-tarefa');
+let addText = document.querySelector('#texto-tarefa');
+let todoList = document.querySelector('#lista-tarefas');
+let delallBtn = document.querySelector('#apaga-tudo');
+let endBtn = document.querySelector('#remover-finalizados');
+let saveBtn = document.querySelector('#salvar-tarefas');
+let upBtn = document.querySelector('#mover-cima');
+let downBtn = document.querySelector('#mover-baixo');
+let delBtn = document.querySelector('#remover-selecionado');
 
 delBtn.addEventListener('click', () => {
+    document.querySelector('.selected').remove();
+})
+
+upBtn.addEventListener('click', (e) => {
+    let allTodos = document.querySelectorAll('.todo');
+    for (let index = 0; index < allTodos.length; index++) {
+        let todo = allTodos[index];
+        let btodo = allTodos[index - 1];
+
+        if (todo.classList.contains('selected') && btodo) {
+            [todo.innerText, btodo.innerText] = [btodo.innerText, todo.innerText];
+            [todo.className, btodo.className] = [btodo.className, todo.className];
+            break;
+        }
+        
+    }
+})
+
+downBtn.addEventListener('click', (e) => {
+    let allTodos = document.querySelectorAll('.todo');
+    for (let index = 0; index < allTodos.length; index++) {
+        let todo = allTodos[index];
+        let btodo = allTodos[index + 1];
+
+        if (todo.classList.contains('selected') && btodo) {
+            [todo.innerText, btodo.innerText] = [btodo.innerText, todo.innerText];
+            [todo.className, btodo.className] = [btodo.className, todo.className];
+            break;
+        }
+    }
+})
+
+
+saveBtn.addEventListener('click', () => {
+    localStorage.todoList = todoList.innerHTML;
+});
+
+window.addEventListener('load', () => {
+    if (localStorage.todoList) {
+        todoList.innerHTML = localStorage.todoList;
+    }
+})
+
+delallBtn.addEventListener('click', () => {
     todoList.innerHTML = '';
 })
 
