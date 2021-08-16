c9201dbc9e57c0bc3a1f0953f683a070d54249f0
Rodrigo Lima <rodrigootavio19@gmail.com>
2021-08-16 15:18:16 -0400

    cumpre requisito 7

--- script.js
+++ script.js
@@ -2,10 +2,20 @@ const takeBtnNewTask = document.getElementById('criar-tarefa');
 const input = document.getElementById('texto-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 
+function changeColor(evento) {
+  const backColor = evento.target;
+
+  if (backColor.style.backgroundColor === '') {
+    backColor.style.backgroundColor = 'rgb(128,128,128)';
+  } else {
+    backColor.style.backgroundColor = '';
+  }
+}
+
 function appendNewTask() {
-  console.log('oi');
   const newTask = document.createElement('li');
 
+  newTask.addEventListener('click', changeColor);
   newTask.innerText = input.value;
   taskList.appendChild(newTask);
 
