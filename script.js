fc040016e69bed9e72a0cb40184e1a24b3be6d94
DanielHott <daniel_hott@outlook.com>
2021-08-16 14:54:43 -0300

    Adicionando requisito numero 4

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ function adicionaTexto() {
   body.appendChild(novoInput);
 }
 adicionaTexto();
+
+function criaLista() {
+  const novaLista = document.createElement('ol');
+  novaLista.id = 'lista-tarefas';
+  const body = document.getElementsByTagName('body')[0];
+  body.appendChild(novaLista);
+}
+criaLista();
