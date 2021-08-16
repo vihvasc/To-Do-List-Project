19a5a6e7058c8716760be8c48eb566246e61a9ef
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2021-08-16 14:46:54 -0300

    adiciona o input

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,26 @@
 function todoList() {
   addTitle();
+  addParagraph();
+  addInput();
+};
+
+function addInput() {
+  const input = document.createElement('input');
+  input.id = 'texto-tarefa';
+  document.body.appendChild(input);
+};
+
+function addParagraph() {
+  const paragraph = document.createElement('p');
+  const header = document.getElementById('header');
+  paragraph.id = 'funcionamento'
+  paragraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+  header.appendChild(paragraph);
 };
 
 function addTitle() {
   const header = document.createElement('header');
+  header.id = 'header';
   const h1 = document.createElement('h1');
   
   h1.innerText = 'Minha Lista de Tarefas';
