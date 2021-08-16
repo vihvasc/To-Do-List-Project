6d05f7ebe6fa76cd34516c57e65d32257917b59f
Virginia <vihvasc@gmail.com>
2021-08-16 17:02:57 -0300

    Requistos 5 e 6

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function newTask () { 
-    
+const textTask = document.getElementById('texto-tarefa');
+const createTask = document.getElementById('criar-tarefa');
+const taskList = document.getElementById('lista-tarefas');
+
+function addTask() {
+  const task = document.createElement('li');
+  const newTask = textTask.value;
+  task.innerHTML = newTask;
+  taskList.appendChild(task);
+  textTask.value = '';
 }
+
+createTask.addEventListener('click', addTask);
