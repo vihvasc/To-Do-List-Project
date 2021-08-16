cb25193aecbbf39191e9f0f4fde737aa904b09c3
PedroAugmed <pedroaugmed7@gmail.com>
2021-08-16 18:02:13 -0300

    Requisitos 6 e 7

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 
 
-
+ 
 function AddTarefas(){
+  
     let Input = document.createElement("input")
     Input.id = "texto-tarefa"
     let BODY = document.body
@@ -10,24 +11,41 @@ function AddTarefas(){
     button.id = "criar-tarefa"
     BODY.appendChild(button)
     let List = document.getElementById("lista-tarefas")
+    let ListOfLi = document.querySelectorAll(".li")
     let TextOfInput;
-    let ClearInput = ""
+    let newList = []
+    
      button.innerText = "Adiciona Tarefa"
     Input.addEventListener("change", addTarefa)
     function addTarefa(){
       TextOfInput = Input.value
-       console.log(TextOfInput)
+    
      
     }
-    
-    button.addEventListener("dblclick", clickAddTarefa)
+     
+    button.addEventListener("click", clickAddTarefa)
     function clickAddTarefa(){
+        
          let li = document.createElement("li")
+
          li.innerText = TextOfInput
+         li.className = "li"
          List.appendChild(li)
-         document.getElementById("texto-tarefa").value = ""
-         console.log(li)
+         Input.value = ""
+         for(let i = 0;i <=ListOfLi.length; i += 1){
+           newList = ListOfLi[i]
+           console.log(newList)
+           }
+         }
+        
     }
   
-}
+  
+    // Li.addEventListener('keydown', AlternBackground)
+    // function AlternBackground(){
+
+    // }
+ 
+ 
+
 AddTarefas()
