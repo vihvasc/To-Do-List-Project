5bc1740805ac2b4f5de010f6972ff3a7e708bd0a
alvesgf16 <alvesgabriel1994@gmail.com>
2021-08-16 18:47:00 -0300

    Adiciona funcionalidade de salvar lista

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const taskList = document.getElementById('lista-tarefas');
+const taskInput = document.getElementById('texto-tarefa');
+
 function selectTask(event) {
   const currentTask = document.getElementById('selected');
 
@@ -19,22 +22,22 @@ function toggleCompletion(event) {
   }
 }
 
-function createTask() {
-  const taskInput = document.getElementById('texto-tarefa');
-  const taskText = taskInput.value;
+function createTask(taskDescription, taskClass) {
   const newTask = document.createElement('li');
-  const taskList = document.getElementById('lista-tarefas');
 
-  newTask.innerText = taskText;
+  newTask.innerText = taskDescription;
+  newTask.className = taskClass;
   newTask.addEventListener('click', selectTask);
   newTask.addEventListener('dblclick', toggleCompletion);
-  taskList.appendChild(newTask);
 
+  taskList.appendChild(newTask);
+}
+
+function clearInput() {
   taskInput.value = '';
 }
 
 function clearAll() {
-  const taskList = document.getElementById('lista-tarefas');
   while (taskList.firstChild) {
     taskList.removeChild(taskList.firstChild);
   }
@@ -49,13 +52,36 @@ function removeDoneTasks() {
   }
 }
 
+function saveTasks() {
+  localStorage.clear();
+  const tasks = document.getElementsByTagName('li');
+  const tasksArray = [];
+
+  for (let index = 0; index < tasks.length; index += 1) {
+    const taskObject = { text: tasks[index].innerText };
+
+    if (tasks[index].classList.contains('completed')) {
+      taskObject.completed = true;
+    } else {
+      taskObject.completed = false;
+    }
+
+    tasksArray.push(taskObject);
+  }
+
+  localStorage.setItem('taskList', JSON.stringify(tasksArray));
+}
+
 function removeSelectedTask() {
   const selectedTask = document.getElementById('selected');
   selectedTask.remove();
 }
 
 const taskButton = document.getElementById('criar-tarefa');
-taskButton.addEventListener('click', createTask);
+taskButton.addEventListener('click', () => {
+  createTask(taskInput.value);
+  clearInput();
+});
 
 const clearButton = document.getElementById('apaga-tudo');
 clearButton.addEventListener('click', clearAll);
@@ -63,5 +89,18 @@ clearButton.addEventListener('click', clearAll);
 const removeDoneButton = document.getElementById('remover-finalizados');
 removeDoneButton.addEventListener('click', removeDoneTasks);
 
+const saveButton = document.getElementById('salvar-tarefas');
+saveButton.addEventListener('click', saveTasks);
+
 const removeSelectedButton = document.getElementById('remover-selecionado');
 removeSelectedButton.addEventListener('click', removeSelectedTask);
+
+const task = JSON.parse(localStorage.getItem('taskList'));
+
+for (let index = 0; index < task.length; index += 1) {
+  if (task[index].completed) {
+    createTask(task[index].text, 'completed');
+  } else {
+    createTask(task[index].text);
+  }
+}
