24a1618b1a6a791b679c05b99244efda0ccf7b30
José Erivam <erivamdev@gmail.com>
2021-08-16 20:42:18 -0300

    Adicionando requisito 7

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ const buttonAdd = document.querySelector('#criar-tarefa');
 
 const ol = document.querySelector('#lista-tarefas');
 
-buttonAdd.addEventListener('click', (event) => {
+buttonAdd.addEventListener('click', () => {
   const text = document.querySelector('#texto-tarefa'); // pega o input
-  const value = text.value; // pega o valor do inpu
+  const { value } = text; // pega o valor do inpu
 
   const list = document.createElement('li'); // cria uma vareavel com o elemento li
+  list.className = 'lista';
   const listAdd = ol.appendChild(list); // adiciona a vareavel com o elemento li nas ordenadas
   listAdd.innerText = value; // insere o texto na li
-  text.value = '';
+  text.value = ''; // apagando o valor digitado
+});
 
+const selectOl = document.querySelector('ol');
+selectOl.addEventListener('click', (event) => {
+  if (event.target.classList.contains('lista')) {
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  }
 });
