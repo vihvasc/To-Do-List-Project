31a54c0bbc23a992d21f1d57dd3832551dd0c40d
Rafael-Lima-GTR <rafael_sousa13@hotmail.com>
2021-08-16 17:02:26 -0300

    Requisitos 7 e 8 - Altera a cor de fundo do item para cinza e não é possível selecionar mais de um elemento da lista ao mesmo tempo

--- script.js
+++ script.js
@@ -5,6 +5,14 @@ createItem.addEventListener('click', () => {
   const listOrder = document.querySelector('#lista-tarefas');
   const createListItem = document.createElement('li');
   createListItem.innerText = inputEnter.value;
+
+  createListItem.addEventListener('click', (event) => { // Requisito 7 e 8 - Mudar background pra cinza
+    const getClassBackGroundColor = document.querySelector('.backgroundGrey');
+    if (getClassBackGroundColor) {
+      getClassBackGroundColor.classList.remove('backgroundGrey');
+    }
+    event.target.classList.add('backgroundGrey');
+  });
   listOrder.appendChild(createListItem);
   inputEnter.onfocus();
 });
