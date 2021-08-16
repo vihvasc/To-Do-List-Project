5923b3ae16abb4dcca688d54a71483f892e2638e
Antônio Campos <aclcampos@live.com>
2021-08-16 16:32:40 -0300

    Realizado Requisito 7

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let text = document.getElementById('texto-tarefa');
 let button = document.getElementById('criar-tarefa');
-let list = document.getElementById('lista-tarefas');
+let listIndex = document.getElementById('lista-tarefas');
+let list = document.querySelectorAll('li');
+
+
+function pintarLi () {
+  let target = event.target;
+  target.style.backgroundColor = 'rgb(128, 128, 128)'
+}
 
 function addText() {
   const liCreate = document.createElement('li');
-  list.appendChild(liCreate);
+  listIndex.appendChild(liCreate);
   const textPush = document.createTextNode(text.value);
   liCreate.appendChild(textPush);
   text.value = '';
+  liCreate.addEventListener('click', pintarLi);
 }
 
 button.addEventListener('click', addText);
