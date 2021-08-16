6a17f4287b8a647a43affc818312b979299f8a7d
Rafael <rafael.gomesbarretomoura@gmail.com>
2021-08-16 16:24:40 -0300

    adiciona requisito 7

--- script.js
+++ script.js
@@ -5,8 +5,13 @@ let inputTask = document.getElementById('texto-tarefa')
 addTaskBtn.addEventListener('click', addToList)
 
 function addToList() {
-  let li = document.createElement('li')
+  var li = document.createElement('li')
   li.innerHTML = inputTask.value
   taskList.appendChild(li)
   inputTask.value = ''
+  li.addEventListener('click', changeColor)
+}
+
+function changeColor(event) {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)'
 }
