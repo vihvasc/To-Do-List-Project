f9e64db4d6d09d8e8fd691256ddd85fbab71b118
alvesgf16 <alvesgabriel1994@gmail.com>
2021-08-16 14:55:47 -0300

    Remove texto do input ao clicar no botao

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function createTask() {
   const taskInput = document.getElementById('texto-tarefa');
-  let taskText = taskInput.value;
+  const taskText = taskInput.value;
   const newTask = document.createElement('li');
   const taskList = document.getElementById('lista-tarefas');
 
   newTask.innerText = taskText;
   taskList.appendChild(newTask);
-  taskText = '';
+  taskInput.value = '';
 }
 
 const taskButton = document.getElementById('criar-tarefa');
