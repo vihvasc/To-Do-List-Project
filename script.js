675b34aa102c608ebcb1fe9151af2a88e7ed5f76
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-08-16 17:54:36 -0300

    Termina os requisitos obrigatorios

--- script.js
+++ script.js
@@ -12,10 +12,33 @@ let botaoCriarTarefa = document.getElementById("criar-tarefa");
 // a adição de elementos será feita mais de uma vez. Será checado se o item anterior não foi apagado
 botaoCriarTarefa.addEventListener("click", createTask);
 
-function createTask(event){
+function createTask(){
   let newTask = document.createElement("li");
   newTask.innerHTML = input.value;  
-  newTask.setAttribute("id", input.value);
+//   newTask.setAttribute("id", input.value);
+  newTask.addEventListener("click", grayColor);
+  newTask.addEventListener("dblclick", taskDone);
   input.value = "";
   listaOrdenada.appendChild(newTask);
 };
+
+
+function grayColor(event){
+  listaOrdenada = document.getElementById("lista-tarefas").children;
+  for (let i = 0; i < listaOrdenada.length; i += 1){
+    let task = listaOrdenada[i];
+    if (window.getComputedStyle(task).backgroundColor === "rgb(128, 128, 128)"){
+      task.style.backgroundColor = "white";
+    };    
+  };
+  event.target.style.backgroundColor = "rgb(128,128,128)"
+};
+
+function taskDone(event){
+  if(event.target.classList.contains("completed")){
+    event.target.classList.remove("completed");
+  } else {
+    event.target.classList.add("completed");
+  }
+};
+
