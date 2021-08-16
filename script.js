41106c50a5a2f82c327e7147490a503dfa002870
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-08-16 14:57:26 -0300

    add requisito 02 e 03

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
 let header1 = document.getElementById('header')
 header1.innerText = 'Minha Lista de Tarefas';
+let p1 = document.getElementById('funcionamento');
+p1.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
