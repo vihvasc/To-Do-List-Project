8c55f6b01d1ee9b20df319d3582b81770529962f
fernandamichetti <f.michetti@hotmail.com>
2021-08-16 15:53:44 -0300

    Adicionando requisito 10, falta o 7, 8 e 9

--- script.js
+++ script.js
@@ -2,13 +2,25 @@
 const buttonOne = document.getElementById('criar-tarefa');
 const infoInput = document.getElementById('texto-tarefa');
 const listTasks = document.getElementById('lista-tarefas');
-
+const arrTasks = document.getElementsByClassName('taskAdd');
+const clearAll = document.getElementById('apaga-tudo');
 /*Adicionando novas tarefas*/
 buttonOne.addEventListener('click', newTask);
 function newTask() {
   const newTask = infoInput.value;
   let anotherTask = document.createElement('li');
+  anotherTask.className = 'taskAdd';
   anotherTask.innerHTML = newTask;
+  anotherTask.addEventListener('click', changeBackColor);
   listTasks.appendChild(anotherTask);
   infoInput.value = '';
+}
+
+function changeBackColor() {
+  anotherTask.style.changeBackColor = 'pink';
+}
+
+clearAll.addEventListener('click', clearItens);
+function clearItens() {
+  listTasks.innerHTML = '';
 }
