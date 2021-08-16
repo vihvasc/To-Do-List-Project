c9ff7204a11a2d743a8858c3056552d838800f43
anniehau <anniehaurani@gmail.com>
2021-08-16 15:13:23 -0300

    Requisitos 7 e 8 completos.

--- script.js
+++ script.js
@@ -12,9 +12,23 @@ function addTask() {
   if (input.value != '') {
     let newTask = document.createElement('li');
     newTask.innerText = input.value;
+    newTask.addEventListener("click", handleTaskClick);
     taskList.appendChild(newTask);
     input.value = "";
   } else {
     window.alert('Você não digitou nada!')
+  }
+}
+
+function handleTaskClick(event) {
+  let selected = document.querySelectorAll('.selected');
+
+  if (!event.target.classList.contains('selected')) {
+    for (let index = 0; index < selected.length; index += 1) {
+      selected[index].classList.remove('selected')
+    }
+    event.target.classList.add('selected');
+  } else {
+    event.target.classList.remove('selected');
   }
 }
