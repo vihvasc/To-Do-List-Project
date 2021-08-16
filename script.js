904f36ae8f0d5ec7d29853240a379bc4a2f36b08
Marco <marquinco@outlook.com>
2021-08-16 16:02:01 -0300

    adiciona requisito 9

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 let adiciona = document.querySelector('#criar-tarefa')
 adiciona.addEventListener('click', adicionaItem)
+function completa(evento) {
+  let li = evento.target
+  if (li.className !== 'completed') {
+    li.className = 'completed'
+    li.style.textDecoration = 'line-through'
+  } else {
+    li.className = ''
+    li.style.textDecoration = 'none'
+  }
+}
 function mudaCor(evento) {
-  if (document.querySelector('.selecionado')) {
-    antigo = document.querySelector('.selecionado')
-    antigo.className = 'naoSelecionado'
+  if (document.querySelector('#selecionado')) {
+    antigo = document.querySelector('#selecionado')
+    antigo.id = ''
     antigo.style.backgroundColor = ''
   }
   item = evento.target
-  item.className = "selecionado"
+  item.id = 'selecionado'
   item.style.backgroundColor = 'rgb(128,128,128)'
 }
 function adicionaItem() {
@@ -16,6 +26,7 @@ function adicionaItem() {
   let li = document.createElement("li")
   li.innerText = item
   li.addEventListener('click', mudaCor)
+  li.addEventListener('dblclick', completa)
   ol.appendChild(li)
   document.querySelector('#texto-tarefa').value = ''
 }
