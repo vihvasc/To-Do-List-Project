448da498f6b1643917ef77bb6ee3786514dd797c
alxedv <alexmaia.etel@gmail.com>
2021-08-16 15:05:07 -0300

    Adiciona o requisito 7

--- script.js
+++ script.js
@@ -5,8 +5,14 @@ btnCriarTarefa.addEventListener("click", adicionaTarefa)
 function adicionaTarefa(){
     let criarTarefa = document.getElementById("texto-tarefa").value
     let tarefa = document.createElement("li")
+    tarefa.id = "tarefa"
     tarefa.innerText = criarTarefa
     listaTarefa.appendChild(tarefa)
     criarTarefa = document.getElementById("texto-tarefa").value = ""
-    
+    tarefa.addEventListener("click", recebeClick)
+}
+
+function recebeClick(){
+    let tarefa = document.getElementById("tarefa")
+    tarefa.style.backgroundColor = "rgb(128, 128, 128)"
 }
