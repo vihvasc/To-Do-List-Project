cae6352276ddb16e7dc0b581639e757196306f4c
Ge <gmarcoscardoso@gmail.com>
2021-08-16 17:43:30 -0300

    adiciona funcionalidade criar-tarefa

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 // reference Dom
-
-// addEventListener
+const createTask = document.querySelector('#criar-tarefa');
+const textTask = document.querySelector('#texto-tarefa');
+const todoList = document.querySelector('.todo_list');
 
 // function
+function taskGenerator() {
+  // cria li com elemento do input
+  const task = document.createElement('li');
+  task.innerHTML = textTask.value;
+  task.classList.add = 'list';
+  todoList.appendChild(task);
+  // limpa o input
+  textTask.value = '';
+}
+
+// addEventListener
+createTask.addEventListener('click', taskGenerator);
