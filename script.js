3c30bc4b3d5c5ea91f55a9adb28fe4c45c8140a4
Gabriel Jorge <gabrieljorge.ca@gmail.com>
2021-08-16 15:50:52 -0300

    add requisito 05 e 06

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ let header1 = document.getElementById('header')
 header1.innerText = 'Minha Lista de Tarefas';
 let p1 = document.getElementById('funcionamento');
 p1.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+
+let getButton = document.getElementById('criar-tarefa');
+function clearText (){
+  document.querySelector('#texto-tarefa').value = '';
+}
+
+function createTask(){
+  let getInputText = document.querySelector('#texto-tarefa').value;
+  let addLi = document.createElement('li');
+  let getOl = document.getElementById('lista-tarefas')
+  getOl.appendChild(addLi);
+  addLi.innerText = getInputText;
+  console.log(getInputText);
+  
+  clearText ();
+}
+
+getButton.addEventListener('click', createTask);
