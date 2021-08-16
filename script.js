9b703cecd969c5bd218274b1157fd5326a51669e
vinicius rampazzo <vinicius_rampazzo10@hotmail.com>
2021-08-16 17:32:44 -0300

    realizando requisito 7

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const botao = document.querySelector('#criar-tarefa');
 const input = document.querySelector('#texto-tarefa');
 const lista = document.querySelector('#lista-tarefas');
-botao.addEventListener('click', interacaoDoBotao);
+// let filhoDaLista = document.createElement('li');
+const classeDoFilhoDaLista = document.querySelector('.list');
 
+// coloquei o .value, pois ele me da o valor
+// do input, e assim consigo manipular 
+// o que fica dentro dele
+
+botao.addEventListener('click', interacaoDoBotao);
 function interacaoDoBotao() {
 	let inputTexto = input.value;
-	let filhoDaLista = document.createElement('li');
+	filhoDaLista = document.createElement('li');
+	filhoDaLista.classList.add('list')
 	lista.appendChild(filhoDaLista);
 	filhoDaLista.innerText = inputTexto;
-	input.value = ''
+	input.value = '';
+	filhoDaLista.addEventListener('click', mudandoBackground);
+}
+
+
+function mudandoBackground(obj) {
+
+	obj.target.style.backgroundColor = 'rgb(128,128,128)'
+
 }
