ceecf18ce329e10fa6f6031555bc6d87768b27d4
Paulo Flóra <tflora.2004@gmail.com>
2021-08-16 14:44:48 -0300

    Feito o requisito 2.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const header = document.createElement('header');
 header.innerHTML = 'Minha Lista de Tarefas';
 document.body.appendChild(header);
+
+const discreteParagraph = document.createElement('p');
+discreteParagraph.id = 'funcionamento';
+discreteParagraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+document.body.appendChild(discreteParagraph);
