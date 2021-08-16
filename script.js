c7f40dda4278809aee497cf7501899c8dfb7ec07
LaizaBertelli <laizabertelli@hotmail.com>
2021-08-16 18:03:13 -0300

    Adiciona funções para riscar um item da lista

--- script.js
+++ script.js
@@ -31,6 +31,43 @@ function changeColor (){
   selected.style.backgroundColor = "rgb(128, 128, 128)";
  }
 
+ function addStripe(index) {
+  let items = document.querySelectorAll("li");
+  items[index].style.textDecoration = "line-through";
+  items[index].classList.add("completed");
+ }
+
+ function removeStripe(index){
+  let items = document.querySelectorAll("li")
+  items[index].style.textDecoration = "none";
+  items[index].classList.remove("completed");
+
+ }
+ function verifyCompleted(){
+  let items = document.querySelectorAll("li");
+  for(let i = 0; i < items.length; i ++){
+    if(items[i] === event.target){
+      if(items[i].classList.contains("completed")){
+        removeStripe(i);
+        break;
+      } else {
+        addStripe(i);
+        break;
+      }
+    }
+  }
+  //  let items = document.querySelectorAll("li");
+  //  for(let i = 0; items.length; i++){
+  //    if(items[i].classList.contains("completed")){
+  //       removeStripe(i);
+  //       break;
+  //    } else {
+  //      addStripe(i);
+  //      break;
+  //    }
+  //  }
+ }
+
 // função para criar eventos 
 function createEvent(){
   const button = document.getElementById('criar-tarefa');
@@ -49,9 +86,23 @@ function selectElement(){
   changeColor();
 }
 
+function dblTarget(){
+  let items = document.querySelectorAll("li");
+   for( let i = 0; i < items.length; i ++){
+    if(items[i] === event.target){
+      items[i].classList.add("completed");
+    }
+   }
+}
+
 function createListEvents(){
   const listItem = document.querySelectorAll(".item-list");
   for(let i = 0; i < listItem.length; i ++){
     listItem[i].addEventListener('click', selectElement);
   }
+
+  for(let i = 0; i < listItem.length; i ++){
+    listItem[i].addEventListener('dblclick', verifyCompleted);
+  }
 }
+
