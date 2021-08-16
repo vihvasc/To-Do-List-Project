eda0a44ddce2c4dead8787a20edb5927b2c53709
aaguero96 <andre.luiz_1996@hotmail.com>
2021-08-16 15:57:47 -0300

    Add webstorage at localStorage

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 // Const
+const local = localStorage;
 const itemList = document.getElementById('lista-tarefas');
 const buttonAddItem = document.getElementById('criar-tarefa');
 const inputAddItem = document.getElementById('texto-tarefa');
+
+// Add Item to list in localStorage
+function addItemToLocal(item) {
+  const listItemLocal = JSON.parse(local.getItem('listaItems'));
+  listItemLocal.push(item);
+  local.setItem('listaItems', JSON.stringify(listItemLocal));
+}
 
 // Function to add an item to ol
 function addItemToList() {
   const newItemText = inputAddItem.value;
   const newItemElement = document.createElement('li');
+  addItemToLocal(newItemText);
   newItemElement.innerText = newItemText;
   itemList.appendChild(newItemElement);
   inputAddItem.value = '';
@@ -14,3 +23,23 @@ function addItemToList() {
 
 // addEventListener to buttonAddItem
 buttonAddItem.addEventListener('click', addItemToList);
+
+// Fill ol with localStorage Values
+function fillWithLocal() {
+  const listaItemsLocal = JSON.parse(local.getItem('listaItems'));
+  for (let i = 0; i < listaItemsLocal.length; i += 1) {
+    const newItemText = listaItemsLocal[i];
+    const newItemElement = document.createElement('li');
+    newItemElement.innerText = newItemText;
+    itemList.appendChild(newItemElement);
+  }
+}
+
+// Load Page
+window.onload = function startPage() {
+  if (local.getItem('listaItems') === null) {
+    local.setItem('listaItems', JSON.stringify([]));
+  } else {
+    fillWithLocal();
+  }
+}
