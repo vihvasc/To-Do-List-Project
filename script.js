e3a2137917738aeb1ab8fdc3029cb27787b96dde
Paulo Flóra <tflora.2004@gmail.com>
2021-08-16 14:52:04 -0300

    Feitos os requisito 2 e 3.

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 const header = document.createElement('header');
-header.innerHTML = 'Minha Lista de Tarefas';
 document.body.appendChild(header);
 
+const title = document.createElement('h1');
+title.innerText = 'Minha Lista de Tarefas';
+header.appendChild(title);
+
+const main = document.createElement('main');
+document.body.appendChild(main);
 const discreteParagraph = document.createElement('p');
 discreteParagraph.id = 'funcionamento';
 discreteParagraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
-document.body.appendChild(discreteParagraph);
+main.appendChild(discreteParagraph);
+
+const inputDiv = document.createElement('div');
+document.body.appendChild(inputDiv);
+
+const input = document.createElement('input');
+input.id = 'texto-tarefa';
+document.body.appendChild(input);
