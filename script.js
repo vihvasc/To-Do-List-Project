2457726573271595f0fa2508d1edb4a843c65555
Walace <walacewab@gmail.com>
2021-08-16 16:36:04 -0300

    Adicionando o 7 requisito

--- script.js
+++ script.js
@@ -6,7 +6,14 @@ const mainList = document.getElementById("lista-tarefas");
 function createTask (event){
     let taskList = document.createElement("li");
     taskList.innerHTML = textAssignment.value;
+    taskList.addEventListener("click", clickBackground)
     mainList.appendChild(taskList)
     textAssignment.value = "";
 }
 buttonAssignment.addEventListener("click", createTask)
+
+function clickBackground (event){
+    let colorBackground = "rgb(128, 128, 128)";
+    let listTarget = event.target;
+    listTarget.style.background = colorBackground;
+}
