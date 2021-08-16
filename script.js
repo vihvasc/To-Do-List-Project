a5c34bc761fb7bbd898bb52c3ee61cfa0d35a560
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2021-08-16 20:01:50 -0300

    adiciona requisitos 7 e 9

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const input = document.querySelector('#texto-tarefa');
 const buttonAddTask = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
-const listItem = document.getElementsByClassName('list-item');
+const item = document.querySelectorAll('.list-item');
 
 function addTask() {
   buttonAddTask.addEventListener('click', () => {
@@ -9,12 +9,23 @@ function addTask() {
       window.alert('Você precisa escrever algo');
     } else {
       const listItem = document.createElement('li');
-     taskList.appendChild(listItem);
+      taskList.appendChild(listItem);
       listItem.innerHTML = input.value;
       listItem.className = 'list-item';
-    }input.value = '';
 
+      listItem.addEventListener('click', (event) => {
+        event.target.classList.add('selected');
+        event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+      })
+
+      listItem.addEventListener('dblclick', (event) => {
+      event.target.classList.add('completed')
+      })
+    }input.value = '';
   });
 }
 addTask();
 
+function lineThrought() {
+
+}
