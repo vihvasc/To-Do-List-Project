28e00856e379e5d780a61f810a648bdc6341f958
Daniel Dias <danieldias.b99@hotmail.com>
2021-08-16 14:57:46 -0300

    Inicia requisito02 e corrige lint.

--- script.js
+++ script.js
@@ -4,9 +4,18 @@
 // ***** Será verificado se sua página possui uma tag header com o conteúdo Minha Lista de Tarefas
 
 function adicionaTitulo() {
-    const elementoHeader = document.createElement('header');
-    elementoHeader.textContent = 'Minha Lista de Tarefas';
-    elementoHeader.id = 'titulo-header';
-    document.body.appendChild(elementoHeader);
+  const elementoHeader = document.createElement('header');
+  elementoHeader.textContent = 'Minha Lista de Tarefas';
+  elementoHeader.id = 'titulo-header';
+  document.body.appendChild(elementoHeader);
 }
 adicionaTitulo();
+
+// 02 - Adicione abaixo do título um pequeno e discreto parágrafo com id="funcionamento" e com o texto "Clique duas vezes em um item para marcá-lo como completo".
+// *** O que será verificado:
+// ***** Será verificado que existe na sua página um elemento com o id funcionamento com o conteúdo Clique duas vezes em um item para marcá-lo como completo
+
+function adicionaParagrafo() {
+  
+}
+adicionaParagrafo();
