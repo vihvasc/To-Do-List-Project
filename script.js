d20f6c6523c16778495e58e46e913b1c88dce8d8
matheusg18 <matheusgabriel.18@gmail.com>
2021-08-16 15:43:40 -0300

    Requisito 8 feito

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-const taskListItemClass = 'tarefa';
+const TASK_LIST_ITEM_CLASS = 'tarefa';
+let selectedTask;
 
 function addTask() {
   const taskInput = document.getElementById('texto-tarefa');
   const taskList = document.getElementById('lista-tarefas');
   const newTask = document.createElement('li');
 
-  newTask.classList.add(taskListItemClass);
+  newTask.classList.add(TASK_LIST_ITEM_CLASS);
   newTask.innerText = taskInput.value;
   taskList.appendChild(newTask);
   taskInput.value = '';
 }
 
 function handleTaskListItemClick(event) {
-  if (event.target.classList.contains(taskListItemClass)) {
+  if (event.target.classList.contains(TASK_LIST_ITEM_CLASS)) {
     const task = event.target;
+
+    if (typeof selectedTask !== 'undefined') {
+      selectedTask.style.backgroundColor = '';
+    }
     task.style.backgroundColor = 'rgb(128, 128, 128)';
+    selectedTask = task;
   }
 }
 
