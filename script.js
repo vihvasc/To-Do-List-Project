445971dd9948f9df5fda2d61a16ea87882110586
Matheus Pessoa <matheuspessoafonseca@gmail.com>
2021-08-16 18:32:46 -0300

    Adiciona requisito obrigatorio 7

--- script.js
+++ script.js
@@ -4,10 +4,19 @@ let taskList = document.getElementById('lista-tarefas');
 
 function handleAddTask() {
   let li = document.createElement('li');
+  li.classList.add('list-item');
   li.textContent = input.value;
+  li.addEventListener('click', handleClickItem);
   taskList.appendChild(li);
 
   input.value = '';
 }
 
 createTaskBtn.addEventListener('click', handleAddTask);
+
+function handleClickItem(event) {
+  for (item of taskList.children) {
+    event.target.style.background = 'rgb(128, 128, 128)';
+    console.log(item);
+  }
+}
