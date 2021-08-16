52a41dab87bb133db1fd079acc9ef1520b4ff43f
jpcp0614 <jpcp0614@gmail.com>
2021-08-16 16:54:02 -0300

    fix: alert for empty field

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ const theList = document.querySelector('#lista-tarefas');
 
 // Created function for addItem and order
 function addItemList() {
+  if (inputValue.value === null || inputValue.value === '') { // ref.: https://pt.stackoverflow.com/questions/39139/como-impedir-o-submit-de-um-formul%C3%A1rio-vazio
+    alert('Digite uma tarefa no campo abaixo!');
+    return false;
+  }
   const creatLiItem = document.createElement('li');
   creatLiItem.innerText = inputValue.value;
   creatLiItem.classList.add('li-list');
