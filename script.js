4f68df65c4609b7d608c1b2039a5b364f46067ed
henrique <hickm018@gmail.com>
2021-08-16 16:42:51 -0300

    Adiciona inicio requisito 9

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ function requisito5e6() {
     const li = document.createElement('li');
     const paiLi = document.getElementById('lista-tarefas');
     paiLi.appendChild(li);
-    li.className = 'colors';
     let input = document.getElementById('texto-tarefa');
     let input2 = input.value;
     li.innerText = input2;
     input.value = '';
-    requisito7();
+    requisito7e8();
+    requisito9();
   }
 }
 
-function requisito7() {
-  const listasLi = document.querySelectorAll('.colors');
+function requisito7e8() {
+  const listasLi = document.getElementsByTagName('li');
   for (let key of listasLi) {
     key.addEventListener('click', changeColor);
   }
@@ -29,5 +29,22 @@ function requisito7() {
     }
     const evento = event.target;
     evento.style.backgroundColor = 'gray';
+  }
+}
+
+function requisito9() {
+  const listasLi = document.getElementsByTagName('li');
+  console.log(listasLi);
+  for (let key of listasLi) {
+    key.addEventListener('dblclick', changeTextDecoration);
+  }
+
+  function changeTextDecoration(event) {
+    console.log();
+    for (let key of listasLi) {
+      key.classList.remove('completed');
+    }
+    const evento = event.target;
+    evento.classList = 'completed';
   }
 }
