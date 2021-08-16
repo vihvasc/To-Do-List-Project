cada5607bba160905a1a4518a1c671e7d3949d12
douglas <douglasmonteiro.m@gmail.com>
2021-08-16 19:01:52 -0300

    Adiciona requisito 7 e 8

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let botao = document.getElementById('criar-tarefa');
 let entrada = document.getElementById('texto-tarefa');
+let itemLista = document.getElementsByTagName('li');
+
 botao.addEventListener('click', addTarefa);
 
 
@@ -9,4 +11,22 @@ function addTarefa (){
     elemento.innerText = entrada.value;
     entrada.value = '';
     lista.appendChild(elemento);
+    addEventoItemLista();
+}
+
+function addEventoItemLista (){
+    for (let index = 0; index < itemLista.length; index++) {
+        itemLista[index].addEventListener('click', mudaFundo);
+    }
+}
+
+function mudaFundo(item){
+    let itemList = document.getElementsByTagName('li');
+    for (let index = 0; index < itemList.length; index++) {
+        console.log(itemList[index]);
+        if(itemList[index].style.backgroundColor){
+            itemList[index].style.backgroundColor = '';
+        }
+    }
+    item.target.style.backgroundColor = 'rgb(128,128,128)';
 }
