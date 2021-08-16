60e4b4dbe98a4806fca64cc6c60b304f088ab061
Antonio Souza <antoniofelipe2001@hotmail.com>
2021-08-16 18:47:53 -0300

    1° ajuste de Eslint

--- script.js
+++ script.js
@@ -3,95 +3,95 @@
 function addEventListenerToArray(array, eventType, func) {
   for (let element of array) {
     element.addEventListener(eventType, func);
-  };
+  }
 };
 
 function removeInputText(input) {
-  input.value = "";
-}
+  input.value = '';
+};
 
 function toggleClass(element, className) {
   element.classList.toggle(className);
-}
+};
 
 // 7 & 8
 
 function changeListItemBackgroundColor(event) {
   let selectedListItem = event.target;
-  let listItens = document.getElementsByClassName("list-item");
+  let listItens = document.getElementsByClassName('list-item');
   for (let item of listItens) {
     if (item === selectedListItem) {
-      if (!item.classList.contains("selected-item")) {
-        item.classList.add("selected-item");
-      };
-    } else if (item.classList.contains("selected-item")) {
-      toggleClass(item, "selected-item");
-    }; 
-  };
+      if (!item.classList.contains('selected-item')) {
+        item.classList.add('selected-item');
+      }
+    } else if (item.classList.contains('selected-item')) {
+      toggleClass(item, 'selected-item');
+    }
+  }
 };
 
 // 9
 
 function completeTask(event) {
   let task = event.target;
-  toggleClass(task, "completed");
+  toggleClass(task, 'completed');
 };
 
 // 5
 
 function addTaskToList() {
-  let input = document.getElementById("texto-tarefa");
-  let taskList = document.getElementById("lista-tarefas");
+  let input = document.getElementById('texto-tarefa');
+  let taskList = document.getElementById('lista-tarefas');
   let task = input.value;
-  let listItem = document.createElement("li");
+  let listItem = document.createElement('li');
   listItem.innerText = task;
-  listItem.className = "list-item";
+  listItem.className = 'list-item';
   taskList.appendChild(listItem);
-  listItem.addEventListener("click", changeListItemBackgroundColor);
-  listItem.addEventListener("dblclick", completeTask);
+  listItem.addEventListener('click', changeListItemBackgroundColor);
+  listItem.addEventListener('dblclick', completeTask);
   removeInputText(input);
 };
 
 //10
 
 function wipeAllTasks() {
-  let tasksList = document.getElementById("lista-tarefas");
-  tasksList.innerHTML = "";
+  let tasksList = document.getElementById('lista-tarefas');
+  tasksList.innerHTML = '';
 };
 
 // 11
 
 function wipeCompletedTasks() {
-  let completedTasks = document.getElementsByClassName("completed");
+  let completedTasks = document.getElementsByClassName('completed');
   let completedLength = completedTasks.length;
   for (let index = 0; index < completedLength; index += 1) {
     completedTasks[0].remove();
-  };
+  }
 };
 
 //12 
 
 function saveTasks() {
-  let tasksList = document.getElementById("lista-tarefas");
+  let tasksList = document.getElementById('lista-tarefas');
   let tasksToSave = tasksList.innerHTML;
-  localStorage.setItem("listItens", tasksToSave);
-}
+  localStorage.setItem('listItens', tasksToSave);
+};
 
 function loadSavedTasks() {
-  let tasksList = document.getElementById("lista-tarefas");
-  let savedTasks = localStorage.getItem("listItens");
+  let tasksList = document.getElementById('lista-tarefas');
+  let savedTasks = localStorage.getItem('listItens');
   if (savedTasks) {
     tasksList.innerHTML = savedTasks;
-    let tasksItens = document.getElementsByClassName("list-item");
+    let tasksItens = document.getElementsByClassName('list-item');
     for (let item of tasksItens) {
-      item.addEventListener("click", changeListItemBackgroundColor);
-      item.addEventListener("dblclick", completeTask);
-    };
-  };
+      item.addEventListener('click', changeListItemBackgroundColor);
+      item.addEventListener('dblclick', completeTask);
+    }
+  }
 };
 
 function moveTaskUp() {
-  let selected = document.querySelector(".selected-item");
+  let selected = document.querySelector('.selected-item');
   if (selected) {
     let previousTask = selected.previousElementSibling;
     if (previousTask) {
@@ -99,14 +99,14 @@ function moveTaskUp() {
       let previousTaskHtml = previousTask.innerHTML;
       selected.innerHTML = previousTaskHtml;
       previousTask.innerHTML = selectedHtml;
-      toggleClass(selected, "selected-item");
-      toggleClass(previousTask, "selected-item");
-    };
-  };
+      toggleClass(selected, 'selected-item');
+      toggleClass(previousTask, 'selected-item');
+    }
+  }
 };
 
 function moveTaskDown() {
-  let selected = document.querySelector(".selected-item");
+  let selected = document.querySelector('.selected-item');
   if (selected) {
     let nextTask = selected.nextElementSibling;
     if (nextTask) {
@@ -114,34 +114,34 @@ function moveTaskDown() {
       let nextTaskHtml = nextTask.innerHTML;
       selected.innerHTML = nextTaskHtml;
       nextTask.innerHTML = selectedHtml;
-      toggleClass(selected, "selected-item");
-      toggleClass(nextTask, "selected-item");
-    };
-  };
+      toggleClass(selected, 'selected-item');
+      toggleClass(nextTask, 'selected-item');
+    }
+  }
 };
 
 function removeSelectedTask() {
-  let task = document.querySelector(".selected-item");
+  let task = document.querySelector('.selected-item');
   if (task) {
     task.remove();
-  };
+  }
 };
 
 function buttonsStart() {
-  let taskButton = document.getElementById("criar-tarefa");
-  taskButton.addEventListener("click", addTaskToList);
-  let wipeAllButton = document.getElementById("apaga-tudo");
-  wipeAllButton.addEventListener("click", wipeAllTasks)
-  let wipeCompletedTasksButton = document.getElementById("remover-finalizados");
-  wipeCompletedTasksButton.addEventListener("click", wipeCompletedTasks)
-  let saveTasksButton = document.getElementById("salvar-tarefas");
-  saveTasksButton.addEventListener("click", saveTasks);
-  let moveTaskUpButton = document.getElementById("mover-cima");
-  moveTaskUpButton.addEventListener("click", moveTaskUp);
-  let moveTaskDownButton = document.getElementById("mover-baixo");
-  moveTaskDownButton.addEventListener("click", moveTaskDown);
-  let removeSelectedButton = document.getElementById("remover-selecionado");
-  removeSelectedButton.addEventListener("click", removeSelectedTask)
+  let taskButton = document.getElementById('criar-tarefa');
+  taskButton.addEventListener('click', addTaskToList);
+  let wipeAllButton = document.getElementById('apaga-tudo');
+  wipeAllButton.addEventListener('click', wipeAllTasks)
+  let wipeCompletedTasksButton = document.getElementById('remover-finalizados');
+  wipeCompletedTasksButton.addEventListener('click', wipeCompletedTasks)
+  let saveTasksButton = document.getElementById('salvar-tarefas');
+  saveTasksButton.addEventListener('click', saveTasks);
+  let moveTaskUpButton = document.getElementById('mover-cima');
+  moveTaskUpButton.addEventListener('click', moveTaskUp);
+  let moveTaskDownButton = document.getElementById('mover-baixo');
+  moveTaskDownButton.addEventListener('click', moveTaskDown);
+  let removeSelectedButton = document.getElementById('remover-selecionado');
+  removeSelectedButton.addEventListener('click', removeSelectedTask)
 };
 
 window.onload = function () {
