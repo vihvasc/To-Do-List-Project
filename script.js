d359d2c54d2171eb3903e5f0633902fcc972eec5
Artur Lemes Moretti <arturl.moretti@gmail.com>
2021-08-16 15:14:11 -0300

    Adiciona requisitos 5 e 6

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 function criarTarefa() {
-  const tarefa = document.createElement('li');
-  const textoTarefa = document.getElementById('texto-tarefa');
-  tarefa.innerText = textoTarefa.innerText;
-  textoTarefa.innerText = '';
-  console.log(textoTarefa.innerText);
+  const tarefaLista = document.createElement('li');
+  const textoTarefaInput = document.getElementById('texto-tarefa');
+  const listaTarefas = document.getElementById('lista-tarefas');
+  console.log(textoTarefaInput.innerText);
+  tarefaLista.innerText = textoTarefaInput.value;
+  listaTarefas.appendChild(tarefaLista);
+  textoTarefaInput.value = '';
 }
 
 const cliqueBotaoCriarTarefa = document.getElementById('criar-tarefa');
