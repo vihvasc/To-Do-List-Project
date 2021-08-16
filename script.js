9efaa30b2c671efaaff65262e1d647959a99b452
Matheus <matheusfelippeh@gmail.com>
2021-08-16 15:07:55 -0300

    add requisite 7

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ function addTask(){
     let textInput = document.getElementById('texto-tarefa').value 
     let newTask = document.createElement('li')
     newTask.innerText = textInput
+    newTask.addEventListener('click',changeColor)
     taskList.appendChild(newTask)
     document.getElementById('texto-tarefa').value = ''
+}
+
+function changeColor(event){
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+    console.log('oi')
 }
