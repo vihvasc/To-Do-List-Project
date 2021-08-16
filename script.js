a917d40c632e6ceed0da018af24e17ff194f2442
Gustavo Gracio <gustavogracio@hotmail.com>
2021-08-16 15:10:10 -0300

    Requisto 8 completo

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+function cleanBackgrood() {
+  const li = document.querySelectorAll('li');
+  for (let key = 0; key < li.length; key += 1) {
+    li[key].style.backgroundColor = 'white';
+  }
+}
+
 function listColor() {
   const li = document.querySelectorAll('li');
   for (let key = 0; key < li.length; key += 1) {
     li[key].addEventListener('click', () => {
+      cleanBackgrood();
       li[key].style.backgroundColor = 'rgb(128, 128, 128)';
     });
   }
