625b820daf8c435aafdd7e0b513a7f457039bb7b
Lucas Peres <lucasperesss@gmail.com>
2021-08-16 17:45:49 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-// let tarefas = [];
+function selecionar(task) {
+  if (document.querySelector('.selected')) {
+    document.querySelector('.selected').classList.remove('selected');
+  }
+  task.target.classList.add('selected');
+}
 
 function criarTarefa() {
   const input = document.getElementById('texto-tarefa');
   const tarefa = document.createElement('li');
   tarefa.innerText = input.value;
+  tarefa.addEventListener('click', selecionar);
   document.getElementById('lista-tarefas').appendChild(tarefa);
   input.value = '';
 }
