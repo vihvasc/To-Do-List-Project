6eef611d16f21c5d173aec3318fb1d3a2ba4becb
Guilherme Saboia <gsaboia@yahoo.com>
2021-08-16 15:15:27 -0300

    Adiciona requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const inputTask = document.getElementById('texto-tarefa');
 const createTask = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
+const body = document.querySelector('body');
 
+// adiciona tarefa à lista
 function addTask() {
   const inputTaskValue = inputTask.value;
   const newTask = document.createElement('li');
@@ -10,3 +12,23 @@ function addTask() {
   inputTask.value = '';
 }
 createTask.addEventListener('click', addTask);
+
+// seleciona a tarefa
+function addSelected(element) {
+  const tasks = Array.from(taskList.children);
+  for (let counter = 0; counter < tasks.length; counter += 1) {
+    const currentTask = tasks[counter];
+    if (currentTask.classList.contains('selected')) {
+      currentTask.classList.remove('selected');
+    }
+  }
+  element.classList.add('selected');
+}
+// verifica se o elemento é um list item
+function checkItem(originEvent) {
+  const element = originEvent.target;
+  if (element.parentNode.id === 'lista-tarefas') {
+    addSelected(element);
+  }
+}
+body.addEventListener('click', checkItem);
