2930345241b7c86abd3dbe5c78a0b545377565dd
1lucascq <1lucascq@gmail.com>
2021-08-17 19:29:26 -0300

    Requisitos 8 a 11

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const button = document.querySelector('#criar-tarefa');
-const taskList = document.getElementById('lista-tarefas');
+const ol = document.getElementById('lista-tarefas');
 
-function selectTask(e) {
+function addSelectedClass(e) {
   const tasks = document.getElementsByClassName('tasks');
   for (let i = 0; i < tasks.length; i += 1) {
     if (tasks[i].classList.contains('selected')) {
@@ -11,14 +11,75 @@ function selectTask(e) {
   e.target.classList.toggle('selected');
 }
 
+function addCompletedClass(e) {
+  e.target.classList.toggle('completed');
+}
+
 function addTask() {
   const inputText = document.getElementById('texto-tarefa');
   const text = inputText.value;
   const newTask = document.createElement('li');
   newTask.innerText = text;
   newTask.className = 'tasks';
-  taskList.appendChild(newTask);
+  ol.appendChild(newTask);
   inputText.value = '';
-  newTask.addEventListener('click', selectTask);
+  newTask.addEventListener('click', addSelectedClass);
+  newTask.addEventListener('dblclick', addCompletedClass);
 }
 button.addEventListener('click', addTask);
+
+function clearAll() {
+  const tasks = document.getElementsByClassName('tasks');
+  for (let i = 0; tasks.length >= 1; i += 1) {
+    ol.removeChild(tasks[0]);
+  }
+}
+const btnClear = document.getElementById('apaga-tudo');
+btnClear.addEventListener('click', clearAll);
+
+function clearCompleted() {
+  const completedTask = document.getElementsByClassName('completed');
+  for (let i = 0; completedTask.length >= 1; i += 1) {
+    ol.removeChild(completedTask[0]);
+  }
+}
+const btnClearCompleted = document.getElementById('remover-finalizados');
+btnClearCompleted.addEventListener('click', clearCompleted);
+
+function clearSelected() {
+  const clearSelectedClass = document.getElementsByClassName('selected');
+  for (let i = 0; clearSelectedClass.length >= 1; i += 1) {
+    ol.removeChild(clearSelectedClass[0]);
+  }
+}
+const btnClearSelected = document.getElementById('remover-selecionado');
+btnClearSelected.addEventListener('click', clearSelected);
+
+function updateLocalStorage() {
+  const oldTasks = JSON.parse(localStorage.getItem('allTasks'));
+  const tasksLists = document.getElementById('lista-tarefas');
+  for (let i = 0; i < tasksLists.length; i += 1) {
+    const taskItem = document.createElement('li');
+    taskItem.innerHTML = tasksLists[i].innerHTML;
+    taskItem.classList = tasksLists[i].classList;
+    oldTasks.push(taskItem);
+  }
+  localStorage.setItem('allTasks', JSON.stringify(oldTasks));
+}
+
+window.onload = () => {
+  if (localStorage.getItem('allTasks') === null) {
+    localStorage.setItem('allTasks', JSON.stringify([]));
+  } else {
+    const tasksList = JSON.parse(localStorage.getItem('allTasks'));
+    for (let i = 0; i < tasksList.length; i += 1) {
+      const newTask = document.createElement('li');
+      newTask.innerText = tasksList[i].text;
+      newTask.classList = tasksList[i].classList;
+      ol.appendChild(newTask);
+    }
+  }
+};
+
+const btnSave = document.getElementById('salvar-tarefas');
+btnSave.addEventListener('click', updateLocalStorage);
