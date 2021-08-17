724d617e782b65b26976ac5a1274d342f5d527b5
Deivid <deividmborges@gmail.com>
2021-08-17 16:13:40 -0300

    adiciona requisito 12

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function criaListaTarefas() {
   geraItemList();
   buttonDeleteAll();
   buttonDeleteSelectedItems();
+  buttonSaveList();
+  getSavedList();
 }
 
 let list = document.getElementById('lista-tarefas');
@@ -43,8 +45,7 @@ function buttonDeleteAll() {
   buttonDeleteAllItens.addEventListener('click', deleteAllLis);
 }
 function deleteAllLis() {
-  myList = document.getElementById('lista-tarefas');
-  myList.remove(myList.lastChild);
+  list.remove(list.lastChild);
 }
 
 function buttonDeleteSelectedItems() {
@@ -56,4 +57,25 @@ function deleteSelectedItems() {
   for (let index = 0; index < mySelectedItems.length; index += 1) {
     mySelectedItems[index].remove();
   }
+}
+function buttonSaveList() {
+  let buttonSave = document.getElementById('salvar-tarefas');
+  buttonSave.addEventListener('click', saveLocal);
+}
+function saveLocal() {
+  let arraySavedList = [];
+  let taskList = Array.from(list.children);
+  for (let index = 0; index < taskList.length; index += 1) {
+    arraySavedList.push(taskList[index].innerText);
+  }
+  localStorage.setItem('myList', JSON.stringify(arraySavedList));
+}
+function getSavedList() {
+  let localList = JSON.parse(localStorage.getItem('myList'));
+  localList.forEach(function(liText){
+    let itemSavedList = document.createElement('li');
+    itemSavedList.innerText = liText; 
+    list.appendChild(itemSavedList);
+    console.log(liText);
+  })
 }
