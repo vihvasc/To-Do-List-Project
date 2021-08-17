427771a2ca9cb06c0d3b8272ee3e32f36e95bf37
israel jeronimo <silvaij16@gmail.com>
2021-08-17 15:16:54 -0300

    8 - Não deve ser possível selecionar mais de um elemento da lista ao mesmo tempo

--- script.js
+++ script.js
@@ -6,11 +6,18 @@ function toDoList() {
   const li = document.createElement('li');
 
   li.innerText = input.value;
-  li.addEventListener('click', function (event) {
+  li.addEventListener('click', function (event) {       
+    
+    const classe = document.querySelector('.selected');
+    if (classe) { 
+      classe.classList.remove('selected');
+      classe.style.backgroundColor = 'white'
+    }
+
     event.target.style.backgroundColor = 'rgb(128,128,128)';
+    event.target.classList.add('selected');
   });
   listaTarefas.appendChild(li);
   input.value = '';
 }
-
 butao.addEventListener('click', toDoList);
