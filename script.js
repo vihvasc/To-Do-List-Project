f94ab0e8143ddc2eda04d158d7284ecbc7dead65
Lucas <theluks21@live.com>
2021-08-16 21:24:49 -0300

    Adiciona o setimo, oitavo e nono requisitos

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-window.onload = function () {
+window.onload = function init () {
   const button = document.getElementById('criar-tarefa');
-
+  
   button.addEventListener('click', handleCreateList);
 };
 
@@ -11,4 +11,31 @@ function handleCreateList() {
   tarefa.innerText = input.value;
   lista.appendChild(tarefa);
   input.value = '';
+  handleColorChange();
+};
+
+function handleColorChange(event) {
+  const getList = document.getElementsByTagName('li');
+  
+  for (let tarefa of getList) {
+    tarefa.addEventListener('click', function color(event) {
+      if (document.querySelector('.selected') === null) {
+        event.target.className += ' selected';
+      } else {
+        const colorCheck = document.querySelector('.selected');
+        colorCheck.classList.remove('selected');
+        event.target.className += ' selected';
+      };
+    });
+    
+    tarefa.addEventListener('dblclick', handleTaskCheck);
+
+  };
+};
+
+function handleTaskCheck(event) {
+  event.target.className += ' completed';
+  event.target.addEventListener('dblclick', function resetLine(event) {
+    event.target.classList.remove('completed');
+  });
 }
