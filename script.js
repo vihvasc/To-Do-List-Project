68b482e4fb6ba509f0b1c80c28132bf03a3c496d
Marina Marques <marina.marqueso@outlook.com>
2021-08-17 15:51:48 -0300

    Adiciona code para listamento de tarefas

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log(document.querySelector('#texto-tarefa').value);
+let input = document.getElementById('texto-tarefa');
+let buttonTask = document.getElementById('criar-tarefa');
+let listTask = document.getElementById('lista-tarefas');
+
+buttonTask.addEventListener('click', createListTask);
+// target: botao
+// evento: clicar no botao, adiciona a task do input na lista
+function createListTask(event) {
+    let liTaks = document.createElement('li');
+    liTaks.innerText = input.value;
+    listTask.appendChild(liTaks);
+    event.target.buttonTask;
+    input.value = "";
+}
+
