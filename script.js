0de5773c080a2696bafc1b107de805592ee80444
Rodrigo Lima <rodrigootavio19@gmail.com>
2021-08-16 23:10:37 -0400

    corrige eslint e stylelint

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const takeBtnClearDone = document.getElementById('remover-finalizados');
 const takeBtnSaveTasks = document.getElementById('salvar-tarefas');
 const takeBtnMoveUp = document.getElementById('mover-cima');
 const takeBtnMoveDown = document.getElementById('mover-baixo');
-const takeBtnDelete = document.getElementById('remover-selecionado')
+const takeBtnDelete = document.getElementById('remover-selecionado');
 
 const input = document.getElementById('texto-tarefa');
 
@@ -25,6 +25,7 @@ function removeAllClass(classe) {
   const currentList = pegaListaAtual();
   for (let i = 0; i < currentList.length; i += 1) {
     currentList[i].classList.remove(classe);
+    currentList[i].style.backgroundColor = '';
   }
 }
 
@@ -36,6 +37,7 @@ function changeColor(evento) {
   } else {
     removeAllClass('selected');
     backColor.classList.add('selected');
+    backColor.style.backgroundColor = 'rgb(128, 128, 128)';
   }
 }
 
@@ -175,7 +177,7 @@ function afterLoad() {
   takeBtnSaveTasks.addEventListener('click', saveTasks);
   takeBtnMoveUp.addEventListener('click', moveItemUp);
   takeBtnMoveDown.addEventListener('click', moveItemDown);
-  takeBtnDelete.addEventListener('click', deleteSelected)
+  takeBtnDelete.addEventListener('click', deleteSelected);
 }
 
 window.onload = afterLoad;
