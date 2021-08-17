b110cd70a2344723e028b85e66d219e6680b6b3f
henrique <hickm018@gmail.com>
2021-08-17 16:03:30 -0300

    Adiciona requisito 11, corrige requisito 9, e refatora estrutura do código

--- script.js
+++ script.js
@@ -1,71 +1,65 @@
-requisito5e6();
+const button = document.getElementById('criar-tarefa');
+// let listasLi = document.getElementsByTagName('li');
+const buttonClear = document.getElementById('apaga-tudo');
 
-function requisito5e6() {
-  const button = document.getElementById('criar-tarefa');
-  button.addEventListener('click', getInputAddLi);
-
-  function getInputAddLi() {
-    const li = document.createElement('li');
-    const paiLi = document.getElementById('lista-tarefas');
-    paiLi.appendChild(li);
-    let input = document.getElementById('texto-tarefa');
-    let input2 = input.value;
-    li.innerText = input2;
-    input.value = '';
-    requisito7e8();
-    requisito9();
-    requisito10();
-  }
-}
-
-function requisito7e8() {
-  const listasLi = document.getElementsByTagName('li');
+function addEventFor() {
+  let listasLi = document.getElementsByTagName('li');
   for (let key of listasLi) {
     key.addEventListener('click', changeColor);
   }
-
-  function changeColor(event) {
-    for (let key of listasLi) {
-      key.style.backgroundColor = 'white';
-    }
-    const evento = event.target;
-    evento.style.backgroundColor = 'gray';
-  }
-}
-
-function requisito9() {
-  const listasLi = document.getElementsByTagName('li');
   for (let key of listasLi) {
     key.addEventListener('dblclick', changeTextDecoration);
   }
-
-  function changeTextDecoration(event) {
-    const evento = event.target;
-    const classeEvento = evento.className;
-    if (classeEvento === 'completed') {
-      for (let key of listasLi) {
-        key.classList.remove('completed');
-      }
-    }
-    if (evento.className !== 'completed') {
-      evento.classList = 'completed';
-    }
-  }
-
-  //   function changeTextDecoration2(event) {
-  //     const evento = event.target;
-  //     evento.classList.remove('completed');
 }
-function requisito10() {
-  let buttonClear = document.getElementById('apaga-tudo');
-  console.log(buttonClear);
-  buttonClear.addEventListener('click', clearList);
 
-  function clearList() {
-    const paiLi = document.getElementById('lista-tarefas');
-    const listasLi = document.getElementsByTagName('li');
-    for (let key of listasLi) {
-      paiLi.removeChild(key);
-    }
+function getInputAddLi() {
+  const li = document.createElement('li');
+  const paiLi = document.getElementById('lista-tarefas');
+  const input = document.getElementById('texto-tarefa');
+  li.innerText = input.value;
+  paiLi.appendChild(li);
+  input.value = '';
+  addEventFor();
+}
+
+function changeColor(event) {
+  let listasLi = document.getElementsByTagName('li');
+  for (let key of listasLi) {
+    key.style.backgroundColor = 'white';
+  }
+  const evento = event.target;
+  evento.style.backgroundColor = 'gray';
+}
+
+function changeTextDecoration(event) {
+  const evento = event.target;
+  console.log(evento.className);
+  const classEvent = evento.clearName;
+  if (classEvent === 'completed') {
+    evento.classList.removeAtribute('completed');
+  } else {
+    evento.classList.toggle('completed');
+  }
+}
+
+function clearList() {
+  let listaLi = document.querySelectorAll('li');
+  const paiLi = document.querySelector('#lista-tarefas');
+  for (let key of listaLi) {
+    paiLi.removeChild(key);
+  }
+}
+
+buttonClear.addEventListener('click', clearList);
+button.addEventListener('click', getInputAddLi);
+
+const buttonClearFinal = document.getElementById('remover-finalizados');
+buttonClearFinal.addEventListener('click', removeItems);
+
+function removeItems() {
+  const selected = document.querySelectorAll('.completed');
+  console.log(selected);
+  for (let key of selected) {
+    key.remove();
   }
 }
