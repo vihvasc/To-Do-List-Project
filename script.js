3b10dcabf8207e44f5bc9829b0534ced39cbd09c
Rodolpho Coller <rodolpho.coller12@gmail.com>
2021-08-17 09:03:27 -0300

    adiciona requisito 10

--- script.js
+++ script.js
@@ -1,45 +1,53 @@
-let button = document.getElementById('criar-tarefa');
+const button = document.getElementById('criar-tarefa');
+const ol = document.getElementById('lista-tarefas');
+const input = document.getElementById('texto-tarefa');
 
-button.addEventListener('click', addTask);
-
-function addTask() {
-  let ol = document.getElementById('lista-tarefas');
-  let input = document.getElementById('texto-tarefa');
-  let li = document.createElement('li');
-  ol.appendChild(li).innerText = input.value;
-  input.value = '';
-  addListener()
-}
-
-let li = document.getElementsByTagName('li');
-let ul = document.getElementById('lista-tarefas');
-
-ul.addEventListener('click', markTask);
-
-function markTask(event) {
-  const selectClick = event.target;
-  cleanMark()
-  if (selectClick.tagName === 'LI') {
-    event.target.style.backgroundColor = 'rgb(128,128,128)';
-  }
-}
-
-function cleanMark() {
-  for (let i = 0; i < li.length; i += 1) {
-    li[i].style.backgroundColor = 'white';
-  }
-}
-
-function addListener() {
-  for (let index = 0; index < li.length; index += 1) {
-    li[index].addEventListener('dblclick',throughTask);
-  }
-}
 function throughTask(event) {
-  let clicked = event.target;
+  const clicked = event.target;
   if (clicked.classList.contains('completed')) {
     clicked.classList.remove('completed');
   } else {
     clicked.classList.add('completed');
   }
 }
+
+const li = document.getElementsByTagName('li');
+function addListener() {
+  for (let index = 0; index < li.length; index += 1) {
+    li[index].addEventListener('dblclick', throughTask);
+  }
+}
+
+function addTask() {
+  const createLi = document.createElement('li');
+  ol.appendChild(createLi).innerText = input.value;
+  input.value = '';
+  addListener();
+}
+button.addEventListener('click', addTask);
+
+const ul = document.getElementById('lista-tarefas');
+function cleanMark() {
+  for (let i = 0; i < li.length; i += 1) {
+    li[i].style.backgroundColor = 'white';
+  }
+}
+
+function markTask(event) {
+  const selectClick = event.target;
+  cleanMark();
+  if (selectClick.tagName === 'LI') {
+    selectClick.style.backgroundColor = 'rgb(128,128,128)';
+  }
+}
+ul.addEventListener('click', markTask);
+
+const buttonCleanAll = document.getElementById('apaga-tudo');
+function cleanAll() {
+  for (let i = 0; i < li.length; i += 0) {
+    ul.removeChild(ul.children[0]);
+  }
+}
+buttonCleanAll.addEventListener('click', cleanAll);
+
+
