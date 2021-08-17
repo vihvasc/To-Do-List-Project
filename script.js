a3802b0df691c1db3417dc35baab8beda88de662
Denis Jonathan <denisjonathan7@gmail.com>
2021-08-16 21:00:08 -0300

    Requisitos 8 e 9 concluídos

--- script.js
+++ script.js
@@ -1,25 +1,41 @@
 const taskInput = document.getElementById('texto-tarefa'); // Input
 const listaTarefas = document.getElementById('lista-tarefas'); // Lista Ordenada
 const criarTarefa = document.getElementById('criar-tarefa'); // Botão adicionar tarefa
+const tarefasCriadas = document.getElementsByTagName('li'); // Todas tarefas criadas
+
+
+function changeColor(event) {
+  for (let index = 0; index < tarefasCriadas.length; index += 1) {
+    tarefasCriadas[index].style.backgroundColor = '';
+  }
+  const taskTarget = event.target;
+  taskTarget.style.backgroundColor = 'rgb(128, 128, 128)';
+}
 
 function addNewTask() {
   const li = document.createElement('li');
+  const inputValue = taskInput.value
+  // inputValue.replace(//)
   li.innerText = taskInput.value;
-  li.className = 'tarefa';
+  li.addEventListener('click', changeColor);
+  li.addEventListener('dblclick', toggleCompleted);
   listaTarefas.appendChild(li);
   taskInput.value = '';
-  addListenerForTasks();
 }
 
-function addListenerForTasks() {
-  const tarefasCriadas = [...document.getElementsByClassName('tarefa')]; // Todas tarefas criadas
-  tarefasCriadas.forEach(tarefa => {
-    tarefa.addEventListener('click', paintGrey);
-  });
-}
-
-function paintGrey(event) {
-  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+function toggleCompleted(event) {
+  const taskClicked = event.target;
+  if (taskClicked.className === 'completed') {
+    taskClicked.classList.remove('completed');
+  } else {
+    taskClicked.classList.add('completed');
+  }
 }
 
 criarTarefa.addEventListener('click', addNewTask);
+taskInput.addEventListener('keypress', function(event) {
+  let enterPressed = event;
+  if (enterPressed.key === 'Enter') {
+    addNewTask();
+  }
+});
