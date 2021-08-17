38e2c15232c4a63bd47f51feaba29610cbe9b054
BrenoRCBrito <brenorcbrito@gmail.com>
2021-08-17 17:07:47 -0300

    adiciona requisito 7

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
+function taskSelector() {
+  const clickOnTask = document.getElementsByTagName('li');
+  for (let taskNumber = clickOnTask.length, liLoop = (taskNumber - 1); liLoop >= 0; liLoop -= 1) {
+    clickOnTask[liLoop].addEventListener('click', () => {
+      if (clickOnTask[liLoop].style.backgroundColor === '') {
+        clickOnTask[liLoop].style.backgroundColor = 'rgb(128,128,128)';
+      }
+    });
+  }
+}
+
 const button = document.getElementById('criar-tarefa');
 button.addEventListener('click', () => {
   const lista = document.getElementById('lista-tarefas');
   const input = document.getElementById('texto-tarefa');
-  let inputValue = input.value;
+  const inputValue = input.value;
   const listItem = document.createElement('li');
   if (inputValue !== '') {
     listItem.innerText = inputValue;
     lista.appendChild(listItem);
     input.value = '';
+    taskSelector();
   }
 });
