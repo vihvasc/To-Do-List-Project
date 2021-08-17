ac3791ada814e6fa0bfc473f706827a462c15727
Murilo <murilocarrijoadm@gmail.com>
2021-08-16 22:09:47 -0300

    Adiciona 7º requisito

--- script.js
+++ script.js
@@ -3,15 +3,25 @@ let button = document.getElementById("criar-tarefa");
 let list = document.getElementById("lista-tarefas");
 let input = document.getElementById("texto-tarefa");
 
-button.addEventListener("click",addList);
+button.addEventListener("click",addList,);
 
 function addList(){
     let listItem = document.createElement("li");
     let text = input.value;
-    console.log(text);
     listItem.innerText = text;
     list.appendChild(listItem);
-    console.log(listItem);
     input.value = "";
+    addClikList();
 }
 
+function addClikList(){
+    let listItem = document.querySelectorAll("li");
+    for(let index = 0; index < listItem.length; index += 1){
+        listItem[index].addEventListener("click", color);
+    }
+}
+
+function color(event){
+    console.log("click");
+    event.target.style.backgroundColor = "rgb(128,128,128)";
+}
