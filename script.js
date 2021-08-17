b7873d815c07178cf2b245216d3cdd7ace69edc1
Erik Lopes <erik.lopesdev@outlook.com>
2021-08-17 15:23:32 -0300

    desafios 8 e 9

--- script.js
+++ script.js
@@ -2,18 +2,19 @@ const getOrdenedList = document.querySelector('#lista-tarefas');
 const getTask = document.querySelector('#texto-tarefa');
 const getCreateTaskButton = document.querySelector('#criar-tarefa');
 
-function verifyInputValueEmpty() {
+function createTasks() {
   if (getTask.value === '') {
     return alert('Insira um texto para a sua tarefa!');
   }
   const li = document.createElement('li');
+  li.className = 'task-item';
   li.innerText = getTask.value;
   getOrdenedList.appendChild(li);
   getTask.value = '';
 }
 
 function createTask() {
-  getCreateTaskButton.addEventListener('click', verifyInputValueEmpty);
+  getCreateTaskButton.addEventListener('click', createTasks);
 }
 
 function changeBackgroundColor(event) {
@@ -31,6 +32,25 @@ getOrdenedList.addEventListener('click', changeBackgroundColor);
 * Código acima foi feito com a ajuda do Aluno Iury Roque da tribo B
 */
 
+function completeTask() {
+  getOrdenedList.addEventListener('dblclick', (event) => {
+    if (event.target.classList.contains('completed')) {
+      event.target.classList.remove('completed');
+    } else {
+      event.target.classList.add('completed');
+    }
+  });
+}
+
+function deleteAll() {
+  const getDeleteAllButton = document.getElementById('apaga-tudo');
+  getDeleteAllButton.addEventListener('click', () => {
+    getOrdenedList.innerText = '';
+  });
+}
+
 window.onload = () => {
   createTask();
+  completeTask();
+  deleteAll();
 };
