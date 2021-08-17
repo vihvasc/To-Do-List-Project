2e89cf1008445b2e71141e829aa45de228b7ef96
BrenoRCBrito <brenorcbrito@gmail.com>
2021-08-17 17:48:14 -0300

    adiciona requisito 8

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-function taskSelector() {
-  const clickOnTask = document.getElementsByTagName('li');
-  for (let taskNumber = clickOnTask.length, liLoop = (taskNumber - 1); liLoop >= 0; liLoop -= 1) {
-    clickOnTask[liLoop].addEventListener('click', () => {
-      if (clickOnTask[liLoop].style.backgroundColor === '') {
-        clickOnTask[liLoop].style.backgroundColor = 'rgb(128,128,128)';
+function selectorValidator(clickOnTask, taskNumber, liLoop) {
+  clickOnTask[liLoop].addEventListener('click', () => {
+    for (let liLoop2 = 0; liLoop2 < taskNumber; liLoop2 += 1) {
+      if (clickOnTask[liLoop2].classList.contains('selected')) {
+        clickOnTask[liLoop2].classList.remove('selected');
       }
-    });
+    }
+    clickOnTask[liLoop].classList.add('selected');
+  });
+}
+
+function taskSelector() {
+  const clickOnTask = document.getElementsByClassName('task');
+  for (let taskNumber = clickOnTask.length, liLoop = 0; liLoop < taskNumber; liLoop += 1) {
+    selectorValidator(clickOnTask, taskNumber, liLoop);
   }
 }
 
-const button = document.getElementById('criar-tarefa');
-button.addEventListener('click', () => {
+function createTaskList() {
   const lista = document.getElementById('lista-tarefas');
   const input = document.getElementById('texto-tarefa');
   const inputValue = input.value;
@@ -18,7 +24,11 @@ button.addEventListener('click', () => {
   if (inputValue !== '') {
     listItem.innerText = inputValue;
     lista.appendChild(listItem);
-    input.value = '';
+    listItem.className = 'task';
     taskSelector();
+    input.value = '';
   }
-});
+}
+
+const button = document.getElementById('criar-tarefa');
+button.addEventListener('click', createTaskList);
