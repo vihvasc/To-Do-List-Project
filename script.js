694366c5b7a151aad496519bf3a3034a3ed5f780
Rafael Colombo <rcolombodev@gmail.com>
2021-08-16 21:00:54 -0300

    Refatora code para maior entendimento

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 const ordererList = document.getElementById('lista-tarefas');// lista ordenada.
 const input = document.querySelector('#texto-tarefa'); // input
 const addTaskButton = document.querySelector('#criar-tarefa');// botão adicionar tarefa.
-const createdTasks = document.getElementsByClassName('task');
+const createdTasks = document.getElementsByTagName('li');
 
-function removeColor() {
+function removeColor(event) {
+  const currentTask = event.target;
   for (let index = 0; index < createdTasks.length; index += 1) {
     createdTasks[index].style.backgroundColor = '';
   }
+  currentTask.style.backgroundColor = 'rgb(128, 128, 128)';
 }
+
+function trocaCompleted(event) {
+  const taskDeAgora = event.target;
+  if (taskDeAgora.className === 'completed') {
+    taskDeAgora.classList.remove('completed');
+  } else {
+    taskDeAgora.classList.add('completed');
+  }
+}
+
 function addGrayBackground() {
   for (let index = 0; index < createdTasks.length; index += 1) {
-    createdTasks[index].addEventListener('click', () => {
-      removeColor();
-      createdTasks[index].style.backgroundColor = 'rgb(128, 128, 128)';
-    });
+    createdTasks[index].addEventListener('click', removeColor);
+    createdTasks[index].addEventListener('dblclick', trocaCompleted);
   }
 }
 
@@ -21,7 +31,6 @@ function addTask() {
   addTaskButton.addEventListener('click', () => {
     const createList = document.createElement('li');
     createList.innerText = input.value;
-    createList.className = 'task';
     ordererList.appendChild(createList);
     input.value = '';
     addGrayBackground();
