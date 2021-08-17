7eee24407b7f92f705039feebc54eeb865279f40
Andre Luiz <andreluizgyn1@gmail.com>
2021-08-16 23:17:17 -0300

    Adciona escutadores na lista de tarefas.

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 const button = document.querySelector('#criar-tarefa');
 const input = document.querySelector('#texto-tarefa');
-let listaTarefas = document.querySelector('#lista-tarefas');
+const listaTarefas = document.querySelector('#lista-tarefas');
+const tarefas = document.getElementsByClassName('tarefas');
+
+function selecionarTarefa(event) {
+  const tarefa = event.target;
+  const selecionado = document.querySelector('.selecionado');
+
+  if (selecionado !== null) {
+    selecionado.classList.remove('selecionado');
+  }
+
+  if (selecionado === event.target) {
+    selecionado.classList.remove('selecionado');
+    return;
+  }
+
+  tarefa.classList.add('selecionado');
+}
 
 function criarTarefa() {
   const tarefa = document.createElement('li');
   tarefa.innerText = input.value;
+  tarefa.classList.add('tarefas');
+  tarefa.addEventListener('click', selecionarTarefa);
   listaTarefas.appendChild(tarefa);
   input.value = '';
 }
