976f795fcdd8082db2e0a3e5bfe34cd0edf9e0a3
jonatan-teixeira <jonatan.teixeira@live.com>
2021-08-17 17:34:54 -0300

    Atualizando botao de adiçao de item

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
- let bancoDeTarefas = [
-     {'descricao': ''}
- ]
+ let bancoDeTarefas = [];
  
- const criarTarefa = (descricao) => {
+ function criarTarefa() {
+     const input = document.getElementById('texto-tarefa');
      const tarefa = document.createElement('label');
      tarefa.classList.add('tarefa');
-     tarefa.innerHTML = `<div class="descricao">${descricao}</div>
-     <input type="button" value="X">`
+     tarefa.innerHTML = `<div class=descricao>${input.value}</div>
+                        <button class="delete"> X </button>`;
+     input.value = "";                        
      
      document.getElementById('lista-tarefas').appendChild(tarefa);
-}
-
- const adicionarTarefa = (e) => {
-    bancoDeTarefas.push({'descricao': ''});
- }    
-
-document.getElementById('criar-tarefa').addEventListener('click', adicionarTarefa);
+ }
