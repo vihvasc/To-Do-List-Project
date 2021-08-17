e6193987bac4ea0e272b72e61ca564bac5ebfc1e
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2021-08-17 16:21:56 -0300

    refatorando codigo

--- script.js
+++ script.js
@@ -1,120 +1,85 @@
-function clickButtonText () {
-  let buttonInput = document.getElementById('criar-tarefa');
+const  creatTaskButton = document.getElementById('criar-tarefa');
+const  getInputList = document.getElementById('texto-tarefa');
+const  listTask = document.getElementById('lista-tarefas');
+const liList = document.getElementsByTagName('li');
+const event = 'error';
+const clearAllButton = document.getElementById('apaga-tudo');
+const completedList = document.getElementsByClassName('completed');
+const clearCompletedButton = document.getElementById('remover-finalizados');
+const selectedList = document.getElementsByClassName('selected');
+const clearSelectedButton = document.getElementById('remover-selecionado');
 
-function inputTextList() {
-  let listItem = document.createElement('li');
-  let getInputList = document.getElementById('texto-tarefa');
-  let listOl = document.getElementById('lista-tarefas');
-  if (getInputList.value === '') { 
-    listItem = false
-  } else {
-  listItem.innerHTML = getInputList.value;
-  getInputList.value = '';}
-  listOl.appendChild(listItem);
+function creatTask() {
+  creatTaskButton.addEventListener('click', () => {
+    listItem = document.createElement('li');
+  
+    if (getInputList.value === '') { 
+      listItem = false
+    } else {
+      listItem.innerHTML = getInputList.value;
+      getInputList.value = '';}
+      listTask.appendChild(listItem);
+  });
 }
 
-buttonInput.addEventListener('click', inputTextList);
+creatTask();
 
+function paintTask() { 
+    listTask.addEventListener('click', (event) => {
+      for (let i = 0; i < liList.length; i += 1) {
+        liList[i].classList.remove('selected');
+      };
+    event.target.classList.add('selected');
+    listTask.classList.remove('selected');
+  });
 }
-
-clickButtonText();
-
-function paintTask() {
-
-    let olList = document.getElementById('lista-tarefas');
-    let taskSelected = document.getElementsByTagName('li');
-   
-    function removeSelected() {
-      for (let i = 0; i < taskSelected.length; i += 1) {
-        taskSelected[i].classList.remove('selected');
-      }
-    }
-
-    function selectTask(event) {
-      let evitaError = event;
-      removeSelected();
-      evitaError.target.classList.add('selected');
-      olList.classList.remove('selected');
-
-    }
-
-    olList.addEventListener('click', selectTask);
-  }
  
-  paintTask();
-
- 
+paintTask();
 
 function checkedTask() {
+  listTask.addEventListener('dblclick', (event) => {
+     // acesado https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event
+    event.target.classList.toggle('completed');
+    // ......
+    listTask.className = '';
+  });
+}
 
-    let olList = document.getElementById('lista-tarefas');
-    let taskSelected = document.getElementsByTagName('li');
+checkedTask();
 
-    function selectchecked(event) {
-      let evitaError = event;
-      // acesado https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event
-      evitaError.target.classList.toggle('completed');
-      // ......
-      olList.className = ''
-    }
-    
+function clearAll() {
+  clearAllButton.addEventListener('click', (event) => {
+    // acessador aqui https://qastack.com.br/programming/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
+    while (listTask.firstElementChild) {
+      listTask.removeChild(listTask.firstElementChild) 
+    };
+    // ....
+  });
 
-    olList.addEventListener('dblclick', selectchecked);
-  }
- 
-  checkedTask();
+}
 
-  function clearAll() {
-    let clear = document.getElementById('apaga-tudo');
+clearAll();
 
-    function removeAll() {
-      let list = document.getElementById('lista-tarefas');
-      // acessador aqui https://qastack.com.br/programming/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
-       while (list.firstElementChild) {
-         list.removeChild(list.firstElementChild)
+function clearCompleted() {
+  clearCompletedButton.addEventListener('click', () => {
+    while (completedList.length > 0) {
+      //  acessado https://qastack.com.br/programming/4777077/removing-elements-by-class-name
+      completedList[0].parentNode.removeChild(completedList[0]); //excluo o filho acessando o pai, como cada elemento( com classe completed) tem um pai diferente (li's diferentes) preciso criar um clico com while para acessar cada pai separado e assim excluir seu unico filho.
       // ....
-       }
-     }
+    };
+  });
+}
 
-    clear.addEventListener('click', removeAll);
+clearCompleted();
 
-  }
-
-  clearAll();
-
-  
-
-  function clearChecked() {
-    let checkeds = document.getElementsByClassName('completed');
-    let lists = document.getElementById('lista-tarefas');
-    let buttonClearCompleted = document.getElementById('remover-finalizados');
-
-    function completedOut() {
-      while (checkeds.length > 0) {
-        //  acessado https://qastack.com.br/programming/4777077/removing-elements-by-class-name
-        checkeds[0].parentNode.removeChild(checkeds[0]); //excluo o filho acessando o pai, como cada elemento( com classe completed) tem um pai diferente (li's diferentes) preciso criar um clico com while para acessar cada pai separado e assim excluir seu unico filho.
-        // ....
-      }
+function clearselected() {
+  clearSelectedButton.addEventListener('click', () => {
+    while (selectedList.length > 0) {
+      //  acessado https://qastack.com.br/programming/4777077/removing-elements-by-class-name
+      selectedList[0].parentNode.removeChild(selectedList[0]); //excluo o filho acessando o pai, como cada elemento( com classe completed) tem um pai diferente (li's diferentes) preciso criar um clico com while para acessar cada pai separado e assim excluir seu unico filho.
+      // ....
     }
-    
-    buttonClearCompleted.addEventListener('click', completedOut);
-  }
+  })
+}
 
-  clearChecked();
-
-  function clearselected() {
-    let selectedes = document.getElementsByClassName('selected');
-    let buttonClearselected = document.getElementById('remover-selecionado');
-
-    function selectedOut() {
-      while (selectedes.length > 0) {
-        //  acessado https://qastack.com.br/programming/4777077/removing-elements-by-class-name
-        selectedes[0].parentNode.removeChild(selectedes[0]); //excluo o filho acessando o pai, como cada elemento( com classe completed) tem um pai diferente (li's diferentes) preciso criar um clico com while para acessar cada pai separado e assim excluir seu unico filho.
-        // ....
-      }
-    }
-    
-    buttonClearselected.addEventListener('click', selectedOut);
-  }
-
-  clearselected();
+clearselected();
