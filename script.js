05c4af2e8d9aaea0e5460c2325b2fdd2ca218f12
MarcoMecenasFilho <marcomecenasfilho@gmail.com>
2021-08-17 16:37:32 -0300

    lint ok

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-const  creatTaskButton = document.getElementById('criar-tarefa');
-const  getInputList = document.getElementById('texto-tarefa');
-const  listTask = document.getElementById('lista-tarefas');
+const creatTaskButton = document.getElementById('criar-tarefa');
+const getInputList = document.getElementById('texto-tarefa');
+const listTask = document.getElementById('lista-tarefas');
 const liList = document.getElementsByTagName('li');
-const event = 'error';
 const clearAllButton = document.getElementById('apaga-tudo');
 const completedList = document.getElementsByClassName('completed');
 const clearCompletedButton = document.getElementById('remover-finalizados');
@@ -11,34 +10,35 @@ const clearSelectedButton = document.getElementById('remover-selecionado');
 
 function creatTask() {
   creatTaskButton.addEventListener('click', () => {
-    listItem = document.createElement('li');
-  
-    if (getInputList.value === '') { 
-      listItem = false
+    let listItem = document.createElement('li');
+
+    if (getInputList.value === '') {
+      listItem = false;
     } else {
       listItem.innerHTML = getInputList.value;
-      getInputList.value = '';}
+      getInputList.value = '';
       listTask.appendChild(listItem);
+    }
   });
 }
 
 creatTask();
 
-function paintTask() { 
-    listTask.addEventListener('click', (event) => {
-      for (let i = 0; i < liList.length; i += 1) {
-        liList[i].classList.remove('selected');
-      };
+function paintTask() {
+  listTask.addEventListener('click', (event) => {
+    for (let i = 0; i < liList.length; i += 1) {
+      liList[i].classList.remove('selected');
+    }
     event.target.classList.add('selected');
     listTask.classList.remove('selected');
   });
 }
- 
+
 paintTask();
 
 function checkedTask() {
   listTask.addEventListener('dblclick', (event) => {
-     // acesado https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event
+    // acesado https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event
     event.target.classList.toggle('completed');
     // ......
     listTask.className = '';
@@ -48,14 +48,13 @@ function checkedTask() {
 checkedTask();
 
 function clearAll() {
-  clearAllButton.addEventListener('click', (event) => {
+  clearAllButton.addEventListener('click', () => {
     // acessador aqui https://qastack.com.br/programming/3955229/remove-all-child-elements-of-a-dom-node-in-javascript
     while (listTask.firstElementChild) {
-      listTask.removeChild(listTask.firstElementChild) 
-    };
+      listTask.removeChild(listTask.firstElementChild);
+    }
     // ....
   });
-
 }
 
 clearAll();
@@ -63,10 +62,10 @@ clearAll();
 function clearCompleted() {
   clearCompletedButton.addEventListener('click', () => {
     while (completedList.length > 0) {
-      //  acessado https://qastack.com.br/programming/4777077/removing-elements-by-class-name
-      completedList[0].parentNode.removeChild(completedList[0]); //excluo o filho acessando o pai, como cada elemento( com classe completed) tem um pai diferente (li's diferentes) preciso criar um clico com while para acessar cada pai separado e assim excluir seu unico filho.
-      // ....
-    };
+      // acessado https://qastack.com.br/programming/4777077/removing-elements-by-class-name
+      completedList[0].parentNode.removeChild(completedList[0]);
+      // excluo o filho acessando o pai, como cada elemento( com classe completed) tem um pai diferente (li's diferentes) preciso criar um clico com while para acessar cada pai separado e assim excluir seu unico filho.
+    }
   });
 }
 
@@ -75,11 +74,11 @@ clearCompleted();
 function clearselected() {
   clearSelectedButton.addEventListener('click', () => {
     while (selectedList.length > 0) {
-      //  acessado https://qastack.com.br/programming/4777077/removing-elements-by-class-name
-      selectedList[0].parentNode.removeChild(selectedList[0]); //excluo o filho acessando o pai, como cada elemento( com classe completed) tem um pai diferente (li's diferentes) preciso criar um clico com while para acessar cada pai separado e assim excluir seu unico filho.
-      // ....
+    //  acessado https://qastack.com.br/programming/4777077/removing-elements-by-class-name
+      selectedList[0].parentNode.removeChild(selectedList[0]);
+    //  excluo o filho acessando o pai, como cada elemento( com classe completed) tem um pai diferente (li's diferentes) preciso criar um clico com while para acessar cada pai separado e assim excluir seu unico filho.
     }
-  })
+  });
 }
 
 clearselected();
