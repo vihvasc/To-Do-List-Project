233645a5c69ce81fae372f70d6dcac207a2929ad
Flávia Labanca <flavialabanca@gmail.com>
2021-08-17 02:17:29 -0300

    requisitos 1, 2, 3 e 4

--- script.js
+++ script.js
@@ -1,2 +1,19 @@
-let newList = createElement('ol');
+const newList = document.createElement('ol');
 newList.id = 'lista-tarefas';
+document.body.appendChild(newList);
+
+function addNewTask() {
+  const addButton = document.querySelector('#criar-tarefa');
+  const getInputField = document.querySelector('#task-input');
+  let getTask = document.querySelector('#texto-tarefa');
+
+  addButton.addEventListener('click', function () {
+    const newLine = document.createElement('li');
+    newList.appendChild(newLine);
+    console.log('valor: ');
+    console.log(getInputField.value);
+    newLine.innerText = getTask;
+    getInputField.value = '';
+  });
+}
+addNewTask();
