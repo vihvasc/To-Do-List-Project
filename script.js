247395b2d28a529ab6fb60d29021f71db643b02f
Ge <gmarcoscardoso@gmail.com>
2021-08-17 10:33:05 -0300

    adiciona funcionalidade de seleçao de task

--- script.js
+++ script.js
@@ -5,13 +5,34 @@ const todoList = document.querySelector('#lista-tarefas');
 
 // function
 function taskGenerator() {
-  // cria li com elemento do input
   const task = document.createElement('li');
+  task.classList.add('list');
   task.innerHTML = textTask.value;
-  task.classList.add = 'list';
   todoList.appendChild(task);
-  // limpa o input
   textTask.value = '';
+  cliclli();
+}
+
+function cliclli() {
+  const listTask = document.querySelectorAll('.list');
+
+  for (const element of listTask) {
+    element.addEventListener('click', (event) => {
+      if (element.classList !== 'select') {
+        removeClassList();
+        element.classList.add('select');
+        event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+      }
+    });
+  }
+}
+
+function removeClassList() {
+  const listTask = document.querySelectorAll('.list');
+  Array.from(listTask).forEach((event) => {
+    event.classList.remove('select');
+    event.style.backgroundColor = null;
+  });
 }
 
 // addEventListener
