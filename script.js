47b1d89145e3c11dec19c8364db3e5d8ca715f11
Deivid <deividmborges@gmail.com>
2021-08-17 17:49:04 -0300

    corrige lint

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-window.onload = criaListaTarefas;
+const list = document.getElementById('lista-tarefas');
+const inputBox = document.getElementById('texto-tarefa');
 
-function criaListaTarefas() {
-  geraItemList();
-  buttonDeleteAll();
-  buttonDeleteSelectedItems();
-  buttonSaveList();
-  getSavedList();
+function selecionaItemLista(event) {
+  const itemList = document.querySelectorAll('.item-list');
+  for (let index = 0; index < itemList.length; index += 1) {
+    if (itemList[index].classList.contains('selected')) {
+      itemList[index].classList.remove('selected');
+    }
+  }
+  event.target.classList.add('selected');
 }
-
-let list = document.getElementById('lista-tarefas');
-let inputBox = document.getElementById('texto-tarefa');
-
-function geraItemList() {
-  let buttonAdcItemList = document.getElementById('criar-tarefa');
-  buttonAdcItemList.addEventListener('click', adicionaItemLista)
+function riscaItemLista(event) {
+  event.target.classList.toggle('completed');
 }
 function adicionaItemLista() {
-  let itemList = document.createElement('li');
+  const itemList = document.createElement('li');
   itemList.innerText = inputBox.value;
   itemList.className = 'item-list';
   inputBox.value = '';
@@ -24,58 +22,54 @@ function adicionaItemLista() {
   itemList.addEventListener('dblclick', riscaItemLista);
   list.appendChild(itemList);
 }
-
-// let selectedItem = document.querySelector('.selected');
-function selecionaItemLista(event) {
-  let itemList = document.querySelectorAll('.item-list');
-  for(let index = 0; index < itemList.length; index += 1) {
-    
-    if(itemList[index].classList.contains('selected')) {
-      itemList[index].classList.remove('selected'); 
-    }
-  }
-  event.target.classList.add('selected');
-}
-function riscaItemLista(event) {
-  event.target.classList.toggle('completed'); 
-}
-
-function buttonDeleteAll() {
-  let buttonDeleteAllItens = document.getElementById('apaga-tudo');
-  buttonDeleteAllItens.addEventListener('click', deleteAllLis);
+function geraItemList() {
+  const buttonAdcItemList = document.getElementById('criar-tarefa');
+  buttonAdcItemList.addEventListener('click', adicionaItemLista);
 }
 function deleteAllLis() {
   list.remove(list.lastChild);
 }
-
-function buttonDeleteSelectedItems() {
-  let buttonDeleteSelectedItems = document.getElementById('remover-finalizados');
-  buttonDeleteSelectedItems.addEventListener('click', deleteSelectedItems);
+function buttonDeleteAll() {
+  const buttonDeleteAllItens = document.getElementById('apaga-tudo');
+  buttonDeleteAllItens.addEventListener('click', deleteAllLis);
 }
 function deleteSelectedItems() {
-  mySelectedItems = document.querySelectorAll('.completed');
+  const mySelectedItems = document.querySelectorAll('.completed');
   for (let index = 0; index < mySelectedItems.length; index += 1) {
     mySelectedItems[index].remove();
   }
 }
-function buttonSaveList() {
-  let buttonSave = document.getElementById('salvar-tarefas');
-  buttonSave.addEventListener('click', saveLocal);
+function generateButtonDelete() {
+  const buttonDeleteSelectedItems = document.getElementById('remover-finalizados');
+  buttonDeleteSelectedItems.addEventListener('click', deleteSelectedItems);
 }
 function saveLocal() {
-  let arraySavedList = [];
-  let taskList = Array.from(list.children);
+  const arraySavedList = [];
+  const taskList = Array.from(list.children);
   for (let index = 0; index < taskList.length; index += 1) {
     arraySavedList.push(taskList[index].innerHTML);
   }
   localStorage.setItem('myList', JSON.stringify(arraySavedList));
 }
-function getSavedList() {
-  let localList = JSON.parse(localStorage.getItem('myList'));
-  if(!localList) return
-    localList.forEach(function(liText){
-    let itemSavedList = document.createElement('li');
-    itemSavedList.innerHTML = liText; 
-    list.appendChild(itemSavedList);
-  })
+function buttonSaveList() {
+  const buttonSave = document.getElementById('salvar-tarefas');
+  buttonSave.addEventListener('click', saveLocal);
 }
+function getSavedList() {
+  const localList = JSON.parse(localStorage.getItem('myList'));
+  if (!localList) return;
+  localList.forEach(function (liText) {
+    const itemSavedList = document.createElement('li');
+    itemSavedList.innerHTML = liText;
+    list.appendChild(itemSavedList);
+  });
+}
+
+function criaListaTarefas() {
+  geraItemList();
+  buttonDeleteAll();
+  generateButtonDelete();
+  buttonSaveList();
+  getSavedList();
+}
+window.onload = criaListaTarefas;
