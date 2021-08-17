6ebd4fabd0dce6e4a2fd8588ea469dffe9ebdce0
Marina Marques <marina.marqueso@outlook.com>
2021-08-17 17:52:05 -0300

    Adiciona requisito 7

--- script.js
+++ script.js
@@ -8,8 +8,30 @@ buttonTask.addEventListener('click', createListTask);
 function createListTask(event) {
     let liTaks = document.createElement('li');
     liTaks.innerText = input.value;
+    liTaks.className = 'liTag';
     listTask.appendChild(liTaks);
     event.target.buttonTask;
     input.value = "";
 }
 
+// target: li tag
+// evento: ao clicar na lista, altera backgroundColor
+function liBackgroundColor(event) {
+    let liTag = listTask.children;
+    for (let i = 0; i < liTag.length; i++) {
+        liTag[i].className = 'liTag';
+    }
+
+    event.target.className = 'liTag selected';
+}
+listTask.addEventListener('click', liBackgroundColor);
+
+function liRisk(event) {
+    let liTag = listTask.children;
+    for (let i = 0; i < liTag.length; i++) {
+        liTag[i].className = 'liTag';
+    }
+
+    event.target.className = 'liTag completed';
+}
+listTask.addEventListener('dbclick', liRisk);
