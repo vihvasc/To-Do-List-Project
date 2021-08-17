3e6b28e23594dbd51b52b5cd99237f56225991a5
assisMeneghetti <assismfabri@hotmail.com>
2021-08-17 09:45:23 -0300

    adiciona requisito 9

--- script.js
+++ script.js
@@ -14,40 +14,22 @@ function adicionaTarefa() {
       for (let index of li) {
         index.style.backgroundColor = 'white';
       }
-      event.target.style.backgroundColor = 'gray';
+      event.target.style.backgroundColor = 'rgb(128,128,128)';
     })
   })
 }
 
 adicionaTarefa();
 
+function lineThrough() {
+  let lista = document.getElementsByTagName('ol');
+  for (let index = 0; index < lista.length; index += 1) {
+    lista[index].addEventListener('dblclick', function (event) {
+      event.target.classList.toggle('completed');
+    })
+  }
+}
 
-// adicionar addeventlistener nas <li> 
-// ao clicar vai adicionar a classe 'task'
+lineThrough();
 
-// function destacaItem() {
-//   let tarefas = document.getElementsByTagName('li');
-
-//   for (let index = 0; index < tarefas.length, index += 1) {
-//     tarefas.addEventListener('click', function (event) {
-//       event.target.classList.add('task');
-//     })
-//   }
-// }
-
-// destacaItem();
-
-// function destacaItem() {
-
-//   for (let index = 0; index < tarefas.length; index += 1) {
-//     let tarefas = document.getElementsByClassName('destaca');
-//     console.log(tarefas);
-//     tarefas.addEventListener('click', function (event) {
-
-//       tarefas.classList.add('task');
-//     })
-//   }
-// }
-
-// destacaItem();
 
