0b2a07982305b0df03dd75c127b7a36b901bf60a
Rafael Colombo <rcolombodev@gmail.com>
2021-08-17 15:53:12 -0300

    Adiciona requesito 12

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ const addTaskButton = document.querySelector('#criar-tarefa');// botão adiciona
 const createdTasks = document.getElementsByTagName('li');
 const deleteButton = document.getElementById('apaga-tudo');
 const deleteCompletedTask = document.getElementById('remover-finalizados');
+const buttonSaveList = document.getElementById('salvar-tarefas');
 
-function removeColor(event) {
+function addRemoveColor(event) {
   const currentTask = event.target;
   for (let index = 0; index < createdTasks.length; index += 1) {
     createdTasks[index].style.backgroundColor = '';
@@ -22,9 +23,9 @@ function changeCompleted(event) {
   }
 }
 
-function addGrayBackground() {
+function eventClickAndDbClick() {
   for (let index = 0; index < createdTasks.length; index += 1) {
-    createdTasks[index].addEventListener('click', removeColor);
+    createdTasks[index].addEventListener('click', addRemoveColor);
     createdTasks[index].addEventListener('dblclick', changeCompleted);
   }
 }
@@ -35,7 +36,7 @@ function addTask() {
     createList.innerText = input.value;
     ordererList.appendChild(createList);
     input.value = '';
-    addGrayBackground();
+    eventClickAndDbClick();
   });
 }
 
@@ -49,10 +50,48 @@ function clearCompletedTask() {
     completedList.remove();
     completedList = document.querySelector('.completed');
   }
+  localStorage.setItem('listTasksSave');
 }
 deleteCompletedTask.addEventListener('click', clearCompletedTask);
 
+function saveStorage() {
+  const tasksProperty = [];
+  for (let index = 0; index < createdTasks.length; index += 1) {
+    const currentTask = createdTasks[index];
+    const objTasks = {
+      text: currentTask.innerText,
+      bgColor: currentTask.style.backgroundColor,
+      className: currentTask.className,
+    };
+    tasksProperty.push(objTasks);
+  }
+  localStorage.setItem('tasks', JSON.stringify(tasksProperty));
+}
+buttonSaveList.addEventListener('click', saveStorage);
+
+function recoverySavedList() {
+  const recoveryList = JSON.parse(localStorage.getItem('tasks'));
+  for (let index = 0; index < recoveryList.length; index += 1) {
+    const li = document.createElement('li');
+    li.innerText = recoveryList[index].text;
+    li.style.backgroundColor = recoveryList[index].bgColor;
+    li.className = recoveryList[index].className;
+    ordererList.appendChild(li);
+  }
+}
+
+// function enterKey(evento) {
+//   const teclaPressionada = evento;
+//   teclaPressionada.addEventListener('enter', addTask);
+//   if (teclaPressionada === 'Enter') {
+//     addTask();
+//   }
+// }
+
 function allTask() {
   addTask();
+  if (localStorage.getItem('tasks')) {
+    recoverySavedList();
+  }
 }
 window.onload = allTask;
