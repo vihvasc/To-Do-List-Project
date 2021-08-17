8f856ccd6d67f90d04a5e0ea3d324ec5cf45b676
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2021-08-17 17:00:56 -0300

    adiciona requisito 8

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const input = document.querySelector('#texto-tarefa');
 const buttonAddTask = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
-const item = document.querySelectorAll('.list-item');
 const deleteButton = document.querySelector('#apaga-tudo');
-const completed = document.querySelectorAll('.completed');
 const completedRemoveButton = document.querySelector('#remover-finalizados')
+const completed = document.querySelectorAll('.completed');
 function addTask() {
   buttonAddTask.addEventListener('click', () => {
     if (input.value === '') {
@@ -14,23 +13,31 @@ function addTask() {
       taskList.appendChild(listItem);
       listItem.innerHTML = input.value;
       listItem.className = 'list-item';
-
+      
+      const item = document.querySelectorAll('.list-item');
+      
       listItem.addEventListener('click', (event) => {
-        listItem.classList.remove('selected')
-        event.target.classList.add('selected');
-      })
-
-      listItem.addEventListener('dblclick', (event) => {
-        if(event.target.classList.contains('completed')) {
-          event.target.classList.remove('completed');
-        } else {
-          event.target.classList.add('completed');
-        }
-      })
+      const selected = document.querySelectorAll('.selected');
+      for(let i = 0; i < selected.length; i += 1) {
+        selected[i].classList.remove('selected')
+      }
+     event.target.classList.add('selected');
+    }) 
+      
+  lineThrough(listItem);
     }input.value = '';
   });
 }
 addTask();
+function lineThrough(listItem) {
+  listItem.addEventListener('dblclick', (event) => {
+    if(event.target.classList.contains('completed')) {
+      event.target.classList.remove('completed');
+    } else {
+      event.target.classList.add('completed');
+    }
+  })
+}
 
 
 function deleteTasks () {
