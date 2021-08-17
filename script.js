b47212061254bb63b889d4fe2cc20336c1df899e
Ronald_D_Bezerra <rebulgame@gmail.com>
2021-08-16 21:05:43 -0300

    atualiza requisito 07; requisito 08

--- script.js
+++ script.js
@@ -13,10 +13,18 @@ butao.addEventListener("click", buton);
 let listas = document.getElementsByTagName("ol")[0]
 listas.addEventListener("click", pintar)
 
+
 function pintar(event) {
-   resultado = event.target
-   resultado.style.backgroundColor = "rgb(128, 128, 128)"
+resultado = event.target
+resultado.classList.add("selected");
+let li = document.getElementsByTagName("li")
+    for(i = 0; i < li.length; i +=1){
+        if(li[i].className === resultado.className && li[i] !== resultado) {
+            li[i].classList.remove("selected")
+        }
+    }
 }
+
 
 function buton () {
  let listaOrdenada = document.getElementById("lista-tarefas");
