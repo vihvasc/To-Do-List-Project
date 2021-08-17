5e925eb6f8109d5973e3dc5d5e9ea35c54fcbf7f
alinemsiqueira <aline.mscruz1@gmail.com>
2021-08-17 11:06:37 -0300

    Requisito 07

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const body = document.querySelector('body');
 const input = document.getElementById('texto-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 const storedTasks = localStorage.getItem('storedTasks');
-const createdTasks = document.querySelectorAll('.tarefa');
+const changeColor = document.querySelectorAll('.tarefa');
 
 function createButton(buttonId, buttonText, insertInsideId) {
   const createButton = document.createElement('button');
@@ -14,21 +14,38 @@ function createButton(buttonId, buttonText, insertInsideId) {
 
 function addTask() {
   const createLi = document.createElement('li');
-  const createInput = document.createElement('input');
-  createInput.type = 'checkbox';
   if (input.value === '') {
-    return alert('Favor inserir um nome válido para a tarefa!');
+      return alert('Favor inserir um nome válido para a tarefa!');
+    }
+    createLi.innerText = input.value;
+    createLi.id = 'tarefa';
+    createLi.addEventListener('click', addClass);
+    taskList.appendChild(createLi);
+    input.value = '';
   }
-  createLi.innerText = input.value;
-  createLi.classList.add('tarefa');
-  taskList.appendChild(createLi);
-  input.value = '';
-}
-
-function createTaskButton() {
-  createButton('criar-tarefa', 'Nova Tarefa', 'lista-tarefas');
-  const criarTarefa = document.getElementById('criar-tarefa');
-  criarTarefa.addEventListener ('click', addTask);
+  
+  function createTaskButton() {
+    createButton('criar-tarefa', 'Nova Tarefa', 'lista-tarefas');
+    const criarTarefa = document.getElementById('criar-tarefa');
+    criarTarefa.addEventListener ('click', addTask);
+    input.addEventListener ('keypress', function (event) {
+      if (event.key === 'Enter') {
+        addTask();
+      }
+  });
 }
 createTaskButton();
 
+function addClass(event) {
+  const selectedClass = document.querySelector('.selected');
+  if (selectedClass) {
+    selectedClass.classList.remove('selected');
+    selectedClass.style.backgroundColor = ''
+  }
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  event.target.classList.add('selected');
+}
+
+function finishedTask(event) {
+  const 
+}
