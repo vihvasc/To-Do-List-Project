3bce398849842e86b5da17b52f54ba915c002d0e
fernandamichetti <f.michetti@hotmail.com>
2021-08-17 15:55:58 -0300

    Finalmente adicionando o requisito 9

--- script.js
+++ script.js
@@ -2,41 +2,47 @@
 const buttonOne = document.getElementById('criar-tarefa');
 const infoInput = document.getElementById('texto-tarefa');
 const listTasks = document.getElementById('lista-tarefas');
-const arrTasks = document.getElementsByClassName('taskAdd');
+const arrTasks = document.getElementsByTagName('li');
 const clearAll = document.getElementById('apaga-tudo');
 const riskLine = document.querySelectorAll('.completed');
 const clearDone = document.getElementById('remover-finalizados');
 
-/*Adicionando novas tarefas*/
+/*Adicionando novas tarefas REQUISITO 1-7*/
 buttonOne.addEventListener('click', newTask);
 function newTask() {
   const newTask = infoInput.value;
   let anotherTask = document.createElement('li');
   anotherTask.className = 'taskAdd';
-  anotherTask.innerHTML = newTask;
+  anotherTask.innerText = newTask;
   anotherTask.addEventListener('click', changeBackColor);
   anotherTask.addEventListener('dblclick', completingTask);
   listTasks.appendChild(anotherTask);
   infoInput.value = '';
 }
-/*Mudar o fundo do item*/
+/*Mudar o fundo do item MEXER NISSO PRO REQUISITO 8*/
 function changeBackColor(event) {
   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-  event.target.className = 'selected';
+  event.target.classList.add('selected');
 }
-/*Riscar item completado*/
+/*se um elemento da lista tem className='selected' os outros nao podem ter. for (let i = 0; i > listTask.length)
+
+/*Riscar item completado REQUISITO 9*/
 function completingTask(event) {
-  event.target.className = 'taskAdd' + ' completed';
-  event.target.style.textDecoration = 'line-through';
+  const taskCompleted = event.target;
+  if (taskCompleted.classList.contains('completed')) {
+    taskCompleted.classList.remove('completed');
+  } else {
+    taskCompleted.classList.add('completed');
+  }
 }
 
-/*Botão para limpar a lista de tarefas*/
+/*Botão para limpar a lista de tarefas REQUISITO 10*/
 clearAll.addEventListener('click', clearItens);
 function clearItens() {
   listTasks.innerHTML = '';
 }
 
-/*botao finalizadas*/
+/*botao finalizadas REQUISITO 11 - PRECISA TERMINAR PQ NAO TA DANDO CERTO*/
 clearDone.addEventListener('click', finalizadasApagar);
 function finalizadasApagar() {
   for (let i = 0; i < riskLine.length; i += 1) {
