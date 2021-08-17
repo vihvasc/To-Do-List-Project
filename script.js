bf249d789b6413049bf810b5a8b3b742b4543941
gustavogss <gustavogss.dev@gmail.com>
2021-08-17 11:51:35 -0300

    requisito5 - adicionando tarefas na lista

---
+++ script.js
@@ -0,0 +1,18 @@
+const btnCreatetask = document.getElementById('criar-tarefa');
+const btnDeleteList = document.getElementById('apaga-tudo');
+const btnDeleteCompletos = document.getElementById('remover-finalizados');
+const btnSalvarTasks = document.getElementById('salvar-tarefas');
+const btnMoveUp = document.getElementById('mover-cima');
+const btnMoveDown = document.getElementById('mover-baixo');
+const toDoList = document.getElementById('lista-tarefas');
+const inputText = document.getElementById('texto-tarefa');
+
+function createTask(event) {
+  event.preventDefault();
+  toDoList.classList.add('todo');
+  const list = document.createElement('li');
+  toDoList.appendChild(list);
+  list.innerText = inputText.value;
+  inputText.value = '';
+}
+btnCreatetask.addEventListener('click', createTask);
