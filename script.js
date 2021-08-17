1dcdac30451f7d5569ad0008e12d1c9a7c60994f
Rodolfo <rodolfoserralha@hotmail.com>
2021-08-17 18:32:23 -0300

    adicionando parte do requisito 13

--- script.js
+++ script.js
@@ -5,10 +5,15 @@ const liSelect = document.getElementsByTagName('li');
 const clearButton = document.getElementById('apaga-tudo');
 const removeButton = document.getElementById('remover-finalizados');
 const saveTasks = document.getElementById('salvar-tarefas');
+const resetTasks = document.getElementById('resetar-tarefas');
+const moveUp = document.getElementById('mover-cima');
+const moveDown = document.getElementById('mover-baixo');
+const selected = document.getElementsByClassName('selected');
 
 function paintingLiBackground(event) {
   for (let index = 0; index < liSelect.length; index += 1) {
     liSelect[index].style.backgroundColor = '';
+    liSelect[index].classList.remove('selected');
   }
   const eventTarget = event.target;
   eventTarget.style.backgroundColor = 'rgb(128, 128, 128)';
@@ -66,3 +71,56 @@ savingTasks();
 window.onload = () => {
   olList.innerHTML = localStorage.getItem('tasksList');
 };
+
+// function resetingTasks() {
+//   resetTasks.addEventListener('click', () => {
+//     localStorage.clear();
+//   });
+// }
+
+// resetingTasks();
+
+// function moveUpLi() {
+//   for (let index = 0; index < li.length; index += 1) {
+//     if (liSelect[index].className = 'selected') {
+      
+//       (selected, selected.previousElementSibling)
+//     }
+//   }
+// }
+
+// function moveUpLi() {
+//   for (let index = 0; index < liSelect.length; index += 1) {
+//     if (liSelect[index].className === 'selected' && liSelect[index] > 0) {
+//     liSelect[index].insertBefore(liSelect[index], liSelect[index].previousElementSibling);
+//     }
+//   }
+// } 
+
+function moveUpLi() {
+  for (let index = 0; index < liSelect.length; index += 1) {
+    if (liSelect[index].style.backgroundColor !== '' && index > 0) {
+      olList.insertBefore(liSelect[index], liSelect[index].previousSibling);
+    }
+  }
+}
+
+function buttonUp() {
+  moveUp.addEventListener('click', moveUpLi);
+}
+
+buttonUp();
+
+// function moveDownLi() {
+//   for (let index = 0; index < liSelect.length; index += 1) {
+//     if (liSelect[index].style.backgroundColor !== '' && index > (liSelect.length - 1)) {
+//       olList.insertBefore(liSelect[index], liSelect[index].previousSibling);
+//     }
+//   }
+// }
+
+// function buttonDown() {
+//   moveDown.addEventListener('click', moveDownLi);
+// }
+
+// buttonDown();
