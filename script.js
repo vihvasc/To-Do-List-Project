211523d6f306ffda56d009832d5e1337057b4f55
Daniel Pordeus Gonzalez <danielpordeusgonzalez@hotmail.com>
2021-08-17 17:46:37 -0300

    comita 5 e 6 quesito

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let createTask = document.querySelector('#criar-tarefa');
+let textTask = document.querySelector('#texto-tarefa');
+let taskList = document.querySelector('#lista-tarefas');
+
+createTask.addEventListener('click', generateTask);
+
+function generateTask(){
+    let task = document.createElement('li');
+    taskList.appendChild(task);
+    task.innerHTML=textTask.value;
+    textTask.value='';
+}
 
 
 
