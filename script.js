334ab07cc80cb86063b2ca8f7b53d420243049e0
Wilian Pereira <wilian_pereira92@outlook.com>
2021-08-17 13:06:28 -0300

    Dessafi 5: Adicionando botao de tarefa e desafi 6: Ordenado a lista

--- script.js
+++ script.js
@@ -10,10 +10,29 @@ const createInput = document.createElement('input');
 createInput.id = 'texto-tarefa';
 const createOrderedList = document.createElement('ol');
 createOrderedList.id = 'lista-tarefas';
+const createButton = document.createElement('button');
+createButton.id = 'criar-tarefa';
+createButton.innerText = 'Adicionar';
+const createInputSectionAndButton = document.createElement('section');
+const createListSection = document.createElement('section');
 
 corpoDoProjeto[0].appendChild(headerTitle);
 headerTitle.appendChild(title);
 corpoDoProjeto[0].appendChild(createSection);
+corpoDoProjeto[0].appendChild(createInputSectionAndButton);
+corpoDoProjeto[0].appendChild(createListSection);
 createSection.appendChild(createParagraph);
-createSection.appendChild(createInput);
-createSection.appendChild(createOrderedList);
+createInputSectionAndButton.appendChild(createInput);
+createListSection.appendChild(createOrderedList);
+createInputSectionAndButton.appendChild(createButton);
+
+const addedValues = document.querySelector('#texto-tarefa');
+const text = addedValues;
+function addedElementToList() {
+  const list = document.createElement('li');
+  createOrderedList.appendChild(list);
+  list.className = 'lista';
+  list.innerText = text.value;
+  text.value = '';
+}
+createButton.addEventListener('click', addedElementToList);
