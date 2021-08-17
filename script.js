a237b915f962f0bb2258e5a2a0a14cdb9369ad53
Lucas Pinheiro Rocha <lucas.pinheiror@gmail.com>
2021-08-17 15:38:45 -0300

    Cria botao que apaga as tarefas concluidas

--- script.js
+++ script.js
@@ -2,26 +2,15 @@ window.onload = function () {
   let botaoCriarTarefa;
 };
 
-let paragrafo = document.getElementById('funcionamento');
-let input = document.getElementById('texto-tarefa');
+const input = document.getElementById('texto-tarefa');
 let listaOrdenada = document.getElementById('lista-tarefas');
-let botaoCriarTarefa = document.getElementById('criar-tarefa');
-
-botaoCriarTarefa.addEventListener('click', createTask);
-
-function createTask() {
-  let newTask = document.createElement('li');
-  newTask.innerHTML = input.value;
-  newTask.addEventListener('click', grayColor);
-  newTask.addEventListener('dblclick', taskDone);
-  input.value = '';
-  listaOrdenada.appendChild(newTask);
-}
+const botaoCriarTarefa = document.getElementById('criar-tarefa');
+const doneButton = document.querySelector('#remover-finalizados');
 
 function grayColor(event) {
-  listaOrdenada = document.getElementById('lista-tarefas').children;
-  for (let i = 0; i < listaOrdenada.length; i += 1) {
-    let task = listaOrdenada[i];
+  let listaOrdenada2 = document.getElementById('lista-tarefas').children;
+  for (let i = 0; i < listaOrdenada2.length; i += 1) {
+    let task = listaOrdenada2[i];
     if (
       window.getComputedStyle(task).backgroundColor === 'rgb(128, 128, 128)'
     ) {
@@ -31,6 +20,18 @@ function grayColor(event) {
   event.target.style.backgroundColor = 'rgb(128,128,128)';
 }
 
+function createTask() {
+  const newTask = document.createElement('li');
+  newTask.innerHTML = input.value;
+  newTask.addEventListener('click', grayColor);
+  newTask.addEventListener('dblclick', taskDone);
+  input.value = '';
+  listaOrdenada.appendChild(newTask);
+}
+
+botaoCriarTarefa.addEventListener('click', createTask);
+
+
 function taskDone(event) {
   if (event.target.classList.contains('completed')) {
     event.target.classList.remove('completed');
@@ -39,14 +40,24 @@ function taskDone(event) {
   }
 }
 
-let clearButton = document.getElementById("apaga-tudo");
-clearButton.addEventListener("click", clearAll);
+let clearButton = document.getElementById('apaga-tudo');
+clearButton.addEventListener('click', clearAll);
 
-function clearAll (){  
-  listaOrdenada = document.getElementById('lista-tarefas').children;
-  listaOrdenada2 = document.getElementById('lista-tarefas')
-  if (listaOrdenada.length > 0){
-    listaOrdenada2.innerHTML = " "    
+function clearAll() {
+  listaOrdenada2 = document.getElementById('lista-tarefas').children;
+  listaOrdenada = document.getElementById('lista-tarefas');
+  if (listaOrdenada2.length > 0) {
+    listaOrdenada.innerHTML = ' ';
+  }
+}
+
+function removeTasksDone (){    
+  const lista = listaOrdenada.children;
+  for (let index = lista.length - 1; index >= 0; index -= 1){
+    if(lista[index].classList.contains("completed")){
+      listaOrdenada.removeChild(lista[index]);
+    };
   };
 };
 
+doneButton.addEventListener("click", removeTasksDone);
