711b888befb3489045f20a82fbe29837ea5f70d0
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2021-08-17 13:59:44 -0300

    welckson valentim - Realiza oitavo requisito do projeto todo list, apos aprovacao local

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 let button = document.querySelector('#criar-tarefa');
 let listaTarefa = document.querySelector('#lista-tarefas');
+let itemListaTarefa = document.getElementsByClassName('.item');
 
-button.addEventListener('click', function() {
+button.addEventListener('click', function( ) {
   let textoTarefa = document.querySelector('#texto-tarefa');
-  let listaTarefa = document.querySelector('#lista-tarefas');
   let criaLi = document.createElement('li');
   criaLi.innerHTML = textoTarefa.value;
+  criaLi.className = "item";
   listaTarefa.appendChild(criaLi);
   textoTarefa.value = '';
 });
 
 function corTarefa(event) {
-  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  let backgroundColorPadrao = 'white';
+  let itemListaTarefa = document.querySelectorAll('.item');
+  let selectColor = 'rgb(128, 128, 128)';
+ for (let index = 0; index < itemListaTarefa.length; index += 1){
+    if (itemListaTarefa[index].style.backgroundColor !== backgroundColorPadrao) {
+      itemListaTarefa[index].style.backgroundColor = backgroundColorPadrao;
+    }
+  }
+  event.target.style.backgroundColor = selectColor;
 }
 listaTarefa.addEventListener('click', corTarefa);
