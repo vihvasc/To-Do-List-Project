53bc9dc19af54c1a4cf4007f60cbbdf7f0dc23ab
Lucas Peres <lucasperesss@gmail.com>
2021-08-17 06:31:25 -0300

    Requisitos 10 e 11

--- script.js
+++ script.js
@@ -9,6 +9,20 @@ function complete(task) {
   task.target.classList.toggle('completed');
 }
 
+function apagarLista() {
+  const lista = document.querySelectorAll('li');
+  lista.forEach((item) => {
+    item.remove();
+  });
+}
+
+function apagarFinalizadas() {
+  const prontas = document.querySelectorAll('.completed');
+  prontas.forEach((item) => {
+    item.remove();
+  });
+}
+
 function criarTarefa() {
   const input = document.getElementById('texto-tarefa');
   const tarefa = document.createElement('li');
@@ -21,4 +35,6 @@ function criarTarefa() {
 
 window.onload = function initPage() {
   document.getElementById('criar-tarefa').addEventListener('click', criarTarefa);
+  document.getElementById('apaga-tudo').addEventListener('click', apagarLista);
+  document.getElementById('remover-finalizados').addEventListener('click', apagarFinalizadas);
 };
