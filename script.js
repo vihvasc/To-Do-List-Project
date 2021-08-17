ad875649ed0194c7e26dbe204be716f3bf6b24c4
jonatan-teixeira <jonatan.teixeira@live.com>
2021-08-17 18:29:35 -0300

    Adicionando a atualizaçao da lista

--- script.js
+++ script.js
@@ -4,9 +4,15 @@
      const input = document.getElementById('texto-tarefa');
      const tarefa = document.createElement('label');
      tarefa.classList.add('tarefa');
-     tarefa.innerHTML = `<div class=descricao>${input.value}</div>
-                        <button class="delete"> X </button>`;
+     tarefa.innerHTML = `<div class=descricao>${input.value}</div>`                        
      input.value = "";                        
      
      document.getElementById('lista-tarefas').appendChild(tarefa);
  }
+
+ function mudarCorDeFundo(color) {
+     const label = document.querySelector('.tarefa');
+     label.style.background = color;
+ }
+ 
+ document.querySelector('.tarefa').addEventListener('onClick', mudarCorDeFundo('rgb(128,128,128)'));
