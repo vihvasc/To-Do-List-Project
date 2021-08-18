a85579c4f78f570ee855fe702e11869e38e8a42d
ArthurMoreira <arthurmcs96@gmail.com>
2021-08-18 11:39:30 -0300

    Adiciona os requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
+function clearColorTask() {
+  const tasks = document.getElementsByTagName('li');
+  for (let i = 0; i < tasks.length; i += 1) {
+    const task = tasks[i];
+    task.style.backgroundColor = '';
+  }
+}
+
+function changeBackgroundColorTask(e) {
+  if (e.target.style.backgroundColor === '') {
+    e.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    e.target.id = 'selected';
+  }
+}
+
 function createdTask() {
   const inputValue = document.getElementById('texto-tarefa').value;
   const list = document.getElementById('lista-tarefas');
   if (inputValue !== '') {
-    const taks = document.createElement('li');
-    taks.innerText = inputValue;
-    list.appendChild(taks);
+    const task = document.createElement('li');
+    task.innerText = inputValue;
+    list.appendChild(task);
+    task.addEventListener('click', clearColorTask);
+    task.addEventListener('click', changeBackgroundColorTask);
   }
   document.getElementById('texto-tarefa').value = '';
 }
