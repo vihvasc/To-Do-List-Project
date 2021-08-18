0880afb296caa651316826b22c2aa8320a9d57b8
José Erivam <erivamdev@gmail.com>
2021-08-18 11:23:17 -0300

    Adicionando requisito 12

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const buttonAdd = document.querySelector('#criar-tarefa');
 
 const ol = document.querySelector('#lista-tarefas');
 
-
 buttonAdd.addEventListener('click', () => {
   const text = document.querySelector('#texto-tarefa'); // pega o input
   const { value } = text; // pega o valor do inpu
@@ -46,3 +45,57 @@ eraserSelected.addEventListener('click', () => {
     liCompleted[i].remove();
   }
 });
+// Requisito 12
+const saveList = document.getElementById('salvar-tarefas');
+
+saveList.addEventListener('click', () => { // função para salvar a lista
+  const inputList = document.querySelectorAll('li');
+  const saveClass = [];
+  const lista = [];
+  for (let i = 0; i < inputList.length; i += 1) {
+    if (inputList[i].classList.contains('completed')) {
+      saveClass.push('completed');
+    } else {
+      saveClass.push('not');
+    }
+    lista.push(inputList[i].innerText);
+  }
+  localStorage.setItem('classeSalva', JSON.stringify(saveClass));
+  localStorage.setItem('listaSalva', JSON.stringify(lista));
+});
+
+/* saveList.addEventListener('click', () => { // função para salvar a lista
+  const inputList = document.querySelectorAll('li');
+  const saveClass = [];
+  const lista = [];
+  for (let i = 0; i < inputList.length; i += 1) {
+    if (inputList[i].classList.contains('completed')) {
+      saveClass.push('true');
+    } else {
+      saveClass.push('false');
+    }
+    lista.push(inputList[i].innerText);
+  }
+  localStorage.setItem('classeSalva', JSON.stringify(saveList));
+  localStorage.setItem('listaSalva', JSON.stringify(lista));
+}); */
+
+function recuperaLista() { // função para recuperar a lista
+  const listaRecuperada = JSON.parse(localStorage.getItem('listaSalva'));
+  const classeRecuperada = JSON.parse(localStorage.getItem('classeSalva'));
+  if (listaRecuperada) {
+    for (let i = 0; i < listaRecuperada.length; i += 1) {
+      const li = document.createElement('li');
+      if(classeRecuperada[i] == 'completed'){
+      li.classList.add('completed')
+      }
+      li.innerText = listaRecuperada[i];
+      li.classList.add('lista');
+      ol.appendChild(li);
+    }
+  }
+}
+
+window.onload = function () {
+  recuperaLista();
+};
