34f768410459f2dbda47c70255594fff6befda47
Rodolfomno <Rodolfo_mno@hotmail.com>
2021-08-18 00:42:30 -0300

    adiciona requisito 4 e 5

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-let title = document.createElement('header')
+const title = document.createElement('header')
 title.innerHTML = 'Minha Lista de Tarefas'
 document.body.appendChild(title)
 
+const p = document.createElement('p')
+p.id = 'funcionamento'
+p.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo'
+document.body.appendChild(p)
+
+const list = document.createElement('ol')
+list.id = 'lista-tarefas'
+document.body.appendChild(list)
+
+const input = document.createElement('input')
+input.type = 'text'
+input.id = 'texto-tarefa'
+document.body.appendChild(input)
+
+input.addEventListener('change', function(){
+
+    let item = document.createElement('li')
+    item.innerText = input.value
+    list.appendChild(item)
+    input.value = ''
+
+})
