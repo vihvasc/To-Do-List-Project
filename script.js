5cb85c3b707833a9f17153101c8631133135cba9
Yamamoto Andre <andre.yamamoto07@gmail.com>
2021-08-18 17:36:12 -0300

    Adicionado o parametro 7

--- script.js
+++ script.js
@@ -7,11 +7,17 @@ function criarElementoTabela(origem){
     let criarElemento = document.createElement("li");
     criarElemento.innerText = valorDeInput.value;
     listaDeTarefas.appendChild(criarElemento);
+    criarElemento.addEventListener("click", mudarCorDeFundo);
     valorDeInput.value = ""
 }
 
 // Fazer o botão adicionar o Elemento 
-
 const botaoDeAtualização = document.querySelector("#criar-tarefa");
-
 botaoDeAtualização.addEventListener("click", criarElementoTabela);
+
+// Mudar a cordo fundo ao clickar no item 
+
+const elementosDaLista = document.querySelectorAll("li");
+function mudarCorDeFundo(origem){
+    origem.target.style.backgroundColor = "rgb(128,128,128)";
+}
