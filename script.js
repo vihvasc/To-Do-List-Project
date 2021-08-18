9866b2a8309ea7b29713b0d2e468d606124c2583
Yamamoto Andre <andre.yamamoto07@gmail.com>
2021-08-18 18:27:01 -0300

    Adicionado o parametro 9

--- script.js
+++ script.js
@@ -3,11 +3,12 @@ const listaDeTarefas = document.querySelector("#lista-tarefas");
 const criarElemento = document.createElement("li");
 // Montar lista de elementos
 
-function criarElementoTabela(origem){
+function criarElementoTabela(origem) {
     let criarElemento = document.createElement("li");
     criarElemento.innerText = valorDeInput.value;
     listaDeTarefas.appendChild(criarElemento);
     criarElemento.addEventListener("click", mudarCorDeFundo);
+    criarElemento.addEventListener("dblclick", riscarElemento);
     valorDeInput.value = ""
 }
 
@@ -17,18 +18,26 @@ botaoDeAtualização.addEventListener("click", criarElementoTabela);
 
 // Mudar a cordo fundo ao clickar no item 
 
-const elementosDaLista = document.querySelectorAll("li");
-function mudarCorDeFundo(origem){
+function mudarCorDeFundo(origem) {
     somenteUm();
-    origem.target.classList.add("select"); 
+    origem.target.classList.add("select");
 }
 
 // somente um selecionado
-
-let elementoSelecionado2 = document.querySelectorAll(".select");
-function somenteUm(origem){
+function somenteUm(origem) {
     let elementoSelecionado = document.querySelectorAll(".select");
-       if (elementoSelecionado.length != 0){
+    if (elementoSelecionado.length != 0) {
         elementoSelecionado[0].classList.remove("select");
     }
+}
+
+// riscar elemento
+
+function riscarElemento(origem){
+    if (origem.target.classList.contains('completed')){
+        origem.target.classList.remove('completed');
+    }else {
+        origem.target.classList.add('completed');
+    }
+    
 }
