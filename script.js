f7f96f066baf19b4b07c098b942910a7d227b4ac
gustavogss <gustavogss.dev@gmail.com>
2021-08-18 14:37:23 -0300

    tentando resolver requisito 7

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const btnDeleteCompletos = document.getElementById('remover-finalizados');
 const btnSalvarTasks = document.getElementById('salvar-tarefas');
 const btnMoveUp = document.querySelector('#mover-cima');
 const btnMoveDown = document.querySelector('#mover-baixo');
-const btnToDoTask = document.querySelector('#remover-selecionado');
+const btnRemoveTask = document.querySelector('#remover-selecionado');
 const toDoList = document.getElementById('lista-tarefas');
 const inputText = document.getElementById('texto-tarefa');
 
@@ -12,7 +12,7 @@ function createTask(event) {
   event.preventDefault();
   toDoList.classList.add('todo');
   const listItem = document.createElement('li');
-  listItem.classList.add('selected');
+  listItem.classList.add('itemtask');
   toDoList.appendChild(listItem);
   listItem.innerText = inputText.value;
   inputText.value = '';
@@ -20,13 +20,12 @@ function createTask(event) {
 btnCreatetask.addEventListener('click', createTask);
 
 toDoList.addEventListener('click', (event) => {
-  const changedColor = event;
-  if (changedColor.target.className === 'selected') {
-    if (changedColor.target.style.backgroundColor === 'rgb(128,128,128)') {
-      changedColor.target.style.backgroundColor = '';
-    } else {
-      changedColor.target.style.backgroundColor = 'rgb(128,128,128)';
-    }
+  const selectedTask = document.querySelector('.selected');
+  if (selectedTask === undefined) {
+    event.target.classList.add('selected');
+  } else {
+    event.target.classList.add('selected');
+    selectedTask.classList.remove('selected');
   }
 });
 
@@ -38,27 +37,20 @@ toDoList.addEventListener('dblclick', (event) => {
   event.target.classList.toggle('completed');
 });
 
-btnSalvarTasks.addEventListener('click', () => {
-
-  localStorage.setItem('savedList', toDoList.innerHTML);
-});
-
-btnMoveUp.addEventListener('click', () => {
-  console.log('clicou');
-});
-
-btnMoveDown.addEventListener('click', () => {
-  console.log('clicou');
-});
-
-btnToDoTask.addEventListener('click', () => {
-  console.log('clicou');
-});
-
 btnDeleteCompletos.addEventListener('click', () => {
   const tasksCompletos = document.getElementsByClassName('completed');
   const comprimento = tasksCompletos.length;
   for (let index = 0; index < comprimento; index += 1) {
     tasksCompletos[0].remove();
   }
+});
+
+btnRemoveTask.addEventListener('click', () => {});
+
+btnMoveUp.addEventListener('click', () => {});
+
+btnMoveDown.addEventListener('click', () => {});
+
+btnSalvarTasks.addEventListener('click', () => {
+  localStorage.setItem('task', toDoList.innerHTML);
 });
