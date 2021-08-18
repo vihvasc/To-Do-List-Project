9d7255f3578d22a8dea2c7a8cbbd959b7e70b8b9
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2021-08-18 12:05:42 -0300

    alteraçao nos itens 5 e 6

--- script.js
+++ script.js
@@ -2,14 +2,17 @@
 const botao = document.querySelector('#criar-tarefa');
 
 function botaoAdd() {
-  const descricaoTarefa = document.querySelector('#texto-tarefa');
+  const descricaoTarefa = document.querySelector('input');
   const novaTarefa = document.createElement('li');
   const listaTarefas = document.querySelector('#lista-tarefas');
 
   novaTarefa.innerHTML = descricaoTarefa.value;
+  novaTarefa.setAttribute('class', 'completed');
   descricaoTarefa.value = '';
 
   listaTarefas.appendChild(novaTarefa);
+
 }
 
 botao.addEventListener('click', botaoAdd);
+
