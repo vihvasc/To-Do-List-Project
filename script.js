92e61be52830da3163cb6ef99b9be21f4da6b9b6
Jamez Mayke <jamezmayke@gmail.com>
2021-08-18 11:22:16 -0300

    Requisito 07 feito

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const getValueInputText = document.getElementById('texto-tarefa');
-let createElementLi = document.getElementById('lista-tarefas');
+const createElementLi = document.getElementById('lista-tarefas');
 const getButton = document.getElementById('criar-tarefa');
-
 
 getButton.addEventListener('click', () => {
   let elementLi = document.createElement('li')
+  
+  elementLi.addEventListener('click', () => {
+    elementLi.style.backgroundColor = 'rgb(128, 128, 128)'
+  })
   
   elementLi.innerText += getValueInputText.value
 
