dc550e66d9702cc4424c01ffa54b1d9f245d7f2d
Laurenz Pinder <laurenzdpinder90@gmail.com>
2021-08-19 19:51:07 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const tarefas = [];
+const button = document.querySelector('#criar-tarefa');
 const listaTarefas = document.querySelector('#lista-tarefas');
+
 
 function handleTarefas() {
   const size = tarefas.length - 1;
   const novaTarefa = tarefas[size];
   const tarefaList = document.createElement('li');
   tarefaList.innerText = novaTarefa;
-
   listaTarefas.appendChild(tarefaList);
 }
 
@@ -17,5 +18,15 @@ function handleButton() {
   handleTarefas();
 }
 
-const button = document.querySelector('#criar-tarefa');
+function greyClick(event) {
+  const selected = document.querySelector('.selected');
+  if (!selected) {
+    event.target.classList.add('selected');
+  } else {
+    selected.classList.remove('selected');
+    event.target.classList.add('selected');
+  }
+}
+
 button.addEventListener('click', handleButton);
+listaTarefas.addEventListener('click', greyClick);
