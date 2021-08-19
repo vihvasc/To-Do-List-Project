d6ea916c4b7e80391c8194f97910fc1ae81f7acb
Lucas Maieski Marques <lucmaieski@gmail.com>
2021-08-19 19:44:57 -0300

    Realiza o oitvao requisito

--- script.js
+++ script.js
@@ -3,14 +3,20 @@ let recebe_info = document.getElementById('texto-tarefa');
 
 document.getElementById('criar-tarefa').addEventListener('click', function() {
     var li = document.createElement('li')
+    let li_lista = document.getElementsByTagName('li');
+    
     li.addEventListener('click', function(){
+        for(let i=0; i < li_lista.length; i+=1){
+            li_lista[i].style.backgroundColor = 'white'
         li.style.backgroundColor = 'rgb(128,128,128)'
+        }
     })
+
+    //coloca 'li' como filho do 'ol':
     let lista_ordenada = document.getElementById('lista-tarefas');
     lista_ordenada.appendChild(li)
     li.innerText = recebe_info.value
+    //limpa a caixa de texto:
     recebe_info.value=''
 
 })
-
-
