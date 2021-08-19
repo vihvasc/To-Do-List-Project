36e25e0379b01998bfaf4bdb760d0ca21d5ad2da
Adson Gomes Oliveira <adsongoliveira2021@gmail.com>
2021-08-19 15:58:29 -0300

    Setimo requisito concluido

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 window.onload = function () {
-  CreateAndAddTask();
+  createAndAddTask();
 };
 
-function CreateAndAddTask() {
+function createAndAddTask() {
   let button = document.querySelector('#criar-tarefa');
   let taskInput = document.querySelector('#texto-tarefa');
 
@@ -17,5 +17,25 @@ function CreateAndAddTask() {
 
     taskInput.value = '';
     taskList.appendChild(task);
+    changeColorOfListItem();
+  }
+}
+function changeColorOfListItem() {
+  let listItem = document.querySelectorAll('.tarefa');
+  console.log(listItem);
+  console.log('cheguei na função');
+
+  for (let index = 0; index < listItem.length; index++) {
+    console.log(listItem[index]);
+    listItem[index].addEventListener('click', changeColor);
+  }
+
+  function changeColor(originEvent) {
+    let grayItem = document.querySelectorAll('.gray-list-item');
+
+    if (grayItem.length > 0) {
+      return originEvent.target.classList.remove('gray-list-item');
+    }
+    originEvent.target.classList.add('gray-list-item');
   }
 }
