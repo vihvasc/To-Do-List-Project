75ed4b347a2d91820ce3e9a4e662093804a8c3d2
Lucas Maieski Marques <lucmaieski@gmail.com>
2021-08-19 01:26:28 -0300

    Realiza o sexto e o  o setimo requisito

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let recebe_info = document.getElementById('texto-tarefa');
 
+
 document.getElementById('criar-tarefa').addEventListener('click', function() {
-    let li = document.createElement('li');
+    var li = document.createElement('li')
+    li.addEventListener('click', function(){
+        li.style.backgroundColor = 'rgb(128,128,128)'
+    })
     let lista_ordenada = document.getElementById('lista-tarefas');
     lista_ordenada.appendChild(li)
     li.innerText = recebe_info.value
     recebe_info.value=''
+    li.classList.add('linha')
 })
+
+
