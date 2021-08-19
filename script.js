2188c107bb3647e0abd70a2324afeac7597e5695
Miguel_Reis <guelsouzar@gmail.com>
2021-08-19 13:10:41 -0300

    requisito 8

--- script.js
+++ script.js
@@ -3,31 +3,44 @@ let input = document.getElementById("texto-tarefa")
 let ol = document.getElementById("lista-tarefas")
 
 
-function Botao() {
+function createAddTaskEvent() {
     botao.addEventListener('click', function () {
-        for (let i = 0; i  < 1; i ++) {
+
         let li = document.createElement('li')
         ol.appendChild(li)
         li.innerText = input.value
-        li.addEventListener('click', function (para) {
-            li.style.backgroundColor = 'rgb(128,128,128)'
-            para.target
+        li.addEventListener('click', function (event) {
+            let selected = document.querySelector('.selected')
+            if(selected === null){
+                event.target.classList.add('selected')
+            } else {
+                selected.classList.remove('selected')
+                event.target.classList.add('selected')
+            } 
+            
+            
+            /* selected.classList.remove('selected') */
+            
+
         })
-               
-    }
-       input.value = ""
+
+
+            input.value = ""
     })
 }
- 
+
 // requisito 9 que tentei mas não consegui
- //eu vi esse video para fazer essa função https://www.youtube.com/watch?v=tr1yzveiArs
+//eu vi esse video para fazer essa função https://www.youtube.com/watch?v=tr1yzveiArs
 /*      function Input() {
         intput.addEventListener('dblclick', function () {
             input.value.classList.add('completed ')
         })
     } 
 
-Input() */ 
+Input() */
 
-Botao()
+
+
+
+createAddTaskEvent()
 
