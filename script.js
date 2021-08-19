a3a4db6200d9fd0524fafa782d4facfce065102b
Wander Luiz <wander.srv@gmail.com>
2021-08-19 19:25:12 -0300

    Projeto concluido

--- script.js
+++ script.js
@@ -7,8 +7,9 @@ const botaoSalvarTarefa = document.querySelector('#salvar-tarefas');
 const removeSelecionado = document.querySelector('#remover-selecionado');
 const botaoCima = document.querySelector('#mover-cima');
 const botaoBaixo = document.querySelector('#mover-baixo');
+
 // Criar item lista
-botaoCriarTarefa.addEventListener('click', function() {
+function criarLi() {
   const criarLi = document.createElement('li');
   criarLi.classList.add('linhasLista');
   criarLi.innerText = inputTexto.value; 
@@ -17,15 +18,19 @@ botaoCriarTarefa.addEventListener('click', function() {
   criarLi.addEventListener('dblclick', riscaLista);
   criarLi.addEventListener('click', selecionaLista);
   inputTexto.value = null;
-})
+}
+
+botaoCriarTarefa.addEventListener('click', criarLi);
+
 // Adiciona seletor cor de fundo
 function selecionaItemCor(event) {
   const liLista = document.querySelectorAll('.linhasLista');
   for (let index = 0; index < liLista.length; index += 1) {
-     liLista[index].style.backgroundColor = 'white';     
+     liLista[index].style.backgroundColor = '';
   }
   event.target.style.backgroundColor = 'rgb(128,128,128)';
 }
+
 // Risca item
 function riscaLista(event) {
   const liLista = document.querySelectorAll('.linhasLista');
@@ -35,6 +40,7 @@ function riscaLista(event) {
     event.target.classList.add('completed');
   }
 }
+
 // Seleciona iten
 function selecionaLista(event) {
   const liLista = document.querySelectorAll('.linhasLista');
@@ -43,10 +49,12 @@ function selecionaLista(event) {
   }
     event.target.classList.add('selected');
 }
+
 // Remove todas li's
 apagaLista.addEventListener('click', function () {
   lista.innerHTML = '';
 })
+
 // Remove Finalizados
   removeFinalizados.addEventListener('click', function() {
   const remover = document.querySelectorAll('.completed');
@@ -54,29 +62,49 @@ apagaLista.addEventListener('click', function () {
     remover[index].parentNode.removeChild(remover[index]);
   }
 })
+
 // Salvar pagina
 botaoSalvarTarefa.addEventListener('click', function () {
-  const listaTarefas = document.querySelectorAll('.linhasLista');
+  let listaTarefas = document.querySelectorAll('.linhasLista');
   let listaSalva = [];
   for (let index = 0; index < listaTarefas.length; index += 1) {
-    let objetoLista = {classe: listaTarefas[index].className,texto: listaTarefas[index].innerText};
+    let objetoLista = {classe: listaTarefas[index].className, texto: listaTarefas[index].innerText};
     listaSalva.push(objetoLista);
   }
-  localStorage.setItem('lista',JSON.stringify(listaSalva));
+  localStorage.setItem('listaNova',JSON.stringify(listaSalva));
 })
+
+// Carregar lista
+function carregaLista(event) {
+  let recuperaLista = localStorage.getItem('listaNova');
+    if (recuperaLista !== null) {
+    recuperaLista = JSON.parse(recuperaLista);
+    let listaTarefas = document.querySelector('#lista-tarefas')
+    for (let index = 0; index < recuperaLista.length; index += 1) {
+      event = document.createElement('li');
+      event.addEventListener('click', selecionaItemCor);
+      event.addEventListener('dblclick', riscaLista);
+      event.addEventListener('click', selecionaLista);
+      event.innerText = recuperaLista[index].texto;
+      event.className = recuperaLista[index].classe;
+      listaTarefas.appendChild(event);
+    }
+  }
+}
+
 // Função Transformar Elemento
 function transformarElemento (elemento) {
   let objeto = { texto: elemento.innerText, classe: elemento.className };
   
   return objeto;
 }
+
 // Mover Elemento pra cima
 botaoCima.addEventListener('click', function() {
   const liSelecionado = document.querySelector('.selected');
 
   if (liSelecionado) {
     let alvoLi = liSelecionado.previousElementSibling;
-    
     if (alvoLi) {
       let objetoSelecionado = transformarElemento(liSelecionado);
       let objetoAlvo = transformarElemento(alvoLi);
@@ -84,7 +112,7 @@ botaoCima.addEventListener('click', function() {
       liSelecionado.innerText = objetoAlvo.texto;
       liSelecionado.className = objetoAlvo.classe;
       liSelecionado.id = '';
-      liSelecionado.style.backgroundColor = 'white'
+      liSelecionado.style.backgroundColor = '';
 
       alvoLi.innerText = objetoSelecionado.texto;
       alvoLi.className = objetoSelecionado.classe;
@@ -93,13 +121,13 @@ botaoCima.addEventListener('click', function() {
     }
   }
 })
+
 // Move Elemento pra baixo
 botaoBaixo.addEventListener('click', function() {
   const liSelecionado = document.querySelector('.selected');
 
   if (liSelecionado) {
     let alvoLi = liSelecionado.nextElementSibling;
-    
     if (alvoLi) {
       let objetoSelecionado = transformarElemento(liSelecionado);
       let objetoAlvo = transformarElemento(alvoLi);
@@ -112,10 +140,11 @@ botaoBaixo.addEventListener('click', function() {
       liSelecionado.innerText = objetoAlvo.texto;
       liSelecionado.className = objetoAlvo.classe;
       liSelecionado.id = '';
-      liSelecionado.style.backgroundColor = 'white'
+      liSelecionado.style.backgroundColor = '';
     }
   }
 })
+
 // Remover Selecionado
 removeSelecionado.addEventListener('click', function(){
   let removerSelecionado = document.querySelector('.selected');
@@ -125,18 +154,8 @@ removeSelecionado.addEventListener('click', function(){
     removerSelecionado.parentNode.removeChild(removerSelecionado);
   }
 })
-// Recupera pagina depois que fechada
-window.onload = function() {  
-  let recuperaLista = localStorage.getItem('lista');
-  recuperaLista = JSON.parse(recuperaLista);
-  let listaTarefas = document.querySelector('#lista-tarefas')
-  for (let index = 0; index < recuperaLista.length; index += 1) {
-    let criarLi = document.createElement('li');
-    criarLi.addEventListener('click', selecionaItemCor);
-    criarLi.addEventListener('dblclick', riscaLista);
-    criarLi.addEventListener('click', selecionaLista);
-    criarLi.innerText = recuperaLista[index].texto;
-    criarLi.className = recuperaLista[index].classe;
-    listaTarefas.appendChild(criarLi);
-  }
+
+// Inicializa Funções
+window.onload = function() {
+  carregaLista();
 }
