22c63de47a964a651a554464c64b3b91495f2fa2
Joao Pedro de Oli. Lage <JoaoPedro.Lage@outlook.com>
2021-08-20 11:38:57 -0300

    Tenta passar nos requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
+const creatTask = document.getElementById('criar-tarefa');
+const list = document.getElementById('lista-tarefas');
+const allLi = document.getElementsByTagName('li');
+
 function addItem() {
-  const add = document.getElementById('texto-tarefa').value;
-  let addToList = document.getElementById('lista-tarefas').innerHTML;
-  addToList = `${addToList}<li>${add}</li>`;
+  const input = document.getElementById('texto-tarefa').value;
+  let addToList = list.innerHTML;
+  addToList = `${addToList}<li>${input}</li>`;
 
   document.getElementById('lista-tarefas').innerHTML = addToList;
   document.getElementById('texto-tarefa').value = '';
 }
+
+function clickChange(event) {
+  for (let i = 0; i < allLi.length; i += 1) {
+    if (allLi[i].classList.contains('greySelected')) {
+      allLi[i].classList.remove('greySelected');
+    }
+  }
+  event.target.classList.add('greySelected');
+}
+
+creatTask.addEventListener('click', addItem);
+list.addEventListener('click', clickChange);
