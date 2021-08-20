c9cbecd188b999be4bfc6fa5c39b0bdf17839117
dionelia <dionelia.pereira@gmail.com>
2021-08-20 17:24:11 -0300

    correcao

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let recebe_info = document.getElementById('texto-tarefa');
 
 document.getElementById('criar-tarefa').addEventListener('click', function() {
     let li = document.createElement('li');
-    var li = document.createElement('li')
     li.addEventListener('click', function(){
         li.style.backgroundColor = 'rgb(128,128,128)'
     })
