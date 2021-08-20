956c4557397d1124ed811bea463a2c5c3a90318f
Laurenz Pinder <laurenzdpinder90@gmail.com>
2021-08-19 22:02:43 -0300

    Modifica/Simplifica Requisitos 5 e 6

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
-const tarefas = [];
+const input = document.querySelector('#texto-tarefa');
 const button = document.querySelector('#criar-tarefa');
-const listaTarefas = document.querySelector('#lista-tarefas');
+const taskList = document.querySelector('#lista-tarefas');
 
-function handleTarefas() {
-  const size = tarefas.length - 1;
-  const novaTarefa = tarefas[size];
-  const tarefaList = document.createElement('li');
-  tarefaList.innerText = novaTarefa;
-  listaTarefas.appendChild(tarefaList);
-}
-
-function handleButton() {
-  const textoTarefa = document.querySelector('#texto-tarefa');
-  tarefas.push(textoTarefa.value);
-  textoTarefa.value = '';
-  handleTarefas();
+function taskCreator() {
+  const taskListItem = document.createElement('li');
+  taskListItem.innerText = input.value;
+  input.value = '';
+  taskList.appendChild(taskListItem);
 }
 
 function greyClick(event) {
@@ -36,6 +28,6 @@ function lineThrough(event) {
   }
 }
 
-button.addEventListener('click', handleButton);
-listaTarefas.addEventListener('click', greyClick);
-listaTarefas.addEventListener('dblclick', lineThrough)
+button.addEventListener('click', taskCreator);
+taskList.addEventListener('click', greyClick);
+taskList.addEventListener('dblclick', lineThrough);
