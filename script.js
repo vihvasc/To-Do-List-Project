78e4d233f5aa456863add6349dd9a363696b6759
Samuel Pereira <gmr.quest.10@gmail.com>
2021-08-21 11:35:43 -0300

    Requisitos 7 e 8 concluidos

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-let button = document.getElementById('criar-tarefa');
-let lista = document.getElementById('lista-tarefas');
-let inputValue = document.getElementById('texto-tarefa');
+const button = document.getElementById('criar-tarefa');
+const list = document.getElementById('lista-tarefas');
+const inputValue = document.getElementById('texto-tarefa');
 
 button.addEventListener('click', addListItem);
 
 function addListItem() {
-  let item = document.createElement('li');
+  const item = document.createElement('li');
   item.innerHTML = inputValue.value;
-  lista.appendChild(item);
+  list.appendChild(item);
 
   inputValue.value = '';
+  chageBackground();
 }
+
+function chageBackground() {
+  const itens = document.querySelectorAll('li');
+
+  for (let i = 0; i < itens.length; i += 1) {
+    itens[i].addEventListener('click', function() {
+      clearBackground();
+      itens[i].style.backgroundColor = 'rgb(128,128,128)';
+    });
+  }
+}
+
+function clearBackground() {
+  const itens = document.querySelectorAll('li');
+  
+  for (let i = 0; i < itens.length; i += 1) {
+    itens[i].style.backgroundColor = 'white';
+  }
+}
+
