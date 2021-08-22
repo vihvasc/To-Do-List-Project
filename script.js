da16b269defbe65b496a7bb620cf8579f810352f
LaizaBertelli <laizabertelli@hotmail.com>
2021-08-22 10:46:21 -0300

    Corrige erros de lint

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ function addClasses() {
   const tamanho = window.localStorage.getItem('tamanho');
   const list = document.querySelectorAll('li');
   for (let i = 0; i < tamanho; i += 1) {
-    let name = `list${i}Class`;
-    let classe = window.localStorage.getItem(name);
+    const name = `list${i}Class`;
+    const classe = window.localStorage.getItem(name);
     list[i].className = classe;
   }
 }
 // função para mudar a cor dos items da lista
-function changeColor () {
-  let items = document.querySelectorAll('li');
-  for(let i = 0; i < items.length; i += 1) {
+function changeColor() {
+  const items = document.querySelectorAll('li');
+  for (let i = 0; i < items.length; i += 1) {
     items[i].style.backgroundColor = 'white';
   }
-  let selected = document.querySelector('.selected');
+  const selected = document.querySelector('.selected');
   selected.style.backgroundColor = 'rgb(128, 128, 128)';
- }
+}
 // função para selecionar um elemento da lista
 function selectElement(event) {
   const items = document.querySelectorAll('li');
@@ -28,11 +28,27 @@ function selectElement(event) {
   }
   changeColor();
 }
- // função que verifica se um item tem a classe completed
- function verifyCompleted(event) {
-  let items = document.querySelectorAll('li');
+
+// função que remove a decoração do texto
+function removeStripe(index) {
+  const items = document.querySelectorAll('li');
+  items[index].style.textDecoration = 'none';
+  items[index].classList.remove('completed');
+}
+
+// função que adiciona decoração no texto
+function addStripe(index) {
+  const items = document.querySelectorAll('li');
+  items[index].style.textDecoration = 'line-through';
+  items[index].classList.add('completed');
+}
+
+// função que verifica se um item tem a classe completed
+function verifyCompleted(event) {
+  const items = document.querySelectorAll('li');
+  const eventTarget = event.target;
   for (let i = 0; i < items.length; i += 1) {
-    if (items[i] === event.target) {
+    if (items[i] === eventTarget) {
       if (items[i].classList.contains('completed')) {
         removeStripe(i);
         break;
@@ -42,7 +58,7 @@ function selectElement(event) {
       }
     }
   }
- }
+}
 
 // função para criar eventos de clique e double click em items da lista
 function createListEvents() {
@@ -51,27 +67,25 @@ function createListEvents() {
     listItem[i].addEventListener('click', selectElement);
   }
 
-  for(let i = 0; i < listItem.length; i += 1) {
+  for (let i = 0; i < listItem.length; i += 1) {
     listItem[i].addEventListener('dblclick', verifyCompleted);
   }
 }
-
+// função que estaura a lista a partir do localStorage
 function restoreList() {
-  let ol = document.getElementById('lista-tarefas');
-  let tamanho = window.localStorage.getItem('tamanho');
-  
+  const ol = document.getElementById('lista-tarefas');
+  const tamanho = window.localStorage.getItem('tamanho');
   for (let i = 0; i < tamanho; i += 1) {
-    let name = `list${i}`;
-    let item = window.localStorage.getItem(name);
-    let li = document.createElement('li');
+    const name = `list${i}`;
+    const item = window.localStorage.getItem(name);
+    const li = document.createElement('li');
     li.innerText = item;
     ol.appendChild(li);
   }
   addClasses();
   createListEvents();
 }
-verifyStorage();
-
+// função que verifica o tamanho da lista de items no localStorage
 function verifyStorage() {
   const length = window.localStorage.getItem('tamanho');
   if (length !== 0) {
@@ -79,92 +93,75 @@ function verifyStorage() {
   }
 }
 
-createEvent();
+verifyStorage();
 
 // função que pega o valor digitado no input
 function getValue() {
   const input = document.getElementById('texto-tarefa');
-  let tarefa = input.value;
-  return tarefa;
+  const task = input.value;
+  return task;
 }
 
 // função que adiciona os valores do input à uma lista
 function addValue() {
-
-  let ol = document.getElementById('lista-tarefas');
-  let list = document.createElement('li');
+  const ol = document.getElementById('lista-tarefas');
+  const list = document.createElement('li');
   list.classList.add('item-list');
   list.innerText = getValue();
   ol.appendChild(list);
-  
   const input = document.getElementById('texto-tarefa');
   input.value = '';
-
   createListEvents();
 }
 
-// função que adiciona decoração no texto
-function addStripe(index) {
-  let items = document.querySelectorAll('li');
-  items[index].style.textDecoration = 'line-through';
-  items[index].classList.add('completed');
-}
-
-// função que remove a decoração do texto
-function removeStripe(index) {
-  let items = document.querySelectorAll('li')
-  items[index].style.textDecoration = 'none';
-  items[index].classList.remove('completed');
-
-}
-
 // função que apaga todos os items da lista
-function clearList () {
-  let items = document.querySelectorAll('li');
-  for (let key of items) {
-    key.remove();
+function clearList() {
+  const items = document.querySelectorAll('li');
+  for (let i = 0; i < items.length; i += 1) {
+    items[i].remove();
   }
 }
 
 // função que apaga items riscados
 function clearCompleted() {
-  let items = document.querySelectorAll('.completed');
-  for (let key of items) {
-    key.remove();
+  const items = document.querySelectorAll('.completed');
+  for (let i = 0; i < items.length; i += 1) {
+    items[i].remove();
   }
 }
 
 // função para salvar lista de tarefas
 function saveList() {
   let length = 0;
-  let list = document.querySelectorAll('li');
+  const list = document.querySelectorAll('li');
   for (let i = 0; i < list.length; i += 1) {
-    let name = `list${i}`;
-    let nameClass = `list${i}Class`;
-    let listClass = list[i].classList;
-    let text = list[i].innerText;
-    window.localStorage.setItem(name, text)
+    const name = `list${i}`;
+    const nameClass = `list${i}Class`;
+    const listClass = list[i].classList;
+    const text = list[i].innerText;
+    window.localStorage.setItem(name, text);
     window.localStorage.setItem(nameClass, listClass);
-    length += 1; 
+    length += 1;
   }
   window.localStorage.setItem('tamanho', length);
 }
 
 // função para remover item selcionado
 function removeSelected() {
-  let selected = document.querySelector('.selected');
+  const selected = document.querySelector('.selected');
   selected.remove();
 }
 
 // função para mudar item selecionado
 function changesSelected(selected, changed) {
-  let items = document.querySelectorAll('li');
-  for (let key of items) {
-    if (key === selected) {
-      key.classList.remove('selected');
+  const items = document.querySelectorAll('li');
+
+  for (let i = 0; i < items.length; i += 1) {
+    if (items[i] === selected) {
+      items[i].classList.remove('selected');
     }
-    if (key === changed) {
-      key.classList.add('selected');
+    if (items[i] === changed) {
+      items[i].classList.add('selected');
     }
   }
 }
@@ -173,7 +170,7 @@ function changesSelected(selected, changed) {
 function getPosition() {
   const list = document.querySelectorAll('li');
   const selected = document.querySelector('.selected');
-  let positionPreviousLater = [];
+  const positionPreviousLater = [];
 
   for (let i = 0; i < list.length; i += 1) {
     if (list[i] === selected) {
@@ -189,32 +186,32 @@ function getPosition() {
 // função para mover item para cima
 function moveUp() {
   const list = document.querySelectorAll('li');
-  let selected = document.querySelector('.selected');
+  const selected = document.querySelector('.selected');
   const selectedText = selected.innerText;
-  let positionPrevious = getPosition();
-  let positionPreviousText = positionPrevious[1].innerText
+  const positionPrevious = getPosition();
+  const positionPreviousText = positionPrevious[1].innerText;
   for (let i = 0; i < list.length; i += 1) {
     if (i === positionPrevious[0] - 1) {
       list[i].innerText = selectedText;
-      list[i + 1 ].innerText = positionPreviousText;
-    } 
+      list[i + 1].innerText = positionPreviousText;
+    }
   }
   changesSelected(selected, positionPrevious[1]);
   changeColor();
 }
 
-// função para mover para baixo
+// função para mover item para baixo
 function moveDown() {
   const list = document.querySelectorAll('li');
-  let selected = document.querySelector('.selected');
-  let positionLater = getPosition();
-  let selectedText = selected.innerText;
-  let positionLaterText = positionLater[2].innerText;
+  const selected = document.querySelector('.selected');
+  const positionLater = getPosition();
+  const selectedText = selected.innerText;
+  const positionLaterText = positionLater[2].innerText;
   for (let i = 0; i < list.length; i += 1) {
     if (i === positionLater[0] + 1) {
       list[i].innerText = selectedText;
       list[i - 1].innerText = positionLaterText;
-    } 
+    }
   }
   changesSelected(selected, positionLater[2]);
   changeColor();
@@ -222,36 +219,31 @@ function moveDown() {
 
 // verifica se algum item está selecionado ao clicar no botão mover-baixo
 function verifySelectedDown() {
-  let selected = document.querySelector('.selected');
-  let listItems = document.querySelectorAll('li');
+  const selected = document.querySelector('.selected');
+  const listItems = document.querySelectorAll('li');
   let count = 0;
-  for (let key of listItems) {
+  // refatorar este for para um for simples
+  for (let i = 0; i < listItems.length; i += 1) {
     count += 1;
   }
-  if(selected !== null && selected !== listItems[count-1]) {
+  if (selected !== null && selected !== listItems[count - 1]) {
     moveDown();
   }
 }
 
 // verifica se algum item está selecionado ao clicar no botão mover-cima
 function verifySelectedUp() {
-  let selected = document.querySelector('.selected');
-  let listItems = document.querySelectorAll('li');
-    if (selected !== null && selected !== listItems[0]) {
-      moveUp();
-    } 
+  const selected = document.querySelector('.selected');
+  const listItems = document.querySelectorAll('li');
+  if (selected !== null && selected !== listItems[0]) {
+    moveUp();
+  }
 }
 
 // função para criar eventos gerais
-function createEvent() {
+function createEventSave() {
   const button = document.getElementById('criar-tarefa');
   button.addEventListener('click', addValue);
-
-  const clearbutton = document.getElementById('apaga-tudo');
-  clearbutton.addEventListener('click', clearList);
-
-  const clearCompletedList = document.getElementById('remover-finalizados');
-  clearCompletedList.addEventListener('click', clearCompleted);
 
   const saveItems = document.getElementById('salvar-tarefas');
   saveItems.addEventListener('click', saveList);
@@ -261,7 +253,18 @@ function createEvent() {
 
   const moveDownButton = document.getElementById('mover-baixo');
   moveDownButton.addEventListener('click', verifySelectedDown);
+}
+
+function createEventRemove() {
+  const clearbutton = document.getElementById('apaga-tudo');
+  clearbutton.addEventListener('click', clearList);
+
+  const clearCompletedList = document.getElementById('remover-finalizados');
+  clearCompletedList.addEventListener('click', clearCompleted);
 
   const removeSelectedButton = document.getElementById('remover-selecionado');
   removeSelectedButton.addEventListener('click', removeSelected);
 }
+
+createEventRemove();
+createEventSave();
