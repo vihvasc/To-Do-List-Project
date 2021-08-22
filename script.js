208d55eabf491f6a0a2990e4066052ee558821cc
dfernan6 <danilo.afernandes@outlook.com>
2021-08-22 13:16:07 -0300

    Enviando a resposta #9

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
-let ingredientsList = [
-    "bisteca",
-]
-
-
 let list = document.getElementById("lista-tarefas");
-    function addTarefa() {
-        let item = document.createElement("li");
-        let input = document.getElementById("texto-tarefa");
-      
-        item.innerText = input.value;
-        item.className = "itemList";
-        list.appendChild(item);
-        input.value = "";
+function addTarefa() {
+  let item = document.createElement("li");
+  let input = document.getElementById("texto-tarefa");
+  
+  item.innerText = input.value;
+  item.className = "itemList";
+  list.appendChild(item);
+  input.value = "";
+}
+  
+let buttonAddTarefa = document.getElementById("criar-tarefa");
+buttonAddTarefa.addEventListener("click", addTarefa);
+
+function changeBackgroundItem(evento) {
+let itemsTarefa = list;
+let itemList = document.querySelectorAll(".itemList");
+    if (itemList) {
+      for (let index = 0; index < itemList.length; index += 1) {
+        itemList[index].classList.remove("selected");
       }
-      
-    let buttonAddTarefa = document.getElementById("criar-tarefa");
-    buttonAddTarefa.addEventListener("click", addTarefa);
-    
-    function changeBackgroundItem(evento) {
-    let itemsTarefa = list;
+      evento.target.classList.add("selected");
+    }
+}
+
+list.addEventListener("click", changeBackgroundItem);
+function changeForCompleted(evento) {
     let itemList = document.querySelectorAll(".itemList");
-      
-        if (itemList) {
-          for (let index = 0; index < itemList.length; index += 1) {
-            itemList[index].classList.remove("selected");
-          }
-          evento.target.classList.add("selected");
-        }
-      }
-    list.addEventListener("click", changeBackgroundItem);
+    if (itemList) {
+    if (evento.target.classList.contains("completed")) {
+        evento.target.classList.remove("completed");
+    } else {
+        evento.target.classList.add("completed");
+    }
+    }
+}
+list.addEventListener("dblclick", changeForCompleted);
