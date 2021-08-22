39aae5352a63ca53dc2c8bb77123533501bbdd3f
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2021-08-22 20:48:54 -0300

    alterado as cores no css pois pelo padrao eslint nao passa no projeto

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // Função para criar items na lista puxando o valor do input
 function criaListaDeItem() {
-
   const listaTarefas = document.getElementById('lista-tarefas');
 
   const input = document.getElementById('texto-tarefa');
@@ -22,11 +21,9 @@ addButton.addEventListener('click', criaListaDeItem);
 
 // Função para alterar o background de uma tarefa da lista quando clicado uma vez
 function selecionartarefa(event) {
-
   const tarefa = document.querySelectorAll('li');
 
   for (let index = 0; index < tarefa.length; index += 1) {
-
     if (tarefa[index].classList.contains('selected')) { // https://www.javascripttutorial.net/dom/css/check-if-an-element-contains-a-class/ dica de amigos.
       const selecionadoTarefa = document.querySelector('.selected');
       selecionadoTarefa.classList.remove('selected');
@@ -38,7 +35,6 @@ function selecionartarefa(event) {
 
 // Função para riscar uma tarefa concluida clicando duas veses
 function riscaItenLista(event) {
-
   if (event.target.classList.contains('completed')) {
     event.target.classList.remove('completed');
   } else {
