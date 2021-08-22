6a1088e5f54461410327319f64f85778376644e7
diego.camomila@gmail.com <git@github.com:diegocamomila/repositorio--tetste.git>
2021-08-22 20:30:21 -0300

    mofificado estrutura arquivo js requisitos 1 ao 9 atingidos

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
-// funçao add itens na lista
-const botao = document.querySelector('#criar-tarefa');
+// Função para criar items na lista puxando o valor do input
+function criaListaDeItem() {
 
-function botaoAdd() {
-  const descricaoTarefa = document.querySelector('input');
+  const listaTarefas = document.getElementById('lista-tarefas');
+
+  const input = document.getElementById('texto-tarefa');
+
   const novaTarefa = document.createElement('li');
-  const listaTarefas = document.querySelector('#lista-tarefas');
-
-  novaTarefa.innerHTML = descricaoTarefa.value;
-  novaTarefa.setAttribute('class', 'completed');
-  descricaoTarefa.value = '';
+  novaTarefa.innerText = input.value;
+  novaTarefa.className = 'tarefa';
+  novaTarefa.addEventListener('click', selecionartarefa);
+  novaTarefa.addEventListener('dblclick', riscaItenLista);
 
   listaTarefas.appendChild(novaTarefa);
 
+  input.value = '';
 }
 
-botao.addEventListener('click', botaoAdd);
+const addButton = document.getElementById('criar-tarefa');
 
+addButton.addEventListener('click', criaListaDeItem);
+
+// Função para alterar o background de uma tarefa da lista quando clicado uma vez
+function selecionartarefa(event) {
+
+  const tarefa = document.querySelectorAll('li');
+
+  for (let index = 0; index < tarefa.length; index += 1) {
+
+    if (tarefa[index].classList.contains('selected')) { // https://www.javascripttutorial.net/dom/css/check-if-an-element-contains-a-class/ dica de amigos.
+      const selecionadoTarefa = document.querySelector('.selected');
+      selecionadoTarefa.classList.remove('selected');
+    }
+  }
+
+  event.target.classList.add('selected');
+}
+
+// Função para riscar uma tarefa concluida clicando duas veses
+function riscaItenLista(event) {
+
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+}
