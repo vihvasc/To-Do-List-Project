1d9df2f04d047048325c64ac46bb3b03e20afa5f
NicolasDimitri <nicolasdodimitri@gmail.com>
2021-08-23 18:42:54 -0300

    requisito 9 concluido

--- script.js
+++ script.js
@@ -3,23 +3,36 @@ const listaDeTarefas = document.getElementById('lista-tarefas');
 
 function trocaCorDaTarefa(event) {
   const tarefaAnterior = document.querySelector('.tarefa-selecionada');
-      const tarefaSelecionada = event.target;
-      tarefaAnterior.classList.remove("tarefa-selecionada");
-      tarefaSelecionada.className = "tarefa-selecionada";
+  const tarefaSelecionada = event.target;
+  const classeDaTarefa = 'tarefa-selecionada';
+  if (tarefaAnterior) {
+    tarefaAnterior.classList.remove(classeDaTarefa);
+    tarefaSelecionada.classList.add(classeDaTarefa);
+  } else {
+    tarefaSelecionada.classList.add(classeDaTarefa);
+  }
+}
+
+function riscaTarefa(event) {
+  const selecionado = document.querySelector('.tarefa-selecionada');
+  if (
+    selecionado.className === 'completed tarefa-selecionada'
+    || selecionado.className === 'tarefa-selecionada completed'
+  ) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
 }
 
 function adicionaTarefa() {
   const tarefa = document.createElement('li');
   const textoEscritoNoInput = document.getElementById('texto-tarefa');
-  listaDeTarefas.appendChild(tarefa);
-  tarefa.innerHTML = textoEscritoNoInput.value;
+  tarefa.innerText = textoEscritoNoInput.value;
   textoEscritoNoInput.value = '';
   tarefa.addEventListener('click', trocaCorDaTarefa);
+  tarefa.addEventListener('dblclick', riscaTarefa);
+  listaDeTarefas.appendChild(tarefa);
 }
 
 btnAdicionar.addEventListener('click', adicionaTarefa);
-function arrumaBug() {
-let teste = document.getElementById("teste");
-teste.className = "tarefa-selecionada";
-}
-window.onload = arrumaBug();
