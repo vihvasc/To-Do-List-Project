19405584d2fc476f89a12cdc3574f9e029dc5d60
Samuel Pereira <gmr.quest.10@gmail.com>
2021-08-23 15:11:19 -0300

    Requisito 11 concluido

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const button = document.getElementById('criar-tarefa');
 const list = document.getElementById('lista-tarefas');
 const inputValue = document.getElementById('texto-tarefa');
 const clearButton = document.getElementById('apaga-tudo');
+const clearDone = document.getElementById('remover-finalizados');
 
 button.addEventListener('click', addListItem);
 
@@ -14,7 +15,8 @@ function addListItem() {
   changeBackground();
 }
 
-// Função criada com base no código da Virginia Alcântara >> https://github.com/tryber/sd-015-b-project-todo-list/blob/virginia-alcantara-todo-list-project/script.js
+// Função construída e entendida com base no código da Virginia Alcântara
+// https://github.com/tryber/sd-015-b-project-todo-list/blob/virginia-alcantara-todo-list-project/script.js
 function changeBackground() {
   const itens = document.querySelectorAll('li');
 
@@ -46,5 +48,17 @@ function clearAll() {
   list.innerHTML = '';
 }
 
+// Função entendida e construída com base no código do Gabriel Alves
+// https://github.com/tryber/sd-015-b-project-todo-list/blob/gabriel-alves-todo-list-project/script.js
+function removeDones() {
+  let done = document.getElementsByClassName('completed');
+  const i = 0;
+
+  while (done.length > 0) {
+    done[i].remove();
+  }
+}
+
 list.addEventListener('dblclick', doneTasks);
 clearButton.addEventListener('click', clearAll);
+clearDone.addEventListener('click', removeDones)
