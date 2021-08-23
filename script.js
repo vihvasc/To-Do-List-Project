0d1fc8c14d38ee419230e4e4e436088fe3a45806
NicolasDimitri <nicolasdodimitri@gmail.com>
2021-08-23 03:40:34 -0300

    requisito 8 completo

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const btnAdicionar = document.getElementById('criar-tarefa');
 const listaDeTarefas = document.getElementById('lista-tarefas');
 
-// function trocaCorDaTarefa(event) {
-//     const tarefaAnterior = document.querySelector('.tarefa-selecionada');
-//     const tarefaSelecionada = event.target;
-//     tarefaSelecionada.className = "tarefa-selecionada";
-//     tarefaAnterior.classList.remove("tarefa-selecionada");
-// }
+function trocaCorDaTarefa(event) {
+  const tarefaAnterior = document.querySelector('.tarefa-selecionada');
+      const tarefaSelecionada = event.target;
+      tarefaAnterior.classList.remove("tarefa-selecionada");
+      tarefaSelecionada.className = "tarefa-selecionada";
+}
 
 function adicionaTarefa() {
   const tarefa = document.createElement('li');
@@ -14,9 +14,12 @@ function adicionaTarefa() {
   listaDeTarefas.appendChild(tarefa);
   tarefa.innerHTML = textoEscritoNoInput.value;
   textoEscritoNoInput.value = '';
-//   tarefa.innerHTML = textoEscritoNoInput.value;
-//   textoEscritoNoInput.value = '';
-  //   tarefa.addEventListener('click', trocaCorDaTarefa);
+  tarefa.addEventListener('click', trocaCorDaTarefa);
 }
 
 btnAdicionar.addEventListener('click', adicionaTarefa);
+function arrumaBug() {
+let teste = document.getElementById("teste");
+teste.className = "tarefa-selecionada";
+}
+window.onload = arrumaBug();
