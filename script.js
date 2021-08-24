84a8884f3a8e5ce5d19c18b0d8186f4f1184c8d0
aklimak <alexandre.skk@gmail.com>
2021-08-24 01:52:07 -0300

    Adiciona requisito 9

--- script.js
+++ script.js
@@ -7,16 +7,23 @@ function addTask() {
     let clearInputValue = document.getElementById('texto-tarefa');
     clearInputValue.value = "";
     createListItem.addEventListener('click', changeBackground);
+    createListItem.addEventListener('dblclick', taskComplete);
 }
 
 function changeBackground(event) {
     if (document.getElementsByClassName('selected').length !== 0) {
         document.getElementsByClassName('selected')[0].classList.remove('selected');
-    }
+    } 
     event.target.classList.add('selected');
 }
 
-
+function taskComplete(event) {
+    if (event.target.classList.contains('completed')) {
+        event.target.classList.remove('completed');
+    } else {
+    event.target.classList.add('completed');
+    }
+}
 
 
 
