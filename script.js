62ec297b681170a7aaa95a239093604f7b9a0f13
Kdulima <carlosedu151@gmail.com>
2021-08-24 11:06:05 -0300

    Correção do Lint e Adição de botões

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
-
 const listaTarefa = document.querySelector('#lista-tarefas');
 const botaoApagarTarefas = document.getElementById('apaga-tudo');
 const caixaDeTexto = document.getElementById('texto-tarefa');
-
 // Requisito 7 e 8
-
 function mudaCor(event) {
   const lis = document.querySelectorAll('.itens');
   const corCinza = 'rgb(128, 128, 128)';
   for (let index = 0; index < lis.length; index += 1) {
-  
-    lis[index].style.backgroundColor = 'transparent';
-  
+  lis[index].style.backgroundColor = 'transparent';
 }
 event.target.style.background = corCinza;
 }
-
 // Requisito 10
-
 function apagaTudo (evento) {
   listaTarefa.innerHTML = ''
 }
-
-
 // Requisito 5 e 6
-
 document.getElementById('criar-tarefa').addEventListener('click', function () {
   const li = document.createElement('li');
-  li.className = 'itens';
+  li.className = 'itens', 'completed';
   const listaTarefa = document.getElementById('lista-tarefas');
   listaTarefa.appendChild(li);
   li.innerText = caixaDeTexto.value;
@@ -36,9 +26,12 @@ document.getElementById('criar-tarefa').addEventListener('click', function () {
   li.addEventListener('click', mudaCor);
   document.getElementById('apaga-tudo').addEventListener('click', apagaTudo);
 });
-
-
-// Requisito 10
-
-
-
+// Requisito 9
+function classeCompleted(event) {
+  const itemClicado = event.target;
+  if (itemClicado.className === 'completed') {
+    itemClicado.classList.remove('completed');
+  } else {
+    itemClicado.classList.add('completed');
+  }
+}
