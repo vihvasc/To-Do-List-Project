2c160a6087ced9114b0d1b3c953ab6a5408a4821
Jeffrey <jeffreydouglascsantos@gmail.com>
2021-08-24 01:14:37 -0300

    requisito -7 (acho que vou dormir cedo :D )

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 let inputo = document.getElementById("texto-tarefa");
 let listaDeTarefas = document.getElementById("lista-tarefas");
 let botao = document.getElementById("criar-tarefa");
+let listaLi = document.getElementsByTagName("li");
 
 
 botao.addEventListener("click", coletaInput);
 botao.addEventListener("click", limpaInput);
+listaDeTarefas.addEventListener("click", mudaCorFundoCinza);
 
 function coletaInput(){
     let textoUser = inputo.value;
@@ -14,4 +16,7 @@ function coletaInput(){
 }
 function limpaInput(){
     inputo.value = "";
+}
+function mudaCorFundoCinza(mudaCor){
+mudaCor.target.style.backgroundColor = "rgb(128, 128, 128)"
 }
