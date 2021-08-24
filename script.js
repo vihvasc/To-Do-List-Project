9709bd19d63b467f267b3ef0705dd3792c1ad4e3
João Victor Arouca <joao.silveira@estudante.ufla.br>
2021-08-24 12:13:24 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/* adicionar tarefas */
 let buttonAdicionar = document.getElementById("criar-tarefa");
 buttonAdicionar.addEventListener("click", addButton);
 function addButton() {
@@ -9,9 +10,23 @@ function addButton() {
     ordernedList.appendChild(listaCriada);
     document.getElementById("texto-tarefa").value = "";
 }
+/* remove todas as tarefas */
  let buttonRemover = document.getElementById("apaga-tudo");
  buttonRemover.addEventListener("click", remButton);
  function remButton () {
      let list = document.getElementById("lista-tarefas");
      list.innerHTML = '';  
 }
+/* mudar background color quando selecionado */
+let list = document.getElementById('lista-tarefas');
+list.addEventListener('click', changeBg);
+function changeBg(event) {
+    let selecionado = document.querySelector('.item-list');
+
+    if (selecionado && selecionado !== event.target) {
+      selecionado.classList.remove('item-list');
+    }
+
+    event.target.classList.toggle('item-list');
+  }
+    
