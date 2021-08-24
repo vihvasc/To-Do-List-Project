8b8f7ccbd112c7365481af385bcb9511bb05b4cc
Wilian Pereira <wilian_pereira92@outlook.com>
2021-08-24 13:51:15 -0300

    Refatorando codigo

--- script.js
+++ script.js
@@ -3,20 +3,8 @@ const createButton = document.querySelector('#criar-tarefa');
 const buttonDelete = document.getElementById('apaga-tudo');
 const buttonDeleteTask = document.getElementById('remover-finalizados');
 
-function addedElementToList() {
-  const addedValues = document.querySelector('#texto-tarefa');
-  const text = addedValues;
-  const list = document.createElement('li');
-  orderedList.appendChild(list);
-  list.innerText = text.value;
-  text.value = '';
-}
-
-createButton.addEventListener('click', addedElementToList);
-
-const itemDalista = document.getElementsByTagName('li');
-
-function AdicionarCor(event) {
+function adicionarCor(event) {
+  const itemDalista = document.getElementsByTagName('li');
   for (let index = 0; index < itemDalista.length; index += 1) {
     if (itemDalista[index].classList.contains('corDoFundo')) {
       itemDalista[index].classList.remove('corDoFundo');
@@ -28,13 +16,22 @@ function AdicionarCor(event) {
   }
 }
 
-orderedList.addEventListener('click', AdicionarCor);
-
 function adicionarRisco(event) {
   event.target.classList.toggle('completed');
 }
 
-orderedList.addEventListener('dblclick', adicionarRisco);
+function addedElementToList() {
+  const addedValues = document.querySelector('#texto-tarefa');
+  const text = addedValues;
+  const list = document.createElement('li');
+  orderedList.appendChild(list);
+  list.innerText = text.value;
+  text.value = '';
+  list.addEventListener('click', adicionarCor);
+  list.addEventListener('dblclick', adicionarRisco);
+}
+
+createButton.addEventListener('click', addedElementToList);
 
 function deleteList() {
   const information = document.querySelectorAll('li');
