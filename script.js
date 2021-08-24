37167ff46a0e2eb711f9b9288a1c62f7da7a3e06
aklimak <alexandre.skk@gmail.com>
2021-08-24 00:18:19 -0300

    Adiciona funçao que trocar a cor dos itens clicados

--- script.js
+++ script.js
@@ -3,7 +3,20 @@ function addTask() {
     let listItems = document.getElementById('lista-tarefas');
     let createListItem = document.createElement('li');
     createListItem.textContent = inputValue;
+    createListItem.className = 'taskListed';
     listItems.appendChild(createListItem);
     let clearInputValue = document.getElementById('texto-tarefa');
     clearInputValue.value = "";
+    createListItem.addEventListener('click', changeBackground);
 }
+
+function changeBackground(event) {
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+
+
+
+
+
+
