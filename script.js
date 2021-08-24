14fe57e78e19de1627f8c862a2cbd96161238f59
fariasao <enzofarias2010@gmail.com>
2021-08-23 23:28:09 -0300

    requisito 11 vqv enfim saindo

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const btnCreate = document.getElementById('criar-tarefa');
 const btnErase = document.getElementById('apaga-tudo');
+const btnFinalizados = document.getElementById('remover-finalizados');
 const list = document.getElementById('lista-tarefas');
 
 function createList() {
@@ -35,3 +36,19 @@ function apagaTudo() {
   list.innerHTML = '';
 }
 btnErase.addEventListener('click', apagaTudo);
+
+function apagarListaFinalizados() {
+  const tarefaCompleta = document.querySelectorAll('.completed');
+  for (let index = 0; index < tarefaCompleta.length; index += 1) {
+    const filhosCompletos = tarefaCompleta[index];
+    filhosCompletos.parentNode.removeChild(filhosCompletos);
+  }
+}
+btnFinalizados.addEventListener('click', apagarListaFinalizados)
+
+// Referencias utilizadas
+// https://www.w3schools.com/jsref/met_document_getelementsbyclassname.asp
+// https://developer.mozilla.org/pt-BR/docs/Web/API/Element/classList
+// https://qastack.com.br/programming/197748/how-do-i-change-the-background-color-with-javascript
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event
+// https://pt.stackoverflow.com/questions/4605/remover-elemento-da-p%C3%A1gina-com-javascript
