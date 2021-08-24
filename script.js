37009d7cbc5ca747839259829c6cc55aac660e2e
Kdulima <carlosedu151@gmail.com>
2021-08-24 09:40:59 -0300

    Commit Bomba, Adicionados Requisitos: 4, 6, 7, 8 e 10

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-// Requisito 5
 
+const listaTarefa = document.querySelector('#lista-tarefas');
+const botaoApagarTarefas = document.getElementById('apaga-tudo');
 const caixaDeTexto = document.getElementById('texto-tarefa');
 
-document.getElementById('criar-tarefa').addEventListener('click', function() {
+// Requisito 7 e 8
+
+function mudaCor(event) {
+  const lis = document.querySelectorAll('.itens');
+  const corCinza = 'rgb(128, 128, 128)';
+  for (let index = 0; index < lis.length; index += 1) {
+  
+    lis[index].style.backgroundColor = 'transparent';
+  
+}
+event.target.style.background = corCinza;
+}
+
+// Requisito 10
+
+function apagaTudo (evento) {
+  listaTarefa.innerHTML = ''
+}
+
+
+// Requisito 5 e 6
+
+document.getElementById('criar-tarefa').addEventListener('click', function () {
   const li = document.createElement('li');
+  li.className = 'itens';
   const listaTarefa = document.getElementById('lista-tarefas');
-  listaTarefa.appendChild(li)
-  li.innerText = caixaDeTexto.value
-  caixaDeTexto.value=''
-})
+  listaTarefa.appendChild(li);
+  li.innerText = caixaDeTexto.value;
+  li.style.backgroundColor = 'transparent';
+  caixaDeTexto.value = '';
+  li.addEventListener('click', mudaCor);
+  document.getElementById('apaga-tudo').addEventListener('click', apagaTudo);
+});
+
+
+
